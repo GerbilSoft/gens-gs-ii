3cf6e4618ca8e8f492eb123277e9d4b316738bf7
David Korth <gerbilsoft@verizon.net>
2010-04-19 11:42:32 -0400

    CMake/: Deleted the old NASM modules.
    These *should* have been deleted in commit 71325cb95a0428fffa8a78ec895e4d5019ab100a,
    but git decided to conveniently ignore the deletion.

--- CMake/CMakeTestASM-NASMCompiler.cmake
+++
@@ -1,8 +0,0 @@
-# Source: http://www.cmake.org/Wiki/CMake/Assembler#NASM
-
-# determine the compiler to use for NASM
-
-SET(ASM_DIALECT "-NASM")
-SET(CMAKE_ASM${ASM_DIALECT}_COMPILER_INIT ${_CMAKE_TOOLCHAIN_PREFIX}nasm)
-INCLUDE(CMakeDetermineASMCompiler)
-SET(ASM_DIALECT)
