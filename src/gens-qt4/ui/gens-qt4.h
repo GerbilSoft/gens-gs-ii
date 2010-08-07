f6322652c2b53b75609fbbd60fb4b5b4645b3ccd
David Korth <gerbilsoft@verizon.net>
2010-08-07 03:36:28 -0400

    gens-qt4/ui/gens-qt4.rc: Ported the Win32 resource file from Gens/GS.
    This includes the Gens/GS manifest file, which is needed for stuff like
    XP theming, Vista UAC, and DPI awareness. (Well, Qt4 handles XP theming,
    but we should include it anyway.)
    
    gens-qt4/ui/resources/win32/: Added a bunch of Win32 icons, including
    gensgs.ico (the application icon).
    
    CMakeLists.txt: Enable the windres compiler on MinGW.

---
+++ src/gens-qt4/ui/gens-qt4.h
@@ -0,0 +1,34 @@
+/***************************************************************************
+ * gens-qt4: Gens Qt4 UI.                                                  *
+ * gens-qt4.h: Win32 resource script.                                      *
+ *                                                                         *
+ * Copyright (c) 1999-2002 by Stéphane Dallongeville.                      *
+ * Copyright (c) 2003-2004 by Stéphane Akhoun.                             *
+ * Copyright (c) 2008-2010 by David Korth.                                 *
+ *                                                                         *
+ * This program is free software; you can redistribute it and/or modify it *
+ * under the terms of the GNU General Public License as published by the   *
+ * Free Software Foundation; either version 2 of the License, or (at your  *
+ * option) any later version.                                              *
+ *                                                                         *
+ * This program is distributed in the hope that it will be useful, but     *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ * GNU General Public License for more details.                            *
+ *                                                                         *
+ * You should have received a copy of the GNU General Public License along *
+ * with this program; if not, write to the Free Software Foundation, Inc., *
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.           *
+ ***************************************************************************/
+
+#ifndef __GENS_QT4_RESOURCE_H__
+#define __GENS_QT4_RESOURCE_H__
+
+// Icons,
+#define IDI_GENS_APP		0x0001
+#define IDI_GENS_APP_OLD	0x0002
+#define IDI_GENS_MD		0x0003
+#define IDI_SONIC		0x0004
+#define IDI_SONIC_WAITING	0x0005
+
+#endif /* __GENS_QT4_RESOURCE_H__ */
