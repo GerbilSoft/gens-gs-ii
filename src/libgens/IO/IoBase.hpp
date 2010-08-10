GerbilSoft/gens-gs-ii

commit 3245df6d3ab58e4a29590f0ac569148a178790e7

IoBase::applyTristate(): Make sure the high bit is always from m_lastData.
There's no control bit for data bit 7 in the tristate register, so it's
basically an extra one-bit register that can be used for anything.

src/libgens/IO/IoBase.hpp
@@ -118,8 +118,8 @@ class IoBase
 		 */
 		inline uint8_t applyTristate(uint8_t data) const
 		{
-			data &= ~(m_ctrl & 0x7F);		// Mask output bits.
-			data |= ((m_lastData & 0x7F) & m_ctrl);	// Apply data buffer.
+			data &= (m_ctrl & 0x7F);		// Mask output bits.
+			data |= (m_lastData & (m_ctrl | 0x80));	// Apply data buffer.
 			return data;
 		}
 		

commit 8ff1692500bc469deadce2ae0f8818354e73cb25

IoBase::applyTristate(): Accidentally dropped a bitwise NOT.
This is a regression from commit 3245df6d3ab58e4a29590f0ac569148a178790e7.

src/libgens/IO/IoBase.hpp
@@ -118,7 +118,7 @@ class IoBase
 		 */
 		inline uint8_t applyTristate(uint8_t data) const
 		{
-			data &= (m_ctrl & 0x7F);		// Mask output bits.
+			data &= (~m_ctrl & 0x7F);		// Mask output bits.
 			data |= (m_lastData & (m_ctrl | 0x80));	// Apply data buffer.
 			return data;
 		}