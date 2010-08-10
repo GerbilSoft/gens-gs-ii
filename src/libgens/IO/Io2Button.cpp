f213791a6ab534419990fd3d91f85e3b7982a144
David Korth <gerbilsoft@verizon.net>
2010-08-10 19:10:18 -0400

    libgens/IO/Io2Button.cpp: Initial 2-button controller I/O device class.
    Io2Button emulates a Sega Master System controller.
    
    Note that, due to the way the tristate buffer works, the SMS controller
    has weird side effects on MD games. For example, if you play Sonic 1,
    pressing the 2 button presses both C and Start, so Sonic will jump and
    the game pauses. (The SMS doesn't implement a SELECT line.)
    
    This behavior was verified by franz in #protos.

---
+++ src/libgens/IO/Io2Button.cpp
@@ -0,0 +1,143 @@
+/***************************************************************************
+ * libgens: Gens Emulation Library.                                        *
+ * Io2Button.cpp: 2-button gamepad device.                                 *
+ *                                                                         *
+ * Copyright (c) 1999-2002 by Stéphane Dallongeville.                      *
+ * Copyright (c) 2003-2004 by Stéphane Akhoun.                             *
+ * Copyright (c) 2008-2010 by David Korth.                                 *
+ *                                                                         *
+ * This program is free software; you can redistribute it and/or modify it *
+ * under the terms of the GNU General Public License as published by the   *
+ * Free Software Foundation; either version 2 of the License, or (at your  *
+ * option) any later version.                                              *
+ *                                                                         *
+ * This program is distributed in the hope that it will be useful, but     *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ * GNU General Public License for more details.                            *
+ *                                                                         *
+ * You should have received a copy of the GNU General Public License along *
+ * with this program; if not, write to the Free Software Foundation, Inc., *
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.           *
+ ***************************************************************************/
+
+#include "Io2Button.hpp"
+
+namespace LibGens
+{
+
+/**
+ * readData(): Read data from the controller.
+ * @return Data from the controller.
+ */
+uint8_t Io2Button::readData(void)
+{
+	/**
+	 * Data format: (x == tristate value)
+	 * - xxCBRLDU
+	 * B == button 1
+	 * C == button 2
+	 * x is replaced by m_lastData for output bits.
+	 * x is replaced with 1 for input bits.
+	 */
+	
+	uint8_t ret = (0xC0 | (m_buttons & 0x3F));
+	
+	// Apply tristate settings.
+	// Tristate is 0 for input, 1 for output.
+	ret &= ~(m_ctrl & 0x7F);		// Mask output bits.
+	ret |= ((m_lastData & 0x7F) & m_ctrl);	// Apply output bits.
+	return ret;
+}
+
+
+/**
+ * keyPress(): Key press handler.
+ * @param key Key.
+ */
+void Io2Button::keyPress(int key)
+{
+	// TODO: Allow customizable keymaps.
+	// TODO: Use LibGens keycodes.
+	switch (key)
+	{
+		case 0x01000013:
+			// Qt::Key_Up
+			m_buttons &= ~BTN_UP;
+			break;
+		case 0x01000015:
+			// Qt::Key_Down
+			m_buttons &= ~BTN_DOWN;
+			break;
+		case 0x01000012:
+			// Qt::Key_Left
+			m_buttons &= ~BTN_LEFT;
+			break;
+		case 0x01000014:
+			// Qt::Key_Right
+			m_buttons &= ~BTN_RIGHT;
+			break;
+		case 0x01000004:
+			// Qt::Key_Return
+			m_buttons &= ~BTN_START;
+			break;
+		case 'S':
+			// Qt::Key_S
+			m_buttons &= ~BTN_1;
+			break;
+		case 'D':
+			// Qt::Key_D
+			m_buttons &= ~BTN_2;
+			break;
+		
+		default:
+			break;
+	}
+}
+
+
+/**
+ * keyRelease(): Key release handler.
+ * @param key Key.
+ */
+void Io2Button::keyRelease(int key)
+{
+	// TODO: Allow customizable keymaps.
+	// TODO: Use LibGens keycodes.
+	switch (key)
+	{
+		case 0x01000013:
+			// Qt::Key_Up
+			m_buttons |= BTN_UP;
+			break;
+		case 0x01000015:
+			// Qt::Key_Down
+			m_buttons |= BTN_DOWN;
+			break;
+		case 0x01000012:
+			// Qt::Key_Left
+			m_buttons |= BTN_LEFT;
+			break;
+		case 0x01000014:
+			// Qt::Key_Right
+			m_buttons |= BTN_RIGHT;
+			break;
+		case 0x01000004:
+			// Qt::Key_Return
+			m_buttons |= BTN_START;
+			break;
+		case 'S':
+			// Qt::Key_S
+			m_buttons |= BTN_1;
+			break;
+		case 'D':
+			// Qt::Key_D
+			m_buttons |= BTN_2;
+			break;
+		
+		default:
+			break;
+	}
+}
+
+}
---
+++ src/libgens/IO/Io2Button.hpp
@@ -0,0 +1,48 @@
+/***************************************************************************
+ * libgens: Gens Emulation Library.                                        *
+ * Io2Button.hpp: 3-button gamepad device.                                 *
+ *                                                                         *
+ * Copyright (c) 1999-2002 by Stéphane Dallongeville.                      *
+ * Copyright (c) 2003-2004 by Stéphane Akhoun.                             *
+ * Copyright (c) 2008-2010 by David Korth.                                 *
+ *                                                                         *
+ * This program is free software; you can redistribute it and/or modify it *
+ * under the terms of the GNU General Public License as published by the   *
+ * Free Software Foundation; either version 2 of the License, or (at your  *
+ * option) any later version.                                              *
+ *                                                                         *
+ * This program is distributed in the hope that it will be useful, but     *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ * GNU General Public License for more details.                            *
+ *                                                                         *
+ * You should have received a copy of the GNU General Public License along *
+ * with this program; if not, write to the Free Software Foundation, Inc., *
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.           *
+ ***************************************************************************/
+
+#ifndef __LIBGENS_IO_IO2BUTTON_HPP__
+#define __LIBGENS_IO_IO2BUTTON_HPP__
+
+#include "IoBase.hpp"
+
+namespace LibGens
+{
+
+class Io2Button : public IoBase
+{
+	public:
+		Io2Button() { }
+		Io2Button(const IoBase *other) : IoBase(other) { }
+		virtual ~Io2Button() { }
+		
+		uint8_t readData(void);
+		
+		// Keypress handling functions.
+		void keyPress(int key);
+		void keyRelease(int key);
+};
+
+}
+
+#endif /* __LIBGENS_IO_IO2BUTTON_HPP__ */
