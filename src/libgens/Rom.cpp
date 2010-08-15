GerbilSoft/gens-gs-ii

commit e72112a387a1ec997e34ea9ff654e578815de42f

Reworked all the ROM loading code to allow ROMs to be loaded via "File, Open ROM".
Stuff changed:

- EmuMD::Init_TEST() is now EmuMD::SetRom(). This function takes
  a pointer to a Rom class, which it then uses to initialize all
  of the various settings.

- EEPRom::DetectEEPRomType() now takes a serial number and checksum
  as parameters instead of a full ROM header.

- Rom::initSRam() is now a const function.

- Added Rom::initEEPRom(), which is also a const function. This function
  initializes the specified EEPRom class with the ROM's EEPRom type ID.
  The EEPRom type ID is obtained in Rom::Rom() by calling the static
  EEPRom::DetectROMType() function, and is stored in m_eprType.

- Added Rom::loadRom(). This loads ROM data into the specified buffer.

- GensWindow::openRom(): Call LibGens::EmuMD::SetRom() to set the ROM
  image after the Rom class is created.

Loading ROMs using the "Open ROM" menu item works now.

The "Blit!" menu item no longer works, and will be removed soon.

src/libgens/Rom.cpp
@@ -40,6 +40,7 @@ Rom::Rom(const char *filename, MDP_SYSTEM_ID sysOverride, RomFormat fmtOverride)
 {
 	// Save the filename for later.
 	m_filename = string(filename);
+	m_romSize = 0;
 	
 	// TODO: Unicode conversion on Win32.
 	m_file = fopen(filename, "rb");
@@ -66,8 +67,13 @@ Rom::Rom(const char *filename, MDP_SYSTEM_ID sysOverride, RomFormat fmtOverride)
 	if (m_sysId == MDP_SYSTEM_UNKNOWN)
 		m_sysId = DetectSystem(header, header_size, m_romFormat);
 	
+	/** MD-only stuff here. **/
+	
 	// Load the ROM header information.
 	readHeaderMD(header, header_size);
+	
+	// Detect the EEPRom type from the ROM serial number and checksum.
+	m_eprType = EEPRom::DetectEEPRomType(m_mdHeader.serialNumber, sizeof(m_mdHeader.serialNumber), m_mdHeader.checksum);
 }
 
 Rom::~Rom()
@@ -297,12 +303,15 @@ std::string Rom::SpaceElim(const char *src, size_t len)
  * @param sram Pointer to SRam class.
  * @return 0 on success; non-zero on error.
  */
-int Rom::initSRam(SRam *sram)
+int Rom::initSRam(SRam *sram) const
 {
 	// TODO: Load SRam from a file.
 	// TODO: Should that be implemented here or in SRam.cpp?
 	if (!isOpen())
 		return 1;
+	
+	// Reset SRam before applying any settings.
+	sram->reset();
 	
 	uint32_t start, end;
 	// Check if the ROM header has SRam information.
@@ -351,6 +360,53 @@ int Rom::initSRam(SRam *sram)
 	// Load the SRam file.
 	// TODO
 	
+	return 0;
+}
+
+
+/**
+ * initEEPRom(): Initialize an EEPRom class using the ROM's header information.
+ * @param eeprom Pointer to EEPRom class.
+ * @return 0 on success; non-zero on error.
+ */
+int Rom::initEEPRom(EEPRom *eeprom) const
+{
+	// TODO: Load EEPRom from a file.
+	// TODO: Should that be implemented here or in SRam.cpp?
+	if (!isOpen())
+		return 1;
+	
+	// Reset the EEPRom and set the type.
+	eeprom->reset();
+	eeprom->setEEPRomType(m_eprType);
+	return 0;
+}
+
+
+/**
+ * loadRom(): Load the ROM image into a buffer.
+ * @param buf Buffer.
+ * @param siz Buffer size.
+ * @return 0 on success; non-zero on error.
+ */
+int Rom::loadRom(void *buf, size_t siz)
+{
+	// TODO: Use error code constants.
+	// NOTE: Don't check for a NULL pointer, since a crash helps diagnose problems.
+	if (!isOpen())
+		return 1;
+	
+	if (siz == 0 || siz < m_romSize)
+	{
+		// ROM buffer isn't large enough for the ROM image.
+		return 2;
+	}
+	
+	// Load the ROM image.
+	// TODO: Decompressor support.
+	// TODO: Error handling.
+	fseek(m_file, 0, SEEK_SET);
+	fread(buf, 1, m_romSize, m_file);
 	return 0;
 }
 

src/libgens/Save/EEPRom.cpp
@@ -138,17 +138,26 @@ void EEPRom::reset(void)
 
 /**
  * DetectEEPRomType(): Detect the EEPRom type used by the specified ROM.
- * @param header ROM header. (Must contain the full 512-byte header!)
+ * @param serial Serial number.
+ * @param serial_len Length of the serial number string.
+ * @param checksum Checksum.
  * @return ROM type, or -1 if this ROM isn't known.
  */
-int EEPRom::DetectEEPRomType(const uint8_t *header)
+int EEPRom::DetectEEPRomType(const char *serial, size_t serial_len, uint16_t checksum)
 {
 	// Scan the database for potential matches.
 	for (int i = 0; i < (sizeof(ms_Database)/sizeof(ms_Database[0])); i++)
 	{
 		// TODO: Figure out how to get rid of the strlen().
-		int serial_len = strlen(ms_Database[i].game_id);
-		if (!memcmp(&header[0x183], ms_Database[i].game_id, serial_len))
+		int dbSerial_len = strlen(ms_Database[i].game_id);
+		if (dbSerial_len > serial_len)
+		{
+			// Serial number in the database is longer than
+			// the given serial number data.
+			continue;
+		}
+		
+		if (!memcmp(serial, ms_Database[i].game_id, serial_len))
 		{
 			// Serial number matches.
 			if (ms_Database[i].checksum == 0)
@@ -158,7 +167,6 @@ int EEPRom::DetectEEPRomType(const uint8_t *header)
 			}
 			
 			// Checksum verification is required.
-			uint16_t checksum = ((header[0x18E] << 8) | header[0x18F]);
 			if (checksum == ms_Database[i].checksum)
 			{
 				// Checksum matches.

src/libgens/Save/EEPRom.hpp
@@ -28,7 +28,9 @@
 #ifndef __LIBGENS_SAVE_EEPROM_HPP__
 #define __LIBGENS_SAVE_EEPROM_HPP__
 
+// C includes.
 #include <stdint.h>
+#include <string.h>
 
 namespace LibGens
 {
@@ -41,7 +43,7 @@ class EEPRom
 		void reset(void);
 		
 		/** EEPROM type detection. **/
-		static int DetectEEPRomType(const uint8_t *header);
+		static int DetectEEPRomType(const char *serial, size_t serial_len, uint16_t checksum);
 		
 		/**
 		 * setEEPRomType(): Set the EEPRom type.

src/gens-qt4/GensWindow.cpp
@@ -268,9 +268,12 @@ void GensWindow::menuTriggered(int id)
 				
 				case MNUID_ITEM(IDM_FILE_BLIT):
 					// Blit!
+					// DEPRECATED
+#if 0
 					LibGens::EmuMD::Init_TEST();
 					m_vBackend->setVbDirty();
 					m_vBackend->vbUpdate();
+#endif
 					break;
 				
 				case MNUID_ITEM(IDM_FILE_EMUTHREAD):
@@ -561,9 +564,12 @@ void GensWindow::openRom(void)
 	m_rom = new LibGens::Rom(filename.toUtf8().constData());
 	printf("ROM information: format == %d, system == %d\n", m_rom->romFormat(), m_rom->sysId());
 	
-	// TODO: Process the ROM image.
-	delete m_rom;
-	m_rom = NULL;
+	// Load the ROM image in EmuMD.
+	LibGens::EmuMD::SetRom(m_rom);
+	m_rom->close();
+	
+	// m_rom isn't deleted, since keeping it around
+	// indicates that a game is running.
 }
 
 

src/libgens/MD/EmuMD.cpp
@@ -82,47 +82,36 @@ void EmuMD::End(void)
 
 
 /**
- * EmuMD::Init_TEST(): Test function for the MD VDP.
+ * EmuMD::SetRom(): Set the ROM image for MD emulation.
+ * NOTE: This function resets the emulated system!
+ * @param rom Rom class with the ROM image.
+ * @return 0 on success; non-zero on error.
  */
-void EmuMD::Init_TEST(void)
+int EmuMD::SetRom(Rom *rom)
 {
-	// Load the ROM file.
-	FILE *f = fopen("test.bin", "rb");
-	if (!f)
+	// Check the ROM size.
+	if ((rom->romSize() == 0) || (rom->romSize() > sizeof(M68K_Mem::Rom_Data)))
 	{
-		printf("Could not open test.bin.\n");
-		return;
+		// ROM is either empty or too big.
+		// TODO: Error code constants.
+		return 1;
 	}
 	
-	// Determine the size of the file.
-	fseek(f, 0, SEEK_END);
-	M68K_Mem::Rom_Size = ftell(f);
-	if (M68K_Mem::Rom_Size > sizeof(M68K_Mem::Rom_Data))
-		M68K_Mem::Rom_Size = sizeof(M68K_Mem::Rom_Data);
-	fseek(f, 0, SEEK_SET);
-	
 	// Load the ROM into memory.
-	fread(&M68K_Mem::Rom_Data.u8[0], 1, M68K_Mem::Rom_Size, f);
-	fclose(f);
+	M68K_Mem::Rom_Size = rom->romSize();
+	rom->loadRom(&M68K_Mem::Rom_Data.u8[0], M68K_Mem::Rom_Size);
 	
 	// Enable SRam/EEPRom by default.
 	// TODO: Make accessor/mutator functions.
 	M68K_Mem::SaveDataEnable = true;
 	
 	// Initialize SRam.
-	// TODO: Split into a separate function.
-	// TODO: Port Gens/GS's SRam initialization code to Gens/GS II.
-	// TODO: Add a function e.g. M68K_Mem::Reset() to reset all memory handling.
-	M68K_Mem::m_SRam.reset();
-	M68K_Mem::m_SRam.setStart(0x200000);
-	M68K_Mem::m_SRam.setEnd(0x20FFFF);
+	rom->initSRam(&M68K_Mem::m_SRam);
 	
 	// Initialize EEPRom.
 	// EEPRom is only used if the ROM is in the EEPRom class's database.
 	// Otherwise, SRam is used.
-	M68K_Mem::m_EEPRom.reset();
-	int eprType = EEPRom::DetectEEPRomType(&M68K_Mem::Rom_Data.u8[0]);
-	M68K_Mem::m_EEPRom.setEEPRomType(eprType);
+	rom->initEEPRom(&M68K_Mem::m_EEPRom);
 	
 	// TODO: Byteswapping flags.
 	// Until they're implemented, byteswap the ROM *after* initializing SRam/EEPRom.

src/libgens/MD/EmuMD.hpp
@@ -28,6 +28,9 @@
 // TODO: Figure out a better place to put these!
 #include "../IO/IoBase.hpp"
 
+// ROM image class.
+#include "../Rom.hpp"
+
 namespace LibGens
 {
 
@@ -37,7 +40,7 @@ class EmuMD
 		static void Init(void);
 		static void End(void);
 		
-		static void Init_TEST(void);
+		static int SetRom(Rom *rom);
 		
 		static void Do_Frame(void);
 		

src/libgens/Rom.hpp
@@ -42,6 +42,7 @@
 
 // LibGens includes.
 #include "Save/SRam.hpp"
+#include "Save/EEPRom.hpp"
 
 /**
  * ROM_HEADER_SIZE: Number of bytes used for ROM type detection.
@@ -92,13 +93,18 @@ class Rom
 		Rom(const char *filename, MDP_SYSTEM_ID sysOverride = MDP_SYSTEM_UNKNOWN, RomFormat fmtOverride = RFMT_UNKNOWN);
 		~Rom();
 		
-		bool isOpen(void) { return (m_file != NULL); }
+		bool isOpen(void) const { return (m_file != NULL); }
 		void close(void) { fclose(m_file); m_file = NULL; }
 		
-		MDP_SYSTEM_ID sysId(void) const { return m_sysId; }
-		RomFormat romFormat(void) const { return m_romFormat; }
+		inline MDP_SYSTEM_ID sysId(void) const { return m_sysId; }
+		inline RomFormat romFormat(void) const { return m_romFormat; }
 		
-		int initSRam(SRam *sram);
+		inline int romSize(void) const { return m_romSize; }
+		
+		int initSRam(SRam *sram) const;
+		int initEEPRom(EEPRom *eeprom) const;
+		
+		int loadRom(void *buf, size_t siz);
 	
 	protected:
 		MDP_SYSTEM_ID m_sysId;
@@ -121,6 +127,7 @@ class Rom
 		
 		std::string m_filename;		// ROM filename.
 		unsigned int m_romSize;		// ROM size.
+		int m_eprType;			// EEPRom type.
 		
 		/**
 		 * MD_RomHeader: ROM header. (MD-style)

commit c8dbcffe3bfd36d15c529d771a6528e38ab276c1

[libgens] Rom.cpp, Save/EEPRom.cpp: Fixed EEPRom detection.
This is a regression from commit e72112a387a1ec997e34ea9ff654e578815de42f.

Two bugs caused EEPRom detection to fail:

- EEProm::DetectEEPRomType(): We were specifying the length of the
  input serial number instead of the database serial number, which
  would cause a failure if the database serial number was not the
  exact same size as the input serial number (which is all the time).

- Rom::Rom(): The full serial number was being passed to
  EEProm::DetectEEPRomType() instead of just the part after "GM ",
  which obviously caused it to fail. The correct position is now
  being passed to the EEPRom detection function.

EEPRom works again.

TODO: Implement SRam/EEPRom saving.

src/libgens/Rom.cpp
@@ -73,7 +73,10 @@ Rom::Rom(const char *filename, MDP_SYSTEM_ID sysOverride, RomFormat fmtOverride)
 	readHeaderMD(header, header_size);
 	
 	// Detect the EEPRom type from the ROM serial number and checksum.
-	m_eprType = EEPRom::DetectEEPRomType(m_mdHeader.serialNumber, sizeof(m_mdHeader.serialNumber), m_mdHeader.checksum);
+	m_eprType = EEPRom::DetectEEPRomType(
+			&m_mdHeader.serialNumber[3],
+			(sizeof(m_mdHeader.serialNumber) - 3),
+			m_mdHeader.checksum);
 }
 
 Rom::~Rom()

src/libgens/Save/EEPRom.cpp
@@ -28,7 +28,6 @@
 
 // C includes.
 #include <string.h>
-#include <stdio.h>
 
 // TODO: Port SRam file handling functions from Gens/GS!
 
@@ -138,10 +137,10 @@ void EEPRom::reset(void)
 
 /**
  * DetectEEPRomType(): Detect the EEPRom type used by the specified ROM.
- * @param serial Serial number.
+ * @param serial Serial number. (NOTE: This does NOT include the "GM " prefix!)
  * @param serial_len Length of the serial number string.
  * @param checksum Checksum.
- * @return ROM type, or -1 if this ROM isn't known.
+ * @return EEPRom type, or -1 if this ROM isn't known.
  */
 int EEPRom::DetectEEPRomType(const char *serial, size_t serial_len, uint16_t checksum)
 {
@@ -157,7 +156,7 @@ int EEPRom::DetectEEPRomType(const char *serial, size_t serial_len, uint16_t che
 			continue;
 		}
 		
-		if (!memcmp(serial, ms_Database[i].game_id, serial_len))
+		if (!memcmp(serial, ms_Database[i].game_id, dbSerial_len))
 		{
 			// Serial number matches.
 			if (ms_Database[i].checksum == 0)

src/libgens/Save/EEPRom.hpp
@@ -42,7 +42,13 @@ class EEPRom
 		
 		void reset(void);
 		
-		/** EEPROM type detection. **/
+		/**
+		 * DetectEEPRomType(): Detect the EEPRom type used by the specified ROM.
+		 * @param serial Serial number. (NOTE: This does NOT include the "GM " prefix!)
+		 * @param serial_len Length of the serial number string.
+		 * @param checksum Checksum.
+		 * @return EEPRom type, or -1 if this ROM isn't known.
+		 */
 		static int DetectEEPRomType(const char *serial, size_t serial_len, uint16_t checksum);
 		
 		/**