9df0f0df08cc09cdc690cd5ab9cde3d53090b098
David Korth <gerbilsoft@verizon.net>
2010-08-16 00:47:33 -0400

    [libgens] credits.c: Initial Gens/GS II credits listing.
    I counted 30 contributors just for Gens/GS and Gens/GS II alone!
    That plus me, the original Gens programmer and the previous multiplatform
    Gens ports adds up to 36 total.
    
    If anyone's missing or has an incorrect description, let me know.

---
+++ src/libgens/credits.c
@@ -0,0 +1,71 @@
+/***************************************************************************
+ * libgens: Gens Emulation Library.                                        *
+ * credits.c: Gens/GS II credits.                                          *
+ *                                                                         *
+ * Copyright (c) 1999-2002 by Stéphane Dallongeville.                      *
+ * Copyright (c) 2003-2004 by Stéphane Akhoun.                             *
+ * Copyright (c) 2008-2010 by David Korth.                                 *
+ *                                                                         *
+ * This program is free software; you can redistribute it and/or modify it *
+ * under the terms of the GNU General Public License as published by the   *
+ * Free Software Foundation; either version 2 of the License, or (at your  *
+ * option) any later version.                                              *
+ *                                                                         *
+ * This program is distributed in the hope that it will be useful, but     *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ * GNU General Public License for more details.                            *
+ *                                                                         *
+ * You should have received a copy of the GNU General Public License along *
+ * with this program; if not, write to the Free Software Foundation, Inc., *
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.           *
+ ***************************************************************************/
+
+#include "credits.h"
+
+const utf8_str *GensGS_credits =
+{
+	"Main programmer: David Korth\n"
+	"\n"
+	"Original Gens programmer: Stéphane Dallongeville\n"
+	"Gens BeOS port: Caz\n"
+	"Gens/Linux port: Stéphane Akhoun\n"
+	"\n"
+	"Gens/Linux contributors:\n"
+	"Lester Barrows (aryarya)\n"
+	"El Pelos (wah wah 69)\n"
+	"\n"
+	"Gens/GS contributors: (alphabetical order)\n"
+	"AamirM: Software testing, MD emulation assistance\n"
+	"Baffo32: Software testing\n"
+	"Biafra Republic: Software testing\n"
+	"Chilly Willy: 32X PWM improvements\n"
+	"Corner-Face-Jacks: Software testing\n"
+	"Delta: Software testing\n"
+	"djohe: Software testing\n"
+	"Glitch: Software testing\n"
+	"Jorge: Provided test ROMs, MD emulation assistance\n"
+	"Joseph Fenton: Added SH2 DMA support for PWM audio\n"
+	"Marc Gordon (Cinossu): Provided the Gens/GS icon\n"
+	"Maximal|Firestorm: Provided icon for Game Genie\n"
+	"mister_k81: Software testing\n"
+	"NeKit: Software testing\n"
+	"Nicolas Bondoux: Software testing\n"
+	"Pietro Gagliardi (andlabs): Mac OS X testing\n"
+	"Phil Costin: Haiku port (in progress)\n"
+	"Puto: Mac OS X testing\n"
+	"segaloco: Software testing\n"
+	"sonicblur: Mac OS X testing\n"
+	"tetsuo: Software testing\n"
+	"theocas: Provided controller icons\n"
+	"Scarred Sun: Software testing\n"
+	"SOTI: Software testing\n"
+	"Sik: Provided test ROMs and icon for SGens\n"
+	"SkyLights: Provided icon for VDP Layer Options\n"
+	"SonicAD: Software testing\n"
+	"superGear: Software testing\n"
+	"Tets: Software testing\n"
+	"TiZ: Software testing\n"
+	"TmEE: MD hardware testing, emulation assistance\n"
+	"Zombie Ryushu: Software testing; WahCade integration"
+};
---
+++ src/libgens/credits.h
@@ -0,0 +1,39 @@
+/***************************************************************************
+ * libgens: Gens Emulation Library.                                        *
+ * credits.h: Gens/GS II credits.                                          *
+ *                                                                         *
+ * Copyright (c) 1999-2002 by Stéphane Dallongeville.                      *
+ * Copyright (c) 2003-2004 by Stéphane Akhoun.                             *
+ * Copyright (c) 2008-2010 by David Korth.                                 *
+ *                                                                         *
+ * This program is free software; you can redistribute it and/or modify it *
+ * under the terms of the GNU General Public License as published by the   *
+ * Free Software Foundation; either version 2 of the License, or (at your  *
+ * option) any later version.                                              *
+ *                                                                         *
+ * This program is distributed in the hope that it will be useful, but     *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ * GNU General Public License for more details.                            *
+ *                                                                         *
+ * You should have received a copy of the GNU General Public License along *
+ * with this program; if not, write to the Free Software Foundation, Inc., *
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.           *
+ ***************************************************************************/
+
+#ifndef __LIBGENS_CREDITS_H__
+#define __LIBGENS_CREDITS_H__
+
+#include "macros/common.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern const utf8_str *GensGS_credits;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __LIBGENS_CREDITS_H__ */
