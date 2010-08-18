GerbilSoft/gens-gs-ii

commit 04b3f6c2542c0e59f0f8ffe6c3d497e8418f0741

[gens-qt4] GensPortAudio::write(): Use pointer arithmetic instead of indexed addressing.
This provides an even bigger performance improvement than just switching
to memset() for zeroing.

           +----------+----------+
           |    -O0   |    -O2   |
+----------+----------+----------+
| memset() | 1.344305 | 0.775117 |
| ptr diff | 1.151114 | 0.367444 |
| % diff   |  -14.37% |  -52.60% |
+----------+----------+----------+

When compared to the original function:

           +----------+----------+
           |    -O0   |    -O2   |
+----------+----------+----------+
| Original | 1.461239 | 0.862154 |
| ptr diff | 1.151114 | 0.367444 |
| % diff   |  -21.22% |  -57.38% |
+----------+----------+----------+

The original version was really just a thrown-together version written
in order to get things working, so that's probably why it wasn't as fast
as it could have been.

src/gens-qt4/Audio/ABackend_Write.cpp
@@ -63,25 +63,29 @@ int GensPortAudio::write(void)
 	
 	// TODO: Mono/stereo, MMX, etc.
 	unsigned int i = 0;
-	unsigned int bufIndex = m_bufPos;
-	for (; i < SegLength && bufIndex < m_bufLen; i++, bufIndex++)
+	
+	// Destination buffer pointer.
+	// NOTE: We're treating it as a single interleaved stream.
+	int16_t *dest = (int16_t*)&m_buf[0][0];
+	
+	for (; i < SegLength; i++, dest += 2)
 	{
 		int32_t L = LibGens::SoundMgr::ms_SegBufL[i];
 		int32_t R = LibGens::SoundMgr::ms_SegBufR[i];
 		
 		if (L < -0x8000)
-			m_buf[bufIndex][0] = -0x8000;
+			*dest = -0x8000;
 		else if (L > 0x7FFF)
-			m_buf[bufIndex][0] = 0x7FFF;
+			*dest = 0x7FFF;
 		else
-			m_buf[bufIndex][0] = (int16_t)L;
+			*dest = (int16_t)L;
 		
 		if (R < -0x8000)
-			m_buf[bufIndex][1] = -0x8000;
+			*(dest+1) = -0x8000;
 		else if (R > 0x7FFF)
-			m_buf[bufIndex][1] = 0x7FFF;
+			*(dest+1) = 0x7FFF;
 		else
-			m_buf[bufIndex][1] = (int16_t)R;
+			*(dest+1) = (int16_t)R;
 	}
 	
 	// Clear the segment buffers.

commit f840da5546fffadd5528b72e7a4496d7597efd43

[gens-qt4] GensPortAudio::writeStereo(): Fixed an error in buffer positioning.
This is a regression from commit 04b3f6c2542c0e59f0f8ffe6c3d497e8418f0741.

In that commit, write() [now writeStereo()] was rewritten to use pointer
arithmetic instead of array indexing for audio writes, but I accidentally
removed the starting position calculation for m_buf. Hence, all segments
were written to the beginning of the buffer instead of the end of the buffer,
resulting in stuttering.

src/gens-qt4/Audio/ABackend_Write.cpp
@@ -80,7 +80,7 @@ int GensPortAudio::writeStereo(void)
 	unsigned int i = 0;
 	
 	// Destination buffer pointer.
-	int16_t *dest = (int16_t*)&m_buf[0];
+	int16_t *dest = (int16_t*)&m_buf[(m_bufPos * m_sampleSize) / sizeof(m_buf[0])];
 	
 	const int SegLength = LibGens::SoundMgr::GetSegLength();
 	for (; i < SegLength; i++, dest += 2)