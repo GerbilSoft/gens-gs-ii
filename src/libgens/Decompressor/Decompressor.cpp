a48dcc36e35b426d290dbb108e1e89eb5fd8c531
David Korth <gerbilsoft@verizon.net>
2010-08-21 15:49:03 -0400

    [libgens] Decompressor/Decompressor.cpp: Added mdp_z_entry_t support and associated functions.
    Decompressor::getFileInfo() returns a single-entry list with the
    filename of the loaded file and its filesize.
    
    Marked getFileInfo() and getFile() as virtual functions, since they should
    be overridden by derived classes, e.g. GZip and Zip decompressors.

--- src/libgens/Decompressor/Decompressor.cpp
+++ src/libgens/Decompressor/Decompressor.cpp
@@ -23,17 +23,27 @@
 
 #include "Decompressor.hpp"
 
+// C includes.
+#include <stdlib.h>
+#include <string.h>
+
+// C++ includes.
+#include <string>
+using std::string;
+
 namespace LibGens
 {
 
 /**
  * Decompressor(): Create a new Decompressor object.
  * @param f File pointer.
+ * @param filename Filename.
  */
-Decompressor::Decompressor(FILE *f)
+Decompressor::Decompressor(FILE *f, const utf8_str *filename)
 {
-	// Keep a copy of the file pointer.
+	// Keep copies of the file pointer and filename.
 	m_file = f;
+	m_filename = string(filename);
 }
 
 /**
@@ -47,6 +57,37 @@ Decompressor::~Decompressor()
 
 
 /**
+ * getFileInfo(): Get information about all files in the archive.
+ * @param z_entry_out Pointer to mdp_z_entry_t*, which will contain an allocated mdp_z_entry_t.
+ * @return MDP error code. [TODO]
+ */
+int Decompressor::getFileInfo(mdp_z_entry_t **z_entry_out)
+{
+	// Make sure a pointer to mdp_z_entry_t* was given.
+	if (!z_entry_out)
+		return -1; // TODO: return -MDP_ERR_INVALID_PARAMETERS;
+	
+	// TODO: fseeko()/ftello()/fseeko64()/ftello64() support on Linux.
+	size_t filesize;
+	fseek(m_file, 0, SEEK_END);
+	filesize = ftell(m_file);
+	
+	// Allocate an mdp_z_entry_t.
+	// NOTE: C-style malloc() is used because MDP is a C API.
+	mdp_z_entry_t *z_entry = (mdp_z_entry_t*)malloc(sizeof(mdp_z_entry_t));
+	
+	// Set the elements of the list entry.
+	z_entry->filesize = filesize;
+	z_entry->filename = (m_filename.size() > 0 ? strdup(m_filename.c_str()) : NULL);
+	z_entry->next = NULL;
+	
+	// Return the list.
+	*z_entry_out = z_entry;
+	return 0; // TODO: return MDP_ERR_OK;
+}
+
+
+/**
  * getFile(): Get a file from the archive.
  * @param z_entry	[in]  Pointer to mdp_z_entry_t describing the file to extract. [TODO]
  * @param buf		[out] Buffer to read the file into.
@@ -54,14 +95,42 @@ Decompressor::~Decompressor()
  * @param ret_siz	[out] Pointer to size_t to store the number of bytes read.
  * @return MDP error code. [TODO]
  */
-int Decompressor::getFile(const void *z_entry, void *buf, size_t siz, size_t *ret_siz)
+int Decompressor::getFile(const mdp_z_entry_t *z_entry, void *buf, size_t siz, size_t *ret_siz)
 {
+	// Make sure all parameters are specified.
+	if (!z_entry || !buf || !siz || !ret_siz)
+		return -1; // TODO: return -MDP_ERR_INVALID_PARAMETERS;
+	
+	// Make sure the file is open.
+	if (!m_file)
+		return -2; // TODO: return -MDP_ERR_INVALID_PARAMETERS;
+	
 	// Seek to the beginning of the file.
-	fseeko(m_file, 0, SEEK_SET);
+	// TODO: fseeko()/fseeko64() support on Linux.
+	fseek(m_file, 0, SEEK_SET);
 	
 	// Read the file into the buffer.
 	*ret_siz = fread(buf, 1, siz, m_file);
-	return 0;
+	return 0; // TODO: return MDP_ERR_OK;
+}
+
+
+/**
+ * z_entry_t_free(): Free an allocated mdp_z_entry_t list.
+ * @param z_entry Pointer to the first entry in the list.
+ */
+void Decompressor::z_entry_t_free(mdp_z_entry_t *z_entry)
+{
+	// Delete the mdp_z_entry_t list.
+	for (mdp_z_entry_t *next; z_entry != NULL; z_entry = next)
+	{
+		// Save the next pointer.
+		next = z_entry->next;
+		
+		// Free the filename and the mdp_z_entry_t.
+		free(z_entry->filename);
+		free(z_entry);
+	}
 }
 
 }
--- src/libgens/Decompressor/Decompressor.hpp
+++ src/libgens/Decompressor/Decompressor.hpp
@@ -24,8 +24,32 @@
 #ifndef __LIBGENS_DECOMPRESSOR_DECOMPRESSOR_HPP__
 #define __LIBGENS_DECOMPRESSOR_DECOMPRESSOR_HPP__
 
+#include "../macros/common.h"
+
 // C includes.
 #include <stdio.h>
+
+// TODO: Use the MDP headers for mdp_z_entry_t.
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#pragma pack(1)
+typedef struct PACKED _mdp_z_entry_t
+{
+	char *filename;
+	size_t filesize;
+	
+	struct _mdp_z_entry_t *next;
+} mdp_z_entry_t;
+#pragma pack()
+
+#ifdef __cplusplus
+}
+#endif
+
+// C++ includes.
+#include <string>
 
 namespace LibGens
 {
@@ -33,7 +57,7 @@ namespace LibGens
 class Decompressor
 {
 	public:
-		Decompressor(FILE *f);
+		Decompressor(FILE *f, const utf8_str *filename);
 		~Decompressor();
 		
 		/**
@@ -46,6 +70,13 @@ class Decompressor
 		static bool DetectFormat(FILE *f) { return (f ? true : false); }
 		
 		/**
+		 * getFileInfo(): Get information about all files in the archive.
+		 * @param z_entry_out Pointer to mdp_z_entry_t*, which will contain an allocated mdp_z_entry_t.
+		 * @return MDP error code. [TODO]
+		 */
+		virtual int getFileInfo(mdp_z_entry_t **z_entry_out);
+		
+		/**
 		 * getFile(): Get a file from the archive.
 		 * @param z_entry	[in]  Pointer to mdp_z_entry_t describing the file to extract. [TODO]
 		 * @param buf		[out] Buffer to read the file into.
@@ -53,10 +84,17 @@ class Decompressor
 		 * @param ret_siz	[out] Pointer to size_t to store the number of bytes read.
 		 * @return MDP error code. [TODO]
 		 */
-		int getFile(const void *z_entry, void *buf, size_t siz, size_t *ret_siz);
+		virtual int getFile(const mdp_z_entry_t *z_entry, void *buf, size_t siz, size_t *ret_siz);
+		
+		/**
+		 * z_entry_t_free(): Free an allocated mdp_z_entry_t list.
+		 * @param z_entry Pointer to the first entry in the list.
+		 */
+		static void z_entry_t_free(mdp_z_entry_t *z_entry);
 	
 	protected:
 		FILE *m_file;
+		std::string m_filename;
 };
 
 }
