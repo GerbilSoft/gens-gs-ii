747a0e450a045ccbcb7c964e8c7ec6f6f7799028
David Korth <gerbilsoft@verizon.net>
2010-08-22 22:34:54 -0400

    [libgens] Decompressor/DcRar.cpp: Ported the Unix Rar decompressor from Gens/GS.
    Like Gens/GS, this decompressor requires a copy of the Unix unrar (or rar)
    binary installed on the system. It currently defaults to /usr/bin/unrar,
    but the path will be customizable later on.
    
    DcRar::getFile(): Fixed a bug that caused a buffer overflow if the requested
    filesize is a multiple of the 4,096-byte RAR buffer (or a multiple of 4,096
    plus 0-2,047 bytes or something like that) and isn't the full file. I didn't
    notice this in Gens/GS, since the ROM format detection buffer was 2,048 bytes,
    but in Gens/GS II, the ROM format detectino buffer is 65,536 bytes, which
    triggered the buffer overflow.

---
+++ src/libgens/Decompressor/DcRar.hpp
@@ -0,0 +1,90 @@
+/***************************************************************************
+ * libgens: Gens Emulation Library.                                        *
+ * DcRar.hpp: RAR decompressor class.                                      *
+ *                                                                         *
+ * Copyright (c) 1999-2002 by Stéphane Dallongeville.                      *
+ * Copyright (c) 2003-2004 by Stéphane Akhoun.                             *
+ * Copyright (c) 2008-2010 by David Korth.                                 *
+ *                                                                         *
+ * This program is free software; you can redistribute it and/or modify it *
+ * under the terms of the GNU General Public License as published by the   *
+ * Free Software Foundation; either version 2 of the License, or (at your  *
+ * option) any later version.                                              *
+ *                                                                         *
+ * This program is distributed in the hope that it will be useful, but     *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ * GNU General Public License for more details.                            *
+ *                                                                         *
+ * You should have received a copy of the GNU General Public License along *
+ * with this program; if not, write to the Free Software Foundation, Inc., *
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.           *
+ ***************************************************************************/
+
+#ifndef __LIBGENS_DECOMPRESSOR_DCRAR_HPP__
+#define __LIBGENS_DECOMPRESSOR_DCRAR_HPP__
+
+#include "Decompressor.hpp"
+
+#ifdef _WIN32
+#include "UnRAR_dll.hpp"
+#endif
+
+// C++ includes.
+#include <string>
+
+namespace LibGens
+{
+
+class DcRar : public Decompressor
+{
+	public:
+		DcRar(FILE *f, const utf8_str *filename);
+		~DcRar();
+		
+		/**
+		 * DetectFormat(): Detect if the file can be handled by this decompressor.
+		 * This function should be reimplemented by derived classes.
+		 * NOTE: Do NOT call this function like a virtual function!
+		 * @param f File pointer.
+		 * @return True if the file can be handled by this decompressor.
+		 */
+		static bool DetectFormat(FILE *f);
+		
+		/**
+		 * getFileInfo(): Get information about all files in the archive.
+		 * @param z_entry_out Pointer to mdp_z_entry_t*, which will contain an allocated mdp_z_entry_t.
+		 * @return MDP error code. [TODO]
+		 */
+		int getFileInfo(mdp_z_entry_t **z_entry_out);
+		
+		/**
+		 * getFile(): Get a file from the archive.
+		 * @param z_entry	[in]  Pointer to mdp_z_entry_t describing the file to extract.
+		 * @param buf		[out] Buffer to read the file into.
+		 * @param siz		[in]  Size of buf.
+		 * @param ret_siz	[out] Pointer to size_t to store the number of bytes read.
+		 * @return MDP error code. [TODO]
+		 */
+		int getFile(const mdp_z_entry_t *z_entry, void *buf, size_t siz, size_t *ret_siz);
+	
+	protected:
+		/**
+		 * ms_RarBinary: RAR executable filename.
+		 * Unix: Filename of "rar" or "unrar".
+		 * Windows: Filename of "unrar.dll".
+		 */
+		static std::string ms_RarBinary;
+		
+#ifdef _WIN32
+		/**
+		 * m_unrarDll: UnRAR DLL.
+		 * Win32 only!
+		 */
+		UnRAR_dll *m_unrarDll;
+#endif
+};
+
+}
+
+#endif /* __LIBGENS_DECOMPRESSOR_DECOMPRESSOR_HPP__ */
---
+++ src/libgens/Decompressor/DcRar_Unix.cpp
@@ -0,0 +1,347 @@
+/***************************************************************************
+ * libgens: Gens Emulation Library.                                        *
+ * DcRar_Unix.cpp: RAR decompressor class. (Unix version)                  *
+ *                                                                         *
+ * Copyright (c) 1999-2002 by Stéphane Dallongeville.                      *
+ * Copyright (c) 2003-2004 by Stéphane Akhoun.                             *
+ * Copyright (c) 2008-2010 by David Korth.                                 *
+ *                                                                         *
+ * This program is free software; you can redistribute it and/or modify it *
+ * under the terms of the GNU General Public License as published by the   *
+ * Free Software Foundation; either version 2 of the License, or (at your  *
+ * option) any later version.                                              *
+ *                                                                         *
+ * This program is distributed in the hope that it will be useful, but     *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ * GNU General Public License for more details.                            *
+ *                                                                         *
+ * You should have received a copy of the GNU General Public License along *
+ * with this program; if not, write to the Free Software Foundation, Inc., *
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.           *
+ ***************************************************************************/
+
+#include "DcRar.hpp"
+
+// C includes.
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdint.h>
+
+// LOG_MSG() subsystem.
+#include "macros/log_msg.h"
+
+// C++ includes.
+#include <string>
+#include <sstream>
+using std::string;
+using std::stringstream;
+
+namespace LibGens
+{
+
+/** Static class variable initialization. **/
+
+/**
+ * ms_RarBinary: RAR executable filename.
+ * Unix: Filename of "rar" or "unrar".
+ * Windows: Filename of "unrar.dll".
+ */
+string DcRar::ms_RarBinary = "/usr/bin/unrar";
+
+
+/**
+ * DcRar(): Create a new DcRar object.
+ * @param f File pointer.
+ * @param filename Filename.
+ */
+DcRar::DcRar(FILE *f, const utf8_str *filename)
+	: Decompressor(f, filename)
+{
+	// MiniZip doesn't support opening files by fd.
+	if (!f || !filename)
+	{
+		// No filename specified.
+		m_file = NULL;
+		m_filename.clear();
+		return;
+	}
+	
+	// TODO: Check that the RAR binary filename is valid.
+}
+
+/**
+ * ~DcRar(): Delete the RAR Decompressor object.
+ */
+DcRar::~DcRar()
+{
+	// TODO
+}
+
+
+/**
+ * DetectFormat(): Detect if the file can be handled by this decompressor.
+ * This function should be reimplemented by derived classes.
+ * NOTE: Do NOT call this function like a virtual function!
+ * @param f File pointer.
+ * @return True if the file can be handled by this decompressor.
+ */
+bool DcRar::DetectFormat(FILE *f)
+{
+	static const uint8_t rar_magic[] = {'R', 'a', 'r', '!'};
+	
+	// Seek to the beginning of the file.
+	// TODO: fseeko()/fseeko64() support on Linux.
+	fseek(f, 0, SEEK_SET);
+	
+	// Read the "magic number".
+	uint8_t header[sizeof(rar_magic)];
+	size_t ret = fread(&header, 1, sizeof(header), f);
+	if (ret < sizeof(header))
+	{
+		// Error reading the "magic number".
+		return false;
+	}
+	
+	// Check the "magic number" and return true if it matches.
+	return (!memcmp(header, rar_magic, sizeof(header)));
+}
+
+
+/**
+ * getFileInfo(): Get information about all files in the archive.
+ * @param z_entry_out Pointer to mdp_z_entry_t*, which will contain an allocated mdp_z_entry_t.
+ * @return MDP error code. [TODO]
+ */
+int DcRar::getFileInfo(mdp_z_entry_t **z_entry_out)
+{
+	// Make sure a pointer to mdp_z_entry_t* was given.
+	if (!z_entry_out)
+		return -1; // TODO: return -MDP_ERR_INVALID_PARAMETERS;
+	
+	// Make sure the file is open.
+	if (!m_file)
+		return -2;
+	
+	// Check that the RAR executable is available.
+	if (access(ms_RarBinary.c_str(), X_OK) != 0)
+	{
+		// Cannot run the RAR executable.
+		return -3; // TODO: return -MDP_ERR_Z_EXE_NOT_FOUND;
+	}
+	
+	// Build the command line.
+	string sCmdLine = "\"" + ms_RarBinary + "\" v \"" + m_filename + "\"";
+	
+	// Open the RAR file.
+	FILE *pRar = popen(sCmdLine.c_str(), "r");
+	if (!pRar)
+	{
+		// Error running the RAR executable.
+		return -4; // TODO: return -MDP_ERR_Z_EXE_NOT_FOUND;
+	}
+	
+	// Read from the pipe.
+	char buf[4096+1];
+	size_t rv;
+	stringstream ss;
+	while ((rv = fread(buf, 1, sizeof(buf)-1, pRar)))
+	{
+		buf[rv] = 0x00;
+		ss << buf;
+	}
+	
+	// Close the RAR file.
+	pclose(pRar);
+	
+	// Get the string and go through it to get the file listing.
+	const string &data = ss.str();
+	
+	// Find the "---", which indicates the start of the file listing.
+	size_t listStart = data.find("---");
+	if (listStart == string::npos)
+	{
+		// Not found. Either there are no files, or the archive is broken.
+		return -5; // TODO: return -MDP_ERR_Z_NO_FILES_IN_ARCHIVE;
+	}
+	
+	// Find the newline after the list start.
+	size_t listStartLF = data.find('\n', listStart);
+	if (listStart == string::npos)
+	{
+		// Not found. Either there are no files, or the archive is broken.
+		return -5; // TODO: return -MDP_ERR_Z_NO_FILES_IN_ARCHIVE;
+	}
+	
+	// List head and tail.
+	mdp_z_entry_t *z_entry_head = NULL;
+	mdp_z_entry_t *z_entry_tail = NULL;
+	
+	// Parse all lines until we hit another "---" (or EOF).
+	size_t curStartPos = listStartLF + 1; // (+ 1) to skip the newline.
+	size_t curEndPos;
+	string curLine;
+	bool endOfRar = false;
+	
+	// Temporary data.
+	string tmp_filename;
+	size_t tmp_filesize;
+	
+	while (!endOfRar)
+	{
+		curEndPos = data.find('\n', curStartPos);
+		if (curEndPos == string::npos)
+		{
+			// End of file listing.
+			break;
+		}
+		
+		// Get the current line.
+		curLine = data.substr(curStartPos, (curEndPos - curStartPos));
+		
+		// First line in a RAR file listing is the filename.
+		// (starting at the second character)
+		if (curLine.size() < 2)
+			break;
+		if (curLine.at(0) == '-')
+		{
+			// End of file listing.
+			break;
+		}
+		
+		tmp_filename = curLine.substr(1);
+		
+		// Get the second line, which contains the filesize and filetype.
+		curStartPos = curEndPos + 1; // (+ 1) to skip the newline.
+		curEndPos = data.find('\n', curStartPos);
+		if (curEndPos == string::npos)
+		{
+			// End of file listing.
+			break;
+		}
+		
+		// Get the current line.
+		curLine = data.substr(curStartPos, (curEndPos - curStartPos));
+		
+		// Check if this is a normal file.
+		if (curLine.size() < 62)
+			break;
+		
+		// Normal file.
+		tmp_filesize = atoi(curLine.substr(12, 10).c_str());
+		
+		// Allocate memory for the next file list element.
+		// NOTE: C-style malloc() is used because MDP is a C API.
+		mdp_z_entry_t *z_entry_cur = (mdp_z_entry_t*)malloc(sizeof(mdp_z_entry_t));
+		
+		// Store the ROM file information.
+		z_entry_cur->filename = strdup(tmp_filename.c_str());
+		z_entry_cur->filesize = tmp_filesize;
+		z_entry_cur->next = NULL;
+		
+		if (!z_entry_head)
+		{
+			// List hasn't been created yet. Create it.
+			z_entry_head = z_entry_cur;
+			z_entry_tail = z_entry_cur;
+		}
+		else
+		{
+			// Append the mdp_z_entry to the end of the list.
+			z_entry_tail->next = z_entry_cur;
+			z_entry_tail = z_entry_cur;
+		}
+		
+		// Go to the next file in the listing.
+		curStartPos = curEndPos + 1; // (+ 1) to skip the newline.
+	}
+	
+	// If there are no files in the archive, return an error.
+	if (!z_entry_head)
+		return -1; // TODO: return -MDP_ERR_Z_NO_FILES_IN_ARCHIVE;
+	
+	// Return the list of files.
+	*z_entry_out = z_entry_head;
+	return 0; // TODO: return MDP_ERR_OK;
+}
+
+
+/**
+ * getFile(): Get a file from the archive.
+ * @param z_entry	[in]  Pointer to mdp_z_entry_t describing the file to extract. [TODO]
+ * @param buf		[out] Buffer to read the file into.
+ * @param siz		[in]  Size of buf.
+ * @param ret_siz	[out] Pointer to size_t to store the number of bytes read.
+ * @return MDP error code. [TODO]
+ */
+int DcRar::getFile(const mdp_z_entry_t *z_entry, void *buf, size_t siz, size_t *ret_siz)
+{
+	// Make sure all parameters are specified.
+	if (!z_entry || !buf || !siz || !ret_siz)
+		return -1; // TODO: return -MDP_ERR_INVALID_PARAMETERS;
+	
+	// Make sure the file is open.
+	if (!m_file)
+		return -2; // TODO: return -MDP_ERR_INVALID_PARAMETERS;
+	
+	// Make sure the z_entry filename is valid.
+	if (!z_entry->filename)
+	{
+		// Invalid filename.
+		return -1; // TODO: Return an appropriate MDP error code.
+	}
+	
+	// Check that the RAR executable is available.
+	if (access(ms_RarBinary.c_str(), X_OK) != 0)
+	{
+		// Cannot run the RAR executable.
+		return -3; // TODO: return -MDP_ERR_Z_EXE_NOT_FOUND;
+	}
+	
+	// Build the command line.
+	string sCmdLine = "\"" + ms_RarBinary + "\" p -ierr \"" + m_filename +
+			"\" \"" + string(z_entry->filename) + "\" 2>/dev/null";
+	
+	// Open the RAR file.
+	FILE *pRar = popen(sCmdLine.c_str(), "r");
+	if (!pRar)
+	{
+		// Error running the RAR executable.
+		return -4; // TODO: return -MDP_ERR_Z_EXE_NOT_FOUND;
+	}
+	
+	// Read from the pipe.
+	size_t extracted_size = 0;
+	size_t rv;
+	uint8_t bufRar[4096];
+	uint8_t *buf8 = (uint8_t*)buf;
+	
+	while ((rv = fread(bufRar, 1, sizeof(bufRar), pRar)))
+	{
+		if (extracted_size + rv > siz)
+		{
+			// Do a partial copy.
+			rv = (siz - extracted_size);
+			if (rv > 0)
+			{
+				memcpy(&buf8[extracted_size], &bufRar, rv);
+				extracted_size += rv;
+			}
+			break;
+		}
+		
+		// Do a full copy.
+		memcpy(&buf8[extracted_size], &bufRar, rv);
+		extracted_size += rv;
+	}
+	
+	// Close the RAR file.
+	pclose(pRar);
+	
+	// File extracted successfully.
+	*ret_siz = extracted_size;
+	return 0; // TODO: return MDP_ERR_OK;
+}
+
+}
