ba60757ba68b238326c92972c7fec5585c1278c9
David Korth <gerbilsoft@verizon.net>
2010-12-18 09:28:14 -0500

    CMake/CheckSystemX8632.cmake: Forgot to add this in commit 1a8b524d901c9934d2fa572cc29fc93adc2a112d.

---
+++ CMake/CheckSystemX8632.cmake
@@ -0,0 +1,46 @@
+# - Check if the system is x86 (32-bit).
+# CHECK_SYSTEM_X86_32(VARIABLE)
+#
+#  VARIABLE - variable to store the result
+# 
+#  This actually calls the check_c_source_compiles macro.
+#  See help for CheckCSourceCompiles for a listing of variables
+#  that can modify the build.
+
+# Copyright (c) 2010, David Korth, <gerbilsoft@verizon.net>
+
+# Based on CHECK_C99_COMPILER_FLAG().
+# Copyright (c) 2006, Alexander Neundorf, <neundorf@kde.org>
+#
+# Redistribution and use is allowed according to the terms of the BSD license.
+# For details see the accompanying COPYING-CMAKE-SCRIPTS file.
+
+# Based on CHECK_C_COMPILER_FLAG(). (CheckCCompilerFlag.cmake)
+
+INCLUDE(CheckCSourceCompiles)
+
+MACRO(CHECK_SYSTEM_X86_32 _RESULT)
+	# Flag listing borrowed from GNU autoconf's AC_PROG_CC_C99 macro.
+	UNSET(${_RESULT})
+	
+	MESSAGE(STATUS "Checking if the system is X86_32:")
+	
+	CHECK_C_SOURCE_COMPILES(
+		"#if (defined(__i386) || defined(__i386__)) && (!defined(__x86_64) && !defined(__x86_64__) && !defined(__amd64) && !defined(__amd64__)
+		/* System is X86_32 */
+		#else
+		/* System is not X86_32 */
+		#error Not X86_32. Aborting.
+		#endif
+		
+		int main(void) { return 0; }" CHECK_X86_32)
+	
+	IF(CHECK_X86_32)
+		SET(${_RESULT} 1)
+		MESSAGE(STATUS "Checking if the system is X86_32: yes")
+	ELSE(CHECK_X86_32)
+		UNSET(${_RESULT})
+		MESSAGE(STATUS "Checking if the system is X86_32: no")
+	ENDIF(CHECK_X86_32)
+	UNSET(CHECK_X86_32)
+ENDMACRO (CHECK_SYSTEM_X86_32)
