349821eae7434cbced08cbdc0cec6bd96776bc73
David Korth <gerbilsoft@verizon.net>
2011-01-24 20:50:03 -0500

    [gens-qt4] EmuManager.cpp: #include <config.h>

--- src/gens-qt4/EmuManager.cpp
+++ src/gens-qt4/EmuManager.cpp
@@ -21,6 +21,8 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.           *
  ***************************************************************************/
 
+#include <config.h>
+
 #include "EmuManager.hpp"
 #include "gqt4_main.hpp"
 
