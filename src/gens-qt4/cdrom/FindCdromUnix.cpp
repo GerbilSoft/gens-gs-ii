1486e2da526a035e21c369e9b17afc7f08156bb2
David Korth <gerbilsoft@verizon.net>
2011-02-02 01:26:31 -0500

    [gens-qt4] cdrom/FindCdromUnix.cpp: Initial Unix CD-ROM detection class.
    This class will be used on Unix and Linux systems where UDisks
    isn't available. It probably won't be used on Mac OS X, since
    the device structure is completely different there.
    
    TODO:
    
    - Proper support for non-Linux systems (e.g. BSD).
    
    - Get device information other than the pathname.

---
+++ src/gens-qt4/cdrom/FindCdromUnix.cpp
@@ -0,0 +1,145 @@
+/***************************************************************************
+ * gens-qt4: Gens Qt4 UI.                                                  *
+ * FindCdromUnix.cpp: Find CD-ROM drives. (UNIX fallback)                  *
+ *                                                                         *
+ * Copyright (c) 2011 by David Korth.                                      *
+ *                                                                         *
+ * This program is free software; you can redistribute it and/or modify it *
+ * under the terms of the GNU General Public License as published by the   *
+ * Free Software Foundation; either version 2 of the License, or (at your  *
+ * option) any later version.                                              *
+ *                                                                         *
+ * This program is distributed in the hope that it will be useful, but     *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ * GNU General Public License for more details.                            *
+ *                                                                         *
+ * You should have received a copy of the GNU General Public License along *
+ * with this program; if not, write to the Free Software Foundation, Inc., *
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.           *
+ ***************************************************************************/
+
+#include "FindCdromUnix.hpp"
+
+// C includes.
+#include <stdio.h>
+
+// stat(2)
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+// Qt includes.
+#include <QtCore/qglobal.h>
+#include <QtCore/QList>
+#include <QtCore/QScopedPointer>
+#include <QtCore/QDir>
+#include <QtCore/QStringList>
+
+// Text translation macro.
+#include <QtCore/QCoreApplication>
+#define TR(text) \
+	QCoreApplication::translate("FindCdromUnix", (text), NULL, QCoreApplication::UnicodeUTF8)
+
+namespace GensQt4
+{
+
+const char *FindCdromUnix::ms_Unix_DevNames[] =
+{
+#ifdef Q_OS_LINUX
+	"cdrom*", "cdrw*", "dvd*", "dvdrw*", "sr*", "scd*", "hd*",
+#endif
+	
+	NULL
+};
+
+/**
+ * query_int(): Asynchronously query for CD-ROM drives. (INTERNAL FUNCTION)
+ * The driveUpdated() signal will be emitted once for each detected drive.
+ * @return 0 on success; non-zero on error.
+ */
+int FindCdromUnix::query_int(void)
+{
+	// Find all CD-ROM devices.
+	
+	// Open the /dev/ directory.
+	QDir dir("/dev/");
+	if (!dir.exists())
+		return -1;
+	
+	// Create the filters for the templates.
+	QStringList nameFilters;
+	for (const char **devName = &ms_Unix_DevNames[0];
+	     *devName != NULL; devName++)
+	{
+		nameFilters.append(QString::fromLatin1(*devName));
+	}
+	
+	// Get a list of filenames.
+	QFileInfoList devFiles = dir.entryInfoList(nameFilters, (QDir::NoSymLinks | QDir::System), QDir::Name);
+	
+	// Search for block devices that are readable by the user.
+	QFileInfo fileInfo;
+	QStringList devicesUsable;
+	foreach(fileInfo, devFiles)
+	{
+		printf("file: %s\n", fileInfo.absoluteFilePath().toLocal8Bit().constData());
+		if (fileInfo.isDir() || !fileInfo.isReadable())
+			continue;
+		
+		// Check if this is a block device file.
+		QString q_filename = QDir::toNativeSeparators(fileInfo.absoluteFilePath());
+		const char *c_filename = q_filename.toLocal8Bit().constData();
+		
+		struct stat st_devFile;
+		if (stat(c_filename, &st_devFile))
+			continue;
+		if (!S_ISBLK(st_devFile.st_mode))
+			continue;
+		
+		// This file is a block device.
+		devicesUsable.append(q_filename);
+	}
+	
+	// Get drive information.
+	// TODO: We're just getting the pathname right now.
+	QString devFilename;
+	foreach(devFilename, devicesUsable)
+	{
+		// Construct the CdromDriveEntry.
+		CdromDriveEntry drive;
+		drive.discs_supported = 0;
+		drive.drive_type = DRIVE_TYPE_NONE;
+		drive.disc_type = 0;
+		
+		// Get various properties.
+		// TODO: Get the properties.
+		drive.path		= devFilename;
+		drive.drive_vendor	= "TODO:";
+		drive.drive_model	= "Get drive info";
+		drive.drive_firmware	= "1.23";
+		drive.disc_label	= "Disc Label";
+		drive.disc_blank	= false;
+		
+		// TODO
+		drive.drive_type = DRIVE_TYPE_CDROM;
+		drive.discs_supported = DISC_TYPE_CDROM;
+		drive.disc_type = DISC_TYPE_CDROM;
+		
+		// TODO: Check if a disc is in the drive.
+		
+		// If the disc is blank, set the disc label to "Blank [disc_type]".
+		// TODO: Make this a common FindCdromBase function?
+		if (drive.disc_type != DISC_TYPE_NONE && drive.disc_blank)
+			drive.disc_label = TR("Blank %1").arg(GetDiscTypeName(drive.disc_type));
+		
+		// Emit the driveUpdated() signal for this drive.
+		// TODO: If scanning all drives, notify when all drives are scanned.
+		emit driveUpdated(drive);
+	}
+	
+	// Devices queried.
+	return 0;
+}
+
+}
---
+++ src/gens-qt4/cdrom/FindCdromUnix.hpp
@@ -0,0 +1,49 @@
+/***************************************************************************
+ * gens-qt4: Gens Qt4 UI.                                                  *
+ * FindCdromUnix.hpp: Find CD-ROM drives. (UNIX fallback)                  *
+ *                                                                         *
+ * Copyright (c) 2011 by David Korth.                                      *
+ *                                                                         *
+ * This program is free software; you can redistribute it and/or modify it *
+ * under the terms of the GNU General Public License as published by the   *
+ * Free Software Foundation; either version 2 of the License, or (at your  *
+ * option) any later version.                                              *
+ *                                                                         *
+ * This program is distributed in the hope that it will be useful, but     *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ * GNU General Public License for more details.                            *
+ *                                                                         *
+ * You should have received a copy of the GNU General Public License along *
+ * with this program; if not, write to the Free Software Foundation, Inc., *
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.           *
+ ***************************************************************************/
+
+#ifndef __GENS_QT4_CDROM_FINDCDROMUNIX_HPP__
+#define __GENS_QT4_CDROM_FINDCDROMUNIX_HPP__
+
+#include <config.h>
+
+#include "FindCdromBase.hpp"
+
+namespace GensQt4
+{
+
+class FindCdromUnix : public FindCdromBase
+{
+	Q_OBJECT
+	
+	protected:
+		static const char *ms_Unix_DevNames[];
+		
+		/**
+		 * query_int(): Asynchronously query for CD-ROM drives. (INTERNAL FUNCTION)
+		 * The driveUpdated() signal will be emitted once for each detected drive.
+		 * @return 0 on success; non-zero on error.
+		 */
+		int query_int(void);
+};
+
+}
+
+#endif /* __GENS_QT4_CDROM_FINDCDROMUNIX_HPP__ */
