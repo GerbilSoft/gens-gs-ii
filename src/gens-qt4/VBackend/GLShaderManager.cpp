GerbilSoft/gens-gs-ii

commit dbe676e65a052d984b7b56144f95865cc3cde70d

[gens-qt4] VBackend/GLShaderManager.cpp: Initial rework to support GL_ATI_text_fragment_shader.
The Paused effect has been ported to GL_ATI_text_fragment_shader.

Unfortunately, my test system for GL_ATI_text_fragment_shader
(Mac OS X 10.5.7 PowerPC) seems to be sampling textures
incorrectly when GL_ATI_text_fragment_shader is enabled, so
I'm going to disable it in the next commit.

src/gens-qt4/VBackend/GLShaderManager.cpp
@@ -36,27 +36,48 @@ namespace GensQt4
 /** Fragment programs. **/
 
 /**
- * ms_Paused_ARBfrag_asm(): Paused effect shader. (GL_ARB_fragment_program)
+ * ms_Paused_ARB_fragment_program_src: Paused effect shader.
+ * GL_ARB_fragment_program
  * Based on grayscale shader from http://arstechnica.com/civis/viewtopic.php?f=19&t=445912
  *
  * Grayscale vector: [0.299 0.587 0.114] (ITU-R BT.601)
  * Source: http://en.wikipedia.org/wiki/YCbCr
  */
-const char *GLShaderManager::ms_Paused_ARBfrag_asm =
+const char *GLShaderManager::ms_Paused_ARB_fragment_program_src =
 	"!!ARBfp1.0\n"
 	"OPTION ARB_precision_hint_fastest;\n"
 	"PARAM grayscale = {0.299, 0.587, 0.114, 0.0};\n"	// Standard RGB to Grayscale vector.
 	"TEMP t0, color;\n"
-	"TEX t0, fragment.texcoord[0], texture[0], 2D;\n"	// Get color coordinate.
+	"TEX t0, fragment.texcoord[0], texture[0], 2D;\n"	// Sample the texture.
 	"DP3 color, t0, grayscale;\n"				// Calculate grayscale value.
 	"ADD_SAT color.z, color.z, color.z;\n"			// Double the blue component.
 	"MOV result.color, color;\n"
 	"END\n";
 
 /**
- * ms_FastBlur_ARBfrag_asm(): Fast Blur effect shader. (GL_ARB_fragment_program)
+ * ms_Paused_ATI_text_fragment_shader_src(): Paused effect shader.
+ * GL_ATI_text_fragment_shader
+ * Based on grayscale shader from http://arstechnica.com/civis/viewtopic.php?f=19&t=445912
+ *
+ * Grayscale vector: [0.299 0.587 0.114] (ITU-R BT.601)
+ * Source: http://en.wikipedia.org/wiki/YCbCr
  */
-const char *GLShaderManager::ms_FastBlur_ARBfrag_asm =
+const char *GLShaderManager::ms_Paused_ATI_text_fragment_shader_src =
+	"!!ATIfs1.0\n"
+	"StartConstants;\n"
+	"  CONSTANT c0 = {0.299, 0.587, 0.114};\n"	// Standard RGB to Grayscale vector.
+	"EndConstants;\n"
+	"StartOutputPass;\n"
+	"  SampleMap r0, t0.str;\n"			// Sample the texture.
+	"  DOT3 r0.rgb, r0, c0;\n"			// Calculate grayscale value.
+	"  ADD r0.b.sat, r0.b, r0.b;\n"			// Double the blue component.
+	"EndPass;\n";
+
+/**
+ * ms_FastBlur_ARB_fragment_program_src(): Fast Blur effect shader.
+ * GL_ARB_fragment_program
+ */
+const char *GLShaderManager::ms_FastBlur_ARB_fragment_program_src =
 	"!!ARBfp1.0\n"
 	"OPTION ARB_precision_hint_fastest;\n"
 	"PARAM div2 = {0.5, 0.5, 0.5, 0.0};\n"			// Divide by 2 vector.
@@ -76,8 +97,16 @@ GLShaderManager::GLShaderManager()
 	m_init = false;
 	
 #ifdef HAVE_GLEW
-	m_paused_ARBfrag = 0;
+	// Initialize shader variables.
 	m_paused_enabled = false;
+	m_paused_type = ST_NONE;
+	
+	m_fastBlur_enabled = false;
+	m_fastBlur_type = ST_NONE;
+	
+	// Clear the shader pointers.
+	m_paused_ARB = 0;
+	m_fastBlur_ARB = 0;
 #endif
 }
 
@@ -102,41 +131,49 @@ void GLShaderManager::init(void)
 	// Check what extensions are supported.
 	if (GLEW_ARB_fragment_program || GLEW_VERSION_2_0)
 	{
-		// Fragment programs are supported.
+		// ARB fragment programs are supported.
 		// Load the fragment programs.
 		
 		// Load the Paused effect fragment program.
 		// TODO: Check for errors!
-		glGenProgramsARB(1, &m_paused_ARBfrag);
-		glBindProgramARB(GL_FRAGMENT_PROGRAM_ARB, m_paused_ARBfrag);
+		glGenProgramsARB(1, &m_paused_ARB);
+		glBindProgramARB(GL_FRAGMENT_PROGRAM_ARB, m_paused_ARB);
 		glGetError();	// Clear the error flag.
 		glProgramStringARB(GL_FRAGMENT_PROGRAM_ARB, GL_PROGRAM_FORMAT_ASCII_ARB,
-				   strlen(ms_Paused_ARBfrag_asm), ms_Paused_ARBfrag_asm);
+					strlen(ms_Paused_ARB_fragment_program_src),
+					ms_Paused_ARB_fragment_program_src);
 		
 		err = glGetError();
-		if (err != GL_NO_ERROR)
+		if (err == GL_NO_ERROR)
+		{
+			// Fragment program loaded.
+			m_paused_type = ST_GL_ARB_FRAGMENT_PROGRAM;
+		}
+		else
 		{
 			// An error occured while loading the fragment program.
 			// TODO: Remove the extra newline at the end of err_str.
 			const char *err_str = (const char*)glGetString(GL_PROGRAM_ERROR_STRING_ARB);
 			LOG_MSG(video, LOG_MSG_LEVEL_ERROR,
-				"Error creating Paused effect FP: %s", (err_str ? err_str : "(unknown)"));
+				"Error creating Paused effect ARB FP: %s", (err_str ? err_str : "(unknown)"));
 			
 			// Delete the fragment program.
-			if (m_paused_ARBfrag > 0)
+			if (m_paused_ARB > 0)
 			{
-				glDeleteProgramsARB(1, &m_paused_ARBfrag);
-				m_paused_ARBfrag = 0;
+				glDeleteProgramsARB(1, &m_paused_ARB);
+				m_paused_ARB = 0;
+				m_paused_type = ST_NONE;
 			}
 		}
 		
 		// Load the Fast Blur effect fragment program.
 		// TODO: Check for errors!
-		glGenProgramsARB(1, &m_fastBlur_ARBfrag);
-		glBindProgramARB(GL_FRAGMENT_PROGRAM_ARB, m_fastBlur_ARBfrag);
+		glGenProgramsARB(1, &m_fastBlur_ARB);
+		glBindProgramARB(GL_FRAGMENT_PROGRAM_ARB, m_fastBlur_ARB);
 		glGetError();	// Clear the error flag.
 		glProgramStringARB(GL_FRAGMENT_PROGRAM_ARB, GL_PROGRAM_FORMAT_ASCII_ARB,
-				   strlen(ms_FastBlur_ARBfrag_asm), ms_FastBlur_ARBfrag_asm);
+					strlen(ms_FastBlur_ARB_fragment_program_src),
+					ms_FastBlur_ARB_fragment_program_src);
 		
 		err = glGetError();
 		if (err != GL_NO_ERROR)
@@ -145,13 +182,52 @@ void GLShaderManager::init(void)
 			// TODO: Remove the extra newline at the end of err_str.
 			const char *err_str = (const char*)glGetString(GL_PROGRAM_ERROR_STRING_ARB);
 			LOG_MSG(video, LOG_MSG_LEVEL_ERROR,
-				"Error creating Fast Blur effect FP: %s", (err_str ? err_str : "(unknown)"));
+				"Error creating Fast Blur effect ARB FP: %s", (err_str ? err_str : "(unknown)"));
 			
 			// Delete the fragment program.
-			if (m_fastBlur_ARBfrag > 0)
+			if (m_fastBlur_ARB > 0)
 			{
-				glDeleteProgramsARB(1, &m_fastBlur_ARBfrag);
-				m_fastBlur_ARBfrag = 0;
+				glDeleteProgramsARB(1, &m_fastBlur_ARB);
+				m_fastBlur_ARB = 0;
+			}
+		}
+	}
+	else if (GLEW_ATI_text_fragment_shader)
+	{
+		// ATI text fragment shaders are supported
+		// Load the fragment shaders.
+		// TODO: Combine with ARB fragment programs code above.
+		// The code is the same, but it uses different values and strings.
+		
+		// Load the Paused effect fragment shader.
+		// TODO: Check for errors!
+		glGenProgramsARB(1, &m_paused_ARB);
+		glBindProgramARB(GL_TEXT_FRAGMENT_SHADER_ATI, m_paused_ARB);
+		glGetError();	// Clear the error flag.
+		glProgramStringARB(GL_TEXT_FRAGMENT_SHADER_ATI, GL_PROGRAM_FORMAT_ASCII_ARB,
+					strlen(ms_Paused_ATI_text_fragment_shader_src),
+					ms_Paused_ATI_text_fragment_shader_src);
+		
+		err = glGetError();
+		if (err == GL_NO_ERROR)
+		{
+			// Fragment program loaded.
+			m_paused_type = ST_GL_ATI_TEXT_FRAGMENT_SHADER;
+		}
+		else
+		{
+			// An error occured while loading the fragment program.
+			// TODO: Remove the extra newline at the end of err_str.
+			const char *err_str = (const char*)glGetString(GL_PROGRAM_ERROR_STRING_ARB);
+			LOG_MSG(video, LOG_MSG_LEVEL_ERROR,
+				"Error creating Paused effect ATI TEXT FS: %s", (err_str ? err_str : "(unknown)"));
+			
+			// Delete the fragment program.
+			if (m_paused_ARB > 0)
+			{
+				glDeleteProgramsARB(1, &m_paused_ARB);
+				m_paused_ARB = 0;
+				m_paused_type = ST_NONE;
 			}
 		}
 	}
@@ -175,10 +251,34 @@ void GLShaderManager::end(void)
 	
 #ifdef HAVE_GLEW
 	// Delete any allocated shaders.
-	if (m_paused_ARBfrag > 0)
+	switch (m_paused_type)
 	{
-		glDeleteProgramsARB(1, &m_paused_ARBfrag);
-		m_paused_ARBfrag = 0;
+		case ST_GL_ARB_FRAGMENT_PROGRAM:
+		case ST_GL_ATI_TEXT_FRAGMENT_SHADER:
+			if (m_paused_ARB > 0)
+			{
+				glDeleteProgramsARB(1, &m_paused_ARB);
+				m_paused_ARB = 0;
+			}
+			break;
+		
+		default:
+			break;
+	}
+	
+	switch (m_fastBlur_type)
+	{
+		case ST_GL_ARB_FRAGMENT_PROGRAM:
+		case ST_GL_ATI_TEXT_FRAGMENT_SHADER:
+			if (m_fastBlur_ARB > 0)
+			{
+				glDeleteProgramsARB(1, &m_fastBlur_ARB);
+				m_fastBlur_ARB = 0;
+			}
+			break;
+		
+		default:
+			break;
 	}
 #endif
 	
@@ -198,6 +298,8 @@ QStringList GLShaderManager::GLExtsInUse(void)
 	
 	if (GLEW_ARB_fragment_program || GLEW_VERSION_2_0)
 		exts.append(QLatin1String("GL_ARB_fragment_program"));
+	else if (GLEW_ATI_text_fragment_shader)
+		exts.append(QLatin1String("GL_ATI_text_fragment_shader"));
 	
 	// TODO: Other GL extensions.
 #if 0
@@ -238,20 +340,48 @@ void GLShaderManager::setPaused(bool newEnabled)
 	// TODO: Check if any other shaders are enabled?
 	if (!m_init)
 		return;
-	if (m_paused_ARBfrag == 0 || newEnabled == m_paused_enabled)
+	if (newEnabled == m_paused_enabled)
 		return;
 	
-	m_paused_enabled = newEnabled;
-	if (m_paused_enabled)
+	switch (m_paused_type)
 	{
-		// Enable the ARB fragment program.
-		glEnable(GL_FRAGMENT_PROGRAM_ARB);
-		glBindProgramARB(GL_FRAGMENT_PROGRAM_ARB, m_paused_ARBfrag);
-	}
-	else
-	{
-		// Disable the ARB fragment program.
-		glDisable(GL_FRAGMENT_PROGRAM_ARB);
+		case ST_GL_ARB_FRAGMENT_PROGRAM:
+		case ST_GL_ATI_TEXT_FRAGMENT_SHADER:
+		{
+			if (m_paused_ARB == 0)
+				break;
+			
+			const GLenum prgType = (m_paused_type == ST_GL_ARB_FRAGMENT_PROGRAM
+						? GL_FRAGMENT_PROGRAM_ARB
+						: GL_TEXT_FRAGMENT_SHADER_ATI
+						);
+			
+			m_paused_enabled = newEnabled;
+			if (m_paused_enabled)
+			{
+				// Enable the shader.
+				glEnable(prgType);
+				glBindProgramARB(prgType, m_paused_ARB);
+			}
+			else
+			{
+				// Disable the shader.
+				glDisable(prgType);
+				if (m_paused_type == ST_GL_ATI_TEXT_FRAGMENT_SHADER)
+				{
+					// HACK: at least the Mac OS X 10.5 PPC Radeon drivers are broken and
+					// without this disable the texture units while the program is still
+					// running (10.4 PPC seems to work without this though).
+					// Reference: http://git.mplayerhq.hu/?p=mplayer;a=blob;f=libvo/gl_common.c;hb=HEAD
+					// NOTE: It doesn't seem to help any...
+					glFlush();
+				}
+			}
+			break;
+		}
+		
+		default:
+			break;
 	}
 }
 
@@ -265,22 +395,38 @@ void GLShaderManager::setFastBlur(bool newEnabled)
 	// TODO: Check if any other shaders are enabled?
 	if (!m_init)
 		return;
-	if (m_fastBlur_ARBfrag == 0 || newEnabled == m_fastBlur_ARBfrag)
+	if (newEnabled == m_paused_enabled)
 		return;
 	
-	m_fastBlur_enabled = newEnabled;
-	if (m_fastBlur_enabled)
+	if (!m_init)
+		return;
+	if (m_fastBlur_ARB == 0 || newEnabled == m_fastBlur_ARB)
+		return;
+	
+	switch (m_paused_type)
 	{
-		// Enable the ARB fragment program.
-		// TODO: Get texture width for the parameter!
-		glEnable(GL_FRAGMENT_PROGRAM_ARB);
-		glProgramLocalParameter4fARB(GL_FRAGMENT_PROGRAM_ARB, 0, (1/512.0f), 0, 0, 0);
-		glBindProgramARB(GL_FRAGMENT_PROGRAM_ARB, m_fastBlur_ARBfrag);
-	}
-	else
-	{
-		// Disable the ARB fragment program.
-		glDisable(GL_FRAGMENT_PROGRAM_ARB);
+		case ST_GL_ARB_FRAGMENT_PROGRAM:
+			if (m_fastBlur_ARB == 0)
+				break;
+			
+			m_fastBlur_enabled = newEnabled;
+			if (m_fastBlur_enabled)
+			{
+				// Enable the ARB fragment program.
+				// TODO: Get texture width for the parameter!
+				glEnable(GL_FRAGMENT_PROGRAM_ARB);
+				glProgramLocalParameter4fARB(GL_FRAGMENT_PROGRAM_ARB, 0, (1/512.0f), 0, 0, 0);
+				glBindProgramARB(GL_FRAGMENT_PROGRAM_ARB, m_fastBlur_ARB);
+			}
+			else
+			{
+				// Disable the ARB fragment program.
+				glDisable(GL_FRAGMENT_PROGRAM_ARB);
+			}
+			break;
+		
+		default:
+			break;
 	}
 }
 #endif

src/gens-qt4/VBackend/GLShaderManager.hpp
@@ -68,11 +68,15 @@ class GLShaderManager
 		
 		/** Query available shaders. **/
 #ifdef HAVE_GLEW
-		inline bool hasPaused(void) const { return (m_paused_ARBfrag != 0); }
-		inline bool hasFastBlur(void) const { return (m_fastBlur_ARBfrag != 0); }
+		inline bool hasPaused(void) const
+			{ return (m_paused_type != ST_NONE); }
+		inline bool hasFastBlur(void) const
+			{ return (m_fastBlur_ARB != 0); }
 #else /* !HAVE_GLEW */
-		inline bool hasPaused(void) const { return false; }
-		inline bool hasFastBlur(void) const { return false; }
+		inline bool hasPaused(void) const
+			{ return false; }
+		inline bool hasFastBlur(void) const
+			{ return false; }
 #endif /* HAVE_GLEW */
 		
 		/** Set Shader functions. **/
@@ -92,18 +96,30 @@ class GLShaderManager
 	protected:
 		bool m_init;	// True if the GL Shader Manager is initialized.
 		
+		enum ShaderType
+		{
+			ST_NONE				= 0,
+			ST_GL_ARB_FRAGMENT_PROGRAM	= 1,
+			ST_GL_ATI_TEXT_FRAGMENT_SHADER	= 2,
+			
+			ST_MAX
+		};
+		
 		/** OpenGL shaders. **/
 		
 #ifdef HAVE_GLEW
-		// Paused effect: ARB fragment program.
-		GLuint m_paused_ARBfrag;
+		// Paused effect.
 		bool m_paused_enabled;
-		static const char *ms_Paused_ARBfrag_asm;
+		ShaderType m_paused_type;
+		GLuint m_paused_ARB;
+		static const char *ms_Paused_ARB_fragment_program_src;
+		static const char *ms_Paused_ATI_text_fragment_shader_src;
 		
-		// Fast Blur: ARB fragment program.
-		GLuint m_fastBlur_ARBfrag;
+		// Fast Blur effect.
 		bool m_fastBlur_enabled;
-		static const char *ms_FastBlur_ARBfrag_asm;
+		ShaderType m_fastBlur_type;
+		GLuint m_fastBlur_ARB;
+		static const char *ms_FastBlur_ARB_fragment_program_src;
 #endif /* HAVE_GLEW */
 };
 

commit cd940796330b7f02915642f63db901fd6eacf898

[gens-qt4] VBackend/GLShaderManager.cpp: Fixed several errors with Fast Blur.
These errors are regressions from commit dbe676e65a052d984b7b56144f95865cc3cde70d.

- setFastBlur(): Removed redundant code. Compare newEnabled to
  m_fastBlur_enabled, not m_paused_enabled.

- end(): Set m_paused_type and m_fastBlur_type to ST_NONE.

- hasFastBlur(): Check (m_fastBlurType != ST_NONE) instead of
  (m_fastBlur_ARB != 0).

src/gens-qt4/VBackend/GLShaderManager.cpp
@@ -102,12 +102,10 @@ GLShaderManager::GLShaderManager()
 	// Initialize shader variables.
 	m_paused_enabled = false;
 	m_paused_type = ST_NONE;
+	m_paused_ARB = 0;
 	
 	m_fastBlur_enabled = false;
 	m_fastBlur_type = ST_NONE;
-	
-	// Clear the shader pointers.
-	m_paused_ARB = 0;
 	m_fastBlur_ARB = 0;
 #endif
 }
@@ -271,6 +269,7 @@ void GLShaderManager::end(void)
 		default:
 			break;
 	}
+	m_paused_type = ST_NONE;
 	
 	switch (m_fastBlur_type)
 	{
@@ -288,6 +287,7 @@ void GLShaderManager::end(void)
 		default:
 			break;
 	}
+	m_fastBlur_type = ST_NONE;
 #endif
 	
 	// OpenGL Shader Manager is shut down.
@@ -413,12 +413,7 @@ void GLShaderManager::setFastBlur(bool newEnabled)
 	// TODO: Check if any other shaders are enabled?
 	if (!m_init)
 		return;
-	if (newEnabled == m_paused_enabled)
-		return;
-	
-	if (!m_init)
-		return;
-	if (m_fastBlur_ARB == 0 || newEnabled == m_fastBlur_ARB)
+	if (newEnabled == m_fastBlur_enabled)
 		return;
 	
 	switch (m_paused_type)

src/gens-qt4/VBackend/GLShaderManager.hpp
@@ -75,7 +75,7 @@ class GLShaderManager
 		inline bool hasPaused(void) const
 			{ return (m_paused_type != ST_NONE); }
 		inline bool hasFastBlur(void) const
-			{ return (m_fastBlur_ARB != 0); }
+			{ return (m_fastBlur_type != ST_NONE); }
 #else /* !HAVE_GLEW */
 		inline bool hasPaused(void) const
 			{ return false; }