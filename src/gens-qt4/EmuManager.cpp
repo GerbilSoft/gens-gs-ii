GerbilSoft/gens-gs-ii

commit 0eadeb32f9a94a833dbf847c7784886b50aeb1dd

[gens-qt4] GensConfig.cpp: Added a property for the current region code.
This property uses a new enum, ConfRegionCode_t, that contains an additional
value for "auto-detect".

TODO: Maybe integrate "auto-detect" with LibGens?

Updated all the config enums (and LibGens::SysVersion::RegionCode_t)
to use "_t" suffixes.

src/gens-qt4/VBackend/VBackend.cpp
@@ -150,7 +150,7 @@ void VBackend::setPaused(paused_t newPaused)
  * setStretchMode(): Set the stretch mode setting.
  * @param newStretchMode New stretch mode setting.
  */
-void VBackend::setStretchMode(GensConfig::StretchMode newStretchMode)
+void VBackend::setStretchMode(GensConfig::StretchMode_t newStretchMode)
 {
 	if ((m_stretchMode == newStretchMode) ||
 	    (newStretchMode < GensConfig::STRETCH_NONE) ||

src/gens-qt4/EmuManager.hpp
@@ -309,7 +309,7 @@ class EmuManager : public QObject
 			{ changePaletteSetting(EmuRequest_t::RQT_PS_INVERTED, (int)newInverted); }
 		void colorScaleMethod_changed_slot(int newColorScaleMethod)
 			{ changePaletteSetting(EmuRequest_t::RQT_PS_COLORSCALEMETHOD, newColorScaleMethod); }
-		void interlacedMode_changed_slot(GensConfig::InterlacedMode newInterlacedMode)
+		void interlacedMode_changed_slot(GensConfig::InterlacedMode_t newInterlacedMode)
 			{ changePaletteSetting(EmuRequest_t::RQT_PS_INTERLACEDMODE, (int)newInterlacedMode); }
 	
 	/** Emulation Request Queue: Processing functions. **/

src/gens-qt4/GeneralConfigWindow.cpp
@@ -379,7 +379,7 @@ void GeneralConfigWindow::apply(void)
 	gqt4_config->setAspectRatioConstraint(chkAspectRatioConstraint->isChecked());
 	gqt4_config->setFastBlur(chkFastBlur->isChecked());
 	gqt4_config->setBilinearFilter(chkBilinearFilter->isChecked());
-	gqt4_config->setInterlacedMode((GensConfig::InterlacedMode)cboInterlacedMode->currentIndex());
+	gqt4_config->setInterlacedMode((GensConfig::InterlacedMode_t)cboInterlacedMode->currentIndex());
 	gqt4_config->setContrast(hsldContrast->value());
 	gqt4_config->setBrightness(hsldBrightness->value());
 	gqt4_config->setGrayscale(chkGrayscale->isChecked());

src/gens-qt4/GeneralConfigWindow_slots.cpp
@@ -76,7 +76,7 @@ void GeneralConfigWindow::on_chkFastBlur_toggled(bool checked)
 void GeneralConfigWindow::on_chkBilinearFilter_toggled(bool checked)
 	{ GENERIC_OPTION(setBilinearFilter, checked); }
 void GeneralConfigWindow::on_cboInterlacedMode_currentIndexChanged(int index)
-	{ GENERIC_OPTION(setInterlacedMode, (GensConfig::InterlacedMode)index); }
+	{ GENERIC_OPTION(setInterlacedMode, (GensConfig::InterlacedMode_t)index); }
 
 void GeneralConfigWindow::on_hsldContrast_valueChanged(int value)
 	{ GENERIC_OPTION(setContrast, value); }

src/gens-qt4/GensConfig.cpp
@@ -125,6 +125,14 @@ int GensConfig::reload(const QString& filename)
 	m_introColor = settings.value(QLatin1String("introColor"), 7).toInt();	// white
 	settings.endGroup();
 	
+	/** System. **/
+	settings.beginGroup(QLatin1String("System"));
+	int regionCode_tmp = settings.value(QLatin1String("regionCode"), (int)CONFREGION_AUTODETECT).toInt();
+	if ((regionCode_tmp < (int)CONFREGION_AUTODETECT) || (regionCode_tmp > (int)CONFREGION_EU_PAL))
+		regionCode_tmp = (int)CONFREGION_AUTODETECT;
+	m_regionCode = (ConfRegionCode_t)regionCode_tmp;
+	settings.endGroup();
+	
 	/** Sega CD Boot ROMs. **/
 	settings.beginGroup(QLatin1String("Sega_CD"));
 	m_mcdRomUSA = settings.value(QLatin1String("bootRomUSA"), QString()).toString();
@@ -159,7 +167,7 @@ int GensConfig::reload(const QString& filename)
 	int interlaced_tmp = settings.value(QLatin1String("interlacedMode"), (int)INTERLACED_FLICKER).toInt();
 	if ((interlaced_tmp < (int)INTERLACED_EVEN) || (interlaced_tmp > (int)INTERLACED_FLICKER))
 		interlaced_tmp = (int)INTERLACED_FLICKER;
-	m_interlacedMode = (InterlacedMode)interlaced_tmp;
+	m_interlacedMode = (InterlacedMode_t)interlaced_tmp;
 	m_contrast = settings.value(QLatin1String("contrast"), 0).toInt();
 	m_brightness = settings.value(QLatin1String("brightness"), 0).toInt();
 	m_grayscale = settings.value(QLatin1String("grayscale"), false).toBool();
@@ -172,7 +180,7 @@ int GensConfig::reload(const QString& filename)
 	int stretch_tmp = settings.value(QLatin1String("stretchMode"), (int)STRETCH_H).toInt();
 	if ((stretch_tmp < (int)STRETCH_NONE) || (stretch_tmp > (int)STRETCH_FULL))
 		stretch_tmp = (int)STRETCH_H;
-	m_stretchMode = (StretchMode)stretch_tmp;
+	m_stretchMode = (StretchMode_t)stretch_tmp;
 	
 	settings.endGroup();
 	
@@ -275,6 +283,11 @@ int GensConfig::save(const QString& filename)
 	settings.beginGroup(QLatin1String("Intro_Effect"));
 	settings.setValue(QLatin1String("introStyle"), m_introStyle);
 	settings.setValue(QLatin1String("introColor"), m_introColor);
+	settings.endGroup();
+	
+	/** System. **/
+	settings.beginGroup(QLatin1String("System"));
+	settings.setValue(QLatin1String("regionCode"), (int)m_regionCode);
 	settings.endGroup();
 	
 	/** Sega CD Boot ROMs. **/
@@ -459,18 +472,20 @@ void GensConfig::set##setPropName(setPropType new##setPropName) \
 	emit propName##_changed(m_##propName); \
 }
 
-
 /** Onscreen display. **/
 GC_PROPERTY_WRITE(osdFpsEnabled, bool, OsdFpsEnabled)
 GC_PROPERTY_WRITE(osdFpsColor, const QColor&, OsdFpsColor)
 GC_PROPERTY_WRITE(osdMsgEnabled, bool, OsdMsgEnabled)
 GC_PROPERTY_WRITE(osdMsgColor, const QColor&, OsdMsgColor)
 
-
 /** Intro effect. **/
 GC_PROPERTY_WRITE_RANGE(introStyle, int, IntroStyle, 0, 2)
 GC_PROPERTY_WRITE_RANGE(introColor, int, IntroColor, 0, 7)
 
+/** System. **/
+GC_PROPERTY_WRITE_RANGE(regionCode, ConfRegionCode_t, RegionCode,
+			(int)CONFREGION_AUTODETECT,
+		        (int)CONFREGION_EU_PAL)
 
 /** Sega CD Boot ROMs. **/
 GC_PROPERTY_WRITE(mcdRomUSA, const QString&, McdRomUSA)
@@ -504,7 +519,7 @@ GC_PROPERTY_WRITE(aspectRatioConstraint, bool, AspectRatioConstraint)
 GC_PROPERTY_WRITE(fastBlur, bool, FastBlur)
 GC_PROPERTY_WRITE(bilinearFilter, bool, BilinearFilter)
 // TODO: Add support for INTERLACED_2X.
-GC_PROPERTY_WRITE_RANGE(interlacedMode, InterlacedMode, InterlacedMode,
+GC_PROPERTY_WRITE_RANGE(interlacedMode, InterlacedMode_t, InterlacedMode,
 			(int)INTERLACED_EVEN, (int)INTERLACED_FLICKER);
 GC_PROPERTY_WRITE(contrast, int, Contrast)
 GC_PROPERTY_WRITE(brightness, int, Brightness)
@@ -513,7 +528,7 @@ GC_PROPERTY_WRITE(inverted, bool, Inverted)
 GC_PROPERTY_WRITE_RANGE(colorScaleMethod, int, ColorScaleMethod,
 			(int)LibGens::VdpPalette::COLSCALE_RAW,
 			(int)LibGens::VdpPalette::COLSCALE_FULL_HS)
-GC_PROPERTY_WRITE_RANGE(stretchMode, StretchMode, StretchMode,
+GC_PROPERTY_WRITE_RANGE(stretchMode, StretchMode_t, StretchMode,
 			(int)STRETCH_NONE, (int)STRETCH_FULL)
 
 

src/gens-qt4/GensConfig.hpp
@@ -121,6 +121,21 @@ class GensConfig : public QObject
 		Q_PROPERTY(int introColor READ introColor WRITE setIntroColor NOTIFY introColor_changed)
 		GC_PROPERTY(int, introColor, int, IntroColor);
 		
+		/** System. **/
+		
+		public:
+			enum ConfRegionCode_t
+			{
+				CONFREGION_AUTODETECT = -1,
+				CONFREGION_JP_NTSC    = 0,
+				CONFREGION_ASIA_PAL   = 1,
+				CONFREGION_USA_NTSC   = 2,
+				CONFREGION_EU_PAL     = 3
+			};
+		Q_ENUMS(ConfRegionCode_t);
+		Q_PROPERTY(ConfRegionCode_t regionCode READ regionCode WRITE setRegionCode NOTIFY regionCode_changed);
+		GC_PROPERTY(ConfRegionCode_t, regionCode, ConfRegionCode_t, RegionCode);
+		
 		/** Sega CD Boot ROMs. **/
 		Q_PROPERTY(QString mcdRomUSA READ mcdRomUSA WRITE setMcdRomUSA NOTIFY mcdRomUSA_changed)
 		GC_PROPERTY(QString, mcdRomUSA, const QString&, McdRomUSA);
@@ -145,16 +160,16 @@ class GensConfig : public QObject
 		Q_PROPERTY(int contrast READ contrast WRITE setContrast NOTIFY contrast_changed)
 		
 		public:
-			enum InterlacedMode
+			enum InterlacedMode_t
 			{
 				INTERLACED_EVEN		= 0,
 				INTERLACED_ODD		= 1,
 				INTERLACED_FLICKER	= 2,
 				INTERLACED_2X		= 3,
 			};
-		Q_ENUMS(InterlacedMode);
-		GC_PROPERTY(InterlacedMode, interlacedMode, InterlacedMode, InterlacedMode);
-		Q_PROPERTY(InterlacedMode interlacedMode READ interlacedMode WRITE setInterlacedMode NOTIFY interlacedMode_changed)
+		Q_ENUMS(InterlacedMode_t);
+		GC_PROPERTY(InterlacedMode_t, interlacedMode, InterlacedMode_t, InterlacedMode);
+		Q_PROPERTY(InterlacedMode_t interlacedMode READ interlacedMode WRITE setInterlacedMode NOTIFY interlacedMode_changed)
 		
 		GC_PROPERTY(int, contrast, int, Contrast);
 		Q_PROPERTY(int brightness READ brightness WRITE setBrightness NOTIFY brightness_changed)
@@ -167,16 +182,16 @@ class GensConfig : public QObject
 		GC_PROPERTY(int, colorScaleMethod, int, ColorScaleMethod);
 		
 		public:
-			enum StretchMode
+			enum StretchMode_t
 			{
 				STRETCH_NONE	= 0,
 				STRETCH_H	= 1,
 				STRETCH_V	= 2,
 				STRETCH_FULL	= 3
 			};
-		Q_ENUMS(StretchMode);
-		Q_PROPERTY(StretchMode stretchMode READ stretchMode WRITE setStretchMode NOTIFY stretchMode_changed)
-		GC_PROPERTY(StretchMode, stretchMode, StretchMode, StretchMode);
+		Q_ENUMS(StretchMode_t);
+		Q_PROPERTY(StretchMode_t stretchMode READ stretchMode WRITE setStretchMode NOTIFY stretchMode_changed)
+		GC_PROPERTY(StretchMode_t, stretchMode, StretchMode_t, StretchMode);
 		
 		/** General settings. **/
 		Q_PROPERTY(bool autoFixChecksum READ autoFixChecksum WRITE setAutoFixChecksum NOTIFY autoFixChecksum_changed)
@@ -213,6 +228,9 @@ class GensConfig : public QObject
 		void osdMsgEnabled_changed(bool enable);
 		void osdMsgColor_changed(const QColor& color);
 		
+		/** System. **/
+		void regionCode_changed(GensConfig::ConfRegionCode_t newRegionCode);
+		
 		/** Intro effect. **/
 		void introStyle_changed(int style);
 		void introColor_changed(int color);
@@ -230,13 +248,13 @@ class GensConfig : public QObject
 		void aspectRatioConstraint_changed(bool newAspectRatioConstraint);
 		void fastBlur_changed(bool newFastBlur);
 		void bilinearFilter_changed(bool newBilinearFilter);
-		void interlacedMode_changed(GensConfig::InterlacedMode newInterlacedMode);
+		void interlacedMode_changed(GensConfig::InterlacedMode_t newInterlacedMode);
 		void contrast_changed(int newContrast);
 		void brightness_changed(int newBrightness);
 		void grayscale_changed(bool newGrayscale);
 		void inverted_changed(bool newInverted);
 		void colorScaleMethod_changed(int newColorScaleMethod);
-		void stretchMode_changed(GensConfig::StretchMode newStretchMode);
+		void stretchMode_changed(GensConfig::StretchMode_t newStretchMode);
 		
 		/** General settings. **/
 		void autoFixChecksum_changed(bool newAutoFixChecksum);

src/gens-qt4/VBackend/GLBackend.cpp
@@ -636,7 +636,7 @@ void GLBackend::glb_paintGL(void)
  * recalcStretchRectF(): Recalculate the stretch mode rectangle.
  * @param mode Stretch mode.
  */
-void GLBackend::recalcStretchRectF(GensConfig::StretchMode mode)
+void GLBackend::recalcStretchRectF(GensConfig::StretchMode_t mode)
 {
 	// Store the current MD screen resolution.
 	m_stretchLastRes.setWidth(LibGens::VdpIo::GetHPix());
@@ -1123,7 +1123,7 @@ void GLBackend::setPauseTint(bool newPauseTint)
 }
 
 
-void GLBackend::setStretchMode(GensConfig::StretchMode newStretchMode)
+void GLBackend::setStretchMode(GensConfig::StretchMode_t newStretchMode)
 {
 	if ((stretchMode() == newStretchMode) ||
 	    (newStretchMode < GensConfig::STRETCH_NONE) ||

src/gens-qt4/VBackend/GLBackend.hpp
@@ -96,7 +96,7 @@ class GLBackend : public VBackend
 		 * setStretchMode(): Set the stretch mode setting.
 		 * @param newStretchMode New stretch mode setting.
 		 */
-		void setStretchMode(GensConfig::StretchMode newStretchMode);
+		void setStretchMode(GensConfig::StretchMode_t newStretchMode);
 	
 	protected:
 		/**
@@ -152,7 +152,7 @@ class GLBackend : public VBackend
 		QRectF m_stretchRectF;		// Current stretch coordinates.
 		QSize m_stretchLastRes;		// Last MD screen resolution.
 		void recalcStretchRectF(void);
-		void recalcStretchRectF(GensConfig::StretchMode mode);
+		void recalcStretchRectF(GensConfig::StretchMode_t mode);
 		
 		// OSD texture.
 		GLTex2D *m_texOsd;	// Texture containing U+0000 - U+00FF.

commit a64a29faef01036d27cb528b6c9fc8ebd66f8102

[gens-qt4] Fixed signal connection issues due to InterlacedMode_t and StretchMode_t changes.
This is a regression from commit 0eadeb32f9a94a833dbf847c7784886b50aeb1dd.
([gens-qt4] GensConfig.cpp: Added a property for the current region code.)

src/gens-qt4/EmuManager.cpp
@@ -102,8 +102,8 @@ EmuManager::EmuManager()
 		this, SLOT(inverted_changed_slot(bool)));
 	connect(gqt4_config, SIGNAL(colorScaleMethod_changed(int)),
 		this, SLOT(colorScaleMethod_changed_slot(int)));
-	connect(gqt4_config, SIGNAL(interlacedMode_changed(GensConfig::InterlacedMode)),
-		this, SLOT(interlacedMode_changed_slot(GensConfig::InterlacedMode)));
+	connect(gqt4_config, SIGNAL(interlacedMode_changed(GensConfig::InterlacedMode_t)),
+		this, SLOT(interlacedMode_changed_slot(GensConfig::InterlacedMode_t)));
 }
 
 EmuManager::~EmuManager()

src/gens-qt4/VBackend/VBackend.cpp
@@ -100,8 +100,8 @@ VBackend::VBackend(QWidget *parent)
 		this, SLOT(setBilinearFilter(bool)));
 	connect(gqt4_config, SIGNAL(pauseTint_changed(bool)),
 		this, SLOT(setPauseTint(bool)));
-	connect(gqt4_config, SIGNAL(stretchMode_changed(GensConfig::StretchMode)),
-		this, SLOT(setStretchMode(GensConfig::StretchMode)));
+	connect(gqt4_config, SIGNAL(stretchMode_changed(GensConfig::StretchMode_t)),
+		this, SLOT(setStretchMode(GensConfig::StretchMode_t)));
 }
 
 VBackend::~VBackend()