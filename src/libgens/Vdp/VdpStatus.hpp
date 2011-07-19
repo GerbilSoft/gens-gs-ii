GerbilSoft/gens-gs-ii

commit 4cdad1cf6e9c483a739da22ecb25bff11e37e0b5

[libgens] Vdp/VdpStatus.hpp: Removed the convenience set/toggle/clear functions in favor of setBit() and toggleBit().
This reduces the size of VdpStatus.hpp while providing the same functionality.

Updated VdpIo.cpp, VdpRend_m5.cpp, and MD/EmuMD.cpp to work with this change.

src/libgens/Vdp/VdpStatus.hpp
@@ -77,21 +77,12 @@ class VdpStatus
 		void write_raw(uint16_t status);
 		
 		/** Convenience functions. **/
-		
 		bool isPal(void) const;
 		bool isNtsc(void) const;
-		
 		bool isOddFrame(void) const;
-		void toggleOddFrame(void);
-		void clearOddFrame(void);
 		
-		void setRegion(bool isPal);
-		void setHBlank(bool HBlank);
-		void setVBlank(bool VBlank);
-		void setVIntHappened(bool VIntHappened);
-		void setDMABusy(bool DMABusy);
-		void setCollision(bool collision);
-		void setSOvr(bool sovr);
+		void setBit(StatusBits bit, bool value);
+		void toggleBit(StatusBits bit);
 	
 	private:
 		uint16_t m_status;
@@ -105,81 +96,23 @@ inline void VdpStatus::write_raw(uint16_t status)
 
 /** VDP status register functions. **/
 
-// PAL/NTSC: Bit 0. 0 == NTSC; 1 == PAL.
 inline bool VdpStatus::isPal(void) const
 	{ return (m_status & VDP_STATUS_PAL); }
 inline bool VdpStatus::isNtsc(void) const
 	{ return (!(m_status & VDP_STATUS_PAL)); }
-
 inline bool VdpStatus::isOddFrame(void) const
 	{ return (!!(m_status & VDP_STATUS_ODD)); }
-inline void VdpStatus::toggleOddFrame(void)
-	{ m_status ^= VDP_STATUS_ODD; }
-inline void VdpStatus::clearOddFrame(void)
-	{ m_status &= ~VDP_STATUS_ODD; }
 
-inline void VdpStatus::setRegion(bool isPal)
+inline void VdpStatus::setBit(StatusBits bit, bool value)
 {
-	// Bit 0: 0 == NTSC; 1 == PAL
-	if (isPal)
-		m_status |= VDP_STATUS_PAL;
+	if (value)
+		m_status |= bit;
 	else
-		m_status &= ~VDP_STATUS_PAL;
+		m_status &= ~bit;
 }
 
-inline void VdpStatus::setHBlank(bool HBlank)
-{
-	// Bit 3: 0 == not in HBlank; 1 == in HBlank
-	if (HBlank)
-		m_status |= VDP_STATUS_HBLANK;
-	else
-		m_status &= ~VDP_STATUS_HBLANK;
-}
-
-inline void VdpStatus::setVBlank(bool VBlank)
-{
-	// Bit 4: 0 == not in HBlank; 1 == in HBlank
-	if (VBlank)
-		m_status |= VDP_STATUS_VBLANK;
-	else
-		m_status &= ~VDP_STATUS_VBLANK;
-}
-
-inline void VdpStatus::setVIntHappened(bool VIntHappened)
-{
-	// Bit 7: 0 == VInt processed; 1 == new VInt happened
-	if (VIntHappened)
-		m_status |= VDP_STATUS_F;
-	else
-		m_status &= ~VDP_STATUS_F;
-}
-
-inline void VdpStatus::setDMABusy(bool DMABusy)
-{
-	// Bit 1: 0 == not processing DMA; 1 == DMA busy
-	if (DMABusy)
-		m_status |= VDP_STATUS_DMA;
-	else
-		m_status &= ~VDP_STATUS_DMA;
-}
-
-inline void VdpStatus::setCollision(bool collision)
-{
-	// Bit 5: 0 == no sprite collision; 1 == sprite collision
-	if (collision)
-		m_status |= VDP_STATUS_COLLISION;
-	else
-		m_status &= ~VDP_STATUS_COLLISION;
-}
-
-inline void VdpStatus::setSOvr(bool sovr)
-{
-	// Bit 6: 0 == no sprite overflow; 1 == sprite overflow
-	if (sovr)
-		m_status |= VDP_STATUS_SOVR;
-	else
-		m_status &= ~VDP_STATUS_SOVR;
-}
+inline void VdpStatus::toggleBit(StatusBits bit)
+	{ m_status &= bit; }
 
 }
 

src/libgens/MD/EmuMD.cpp
@@ -183,9 +183,9 @@ int EmuMD::hardReset(void)
 	// Make sure the VDP's video mode bit is set properly.
 	// TODO: Make a VdpIo inline function for this?
 	if (M68K_Mem::ms_SysVersion.isPal())
-		Vdp::Reg_Status.setRegion(true);	// PAL: Set the PAL bit.
+		Vdp::Reg_Status.setBit(VdpStatus::VDP_STATUS_PAL, true);	// PAL: Set the PAL bit.
 	else
-		Vdp::Reg_Status.setRegion(false);	// NTSC: Clear the PAL bit.
+		Vdp::Reg_Status.setBit(VdpStatus::VDP_STATUS_PAL, false);	// NTSC: Clear the PAL bit.
 	
 	// Reset successful.
 	return 0;
@@ -229,13 +229,13 @@ int EmuMD::setRegion_int(SysVersion::RegionCode_t region, bool preserveState)
 	{
 		M68K_Mem::CPL_M68K = (int)floor((((double)CLOCK_PAL / 7.0) / 50.0) / 312.0);
 		M68K_Mem::CPL_Z80 = (int)floor((((double)CLOCK_PAL / 15.0) / 50.0) / 312.0);
-		Vdp::Reg_Status.setRegion(true);	// PAL: Set the PAL bit.
+		Vdp::Reg_Status.setBit(VdpStatus::VDP_STATUS_PAL, true);	// PAL: Set the PAL bit.
 	}
 	else
 	{
 		M68K_Mem::CPL_M68K = (int)floor((((double)CLOCK_NTSC / 7.0) / 60.0) / 262.0);
 		M68K_Mem::CPL_Z80 = (int)floor((((double)CLOCK_NTSC / 15.0) / 60.0) / 262.0);
-		Vdp::Reg_Status.setRegion(false);	// NTSC: Clear the PAL bit.
+		Vdp::Reg_Status.setBit(VdpStatus::VDP_STATUS_PAL, false);	// NTSC: Clear the PAL bit.
 	}
 	
 	// Initialize audio.
@@ -284,9 +284,9 @@ FORCE_INLINE void EmuMD::T_execLine(void)
 	{
 		case LINETYPE_ACTIVEDISPLAY:
 			// In visible area.
-			Vdp::Reg_Status.setHBlank(true);	// HBlank = 1
+			Vdp::Reg_Status.setBit(VdpStatus::VDP_STATUS_HBLANK, true);	// HBlank = 1
 			M68K::Exec(M68K_Mem::Cycles_M68K - 404);
-			Vdp::Reg_Status.setHBlank(false);	// HBlank = 0
+			Vdp::Reg_Status.setBit(VdpStatus::VDP_STATUS_HBLANK, false);	// HBlank = 0
 			
 			if (--Vdp::HInt_Counter < 0)
 			{
@@ -311,13 +311,13 @@ FORCE_INLINE void EmuMD::T_execLine(void)
 			CONGRATULATIONS_PRECHECK();
 #endif
 			// VBlank = 1 et HBlank = 1 (retour de balayage vertical en cours)
-			Vdp::Reg_Status.setHBlank(true);
-			Vdp::Reg_Status.setVBlank(true);
+			Vdp::Reg_Status.setBit(VdpStatus::VDP_STATUS_HBLANK, true);
+			Vdp::Reg_Status.setBit(VdpStatus::VDP_STATUS_VBLANK, true);
 			
 			// If we're using NTSC V30 and this is an "even" frame,
 			// don't set the VBlank flag.
 			if (Vdp::VDP_Lines.NTSC_V30.VBlank_Div != 0)
-				Vdp::Reg_Status.setVBlank(false);
+				Vdp::Reg_Status.setBit(VdpStatus::VDP_STATUS_VBLANK, false);
 			
 			M68K::Exec(M68K_Mem::Cycles_M68K - 360);
 			Z80::Exec(168);
@@ -326,10 +326,10 @@ FORCE_INLINE void EmuMD::T_execLine(void)
 			CONGRATULATIONS_POSTCHECK();
 #endif
 			
-			Vdp::Reg_Status.setHBlank(false);	// HBlank = 0
+			Vdp::Reg_Status.setBit(VdpStatus::VDP_STATUS_HBLANK, false);	// HBlank = 0
 			if (Vdp::VDP_Lines.NTSC_V30.VBlank_Div == 0)
 			{
-				Vdp::Reg_Status.setVIntHappened(true);	// V Int happened
+				Vdp::Reg_Status.setBit(VdpStatus::VDP_STATUS_F, true);	// V Int happened
 				
 				Vdp::VDP_Int |= 0x8;
 				Vdp::Update_IRQ_Line();
@@ -409,9 +409,9 @@ FORCE_INLINE void EmuMD::T_execFrame(void)
 	// Both Interlaced Modes 1 and 2 set this bit on odd frames.
 	// This bit is cleared on even frames and if not running in interlaced mode.
 	if (Vdp::VDP_Reg.m5.Set4 & 0x06)
-		Vdp::Reg_Status.toggleOddFrame();
+		Vdp::Reg_Status.toggleBit(VdpStatus::VDP_STATUS_ODD);
 	else
-		Vdp::Reg_Status.clearOddFrame();
+		Vdp::Reg_Status.setBit(VdpStatus::VDP_STATUS_ODD, false);
 	
 	/** Main execution loops. **/
 	
@@ -428,8 +428,8 @@ FORCE_INLINE void EmuMD::T_execFrame(void)
 	}
 	
 	/** Visible line 0. **/
-	Vdp::HInt_Counter = Vdp::VDP_Reg.m5.H_Int;	// Initialize HInt_Counter.
-	Vdp::Reg_Status.setVBlank(false);		// Clear VBlank status.
+	Vdp::HInt_Counter = Vdp::VDP_Reg.m5.H_Int;			// Initialize HInt_Counter.
+	Vdp::Reg_Status.setBit(VdpStatus::VDP_STATUS_VBLANK, false);	// Clear VBlank flag.
 	
 	/** Loop 1: Active display. **/
 	do

src/libgens/Vdp/VdpIo.cpp
@@ -787,7 +787,7 @@ unsigned int Vdp::Update_DMA(void)
 	cycles >>= 16;
 	
 	// Clear the DMA Busy flag.
-	Reg_Status.setDMABusy(false);
+	Reg_Status.setBit(VdpStatus::VDP_STATUS_DMA, false);
 	
 	if (DMAT_Type & 2)
 	{
@@ -846,7 +846,7 @@ void Vdp::DMA_Fill(uint16_t data)
 	}
 	
 	// Set the DMA Busy flag.
-	Reg_Status.setDMABusy(true);
+	Reg_Status.setBit(VdpStatus::VDP_STATUS_DMA, true);
 	
 	// TODO: Although we decrement DMAT_Length correctly based on
 	// DMA cycles per line, we fill everything immediately instead
@@ -1171,7 +1171,7 @@ inline void Vdp::T_DMA_Loop(unsigned int src_address, uint16_t dest_address, int
 	if (DMAT_Length <= 0)
 	{
 		// No DMA left!
-		Reg_Status.setDMABusy(false);
+		Reg_Status.setBit(VdpStatus::VDP_STATUS_DMA, false);
 		return;
 	}
 	
@@ -1316,7 +1316,7 @@ void Vdp::Write_Ctrl(uint16_t data)
 	{
 		// DMA COPY.
 		src_address &= 0xFFFF;
-		Reg_Status.setDMABusy(true);	// Set the DMA BUSY bit.
+		Reg_Status.setBit(VdpStatus::VDP_STATUS_DMA, true);	// Set the DMA BUSY bit.
 		DMA_Length = 0;
 		DMAT_Length = length;
 		DMAT_Type = 0x3;
@@ -1397,7 +1397,7 @@ void Vdp::Write_Ctrl(uint16_t data)
 DMA_Src_OK:
 	
 	// Set the DMA BUSY bit.
-	Reg_Status.setDMABusy(true);
+	Reg_Status.setBit(VdpStatus::VDP_STATUS_DMA, true);
 	
 	switch (DMA_TYPE(src_component, dest_component))
 	{

src/libgens/Vdp/VdpRend_m5.cpp
@@ -501,7 +501,7 @@ FORCE_INLINE void Vdp::T_PutLine_Sprite(int disp_pixnum, uint32_t pattern, int p
 	
 	// Check for sprite collision.
 	if (status & LINEBUF_SPR_B)
-		Reg_Status.setCollision(true);
+		Reg_Status.setBit(VdpStatus::VDP_STATUS_COLLISION, true);
 }
 
 
@@ -1146,7 +1146,7 @@ FORCE_INLINE unsigned int Vdp::T_Update_Mask_Sprite(void)
 			{
 				// Sprite overflow!
 				// Set the SOVR flag.
-				Reg_Status.setSOvr(true);
+				Reg_Status.setBit(VdpStatus::VDP_STATUS_SOVR, true);
 				break;
 			}
 		}

commit 98a6fa1f6d372a7bf54d70dc148d514683f2a100

[libgens] VdpStatus::toggleBit(): Toggle should use ^=, not &=.
This is a regression from commit 4cdad1cf6e9c483a739da22ecb25bff11e37e0b5.
([libgens] Vdp/VdpStatus.hpp: Removed the convenience set/toggle/clear functions in favor of setBit() and toggleBit().)

src/libgens/Vdp/VdpStatus.hpp
@@ -112,7 +112,7 @@ inline void VdpStatus::setBit(StatusBits bit, bool value)
 }
 
 inline void VdpStatus::toggleBit(StatusBits bit)
-	{ m_status &= bit; }
+	{ m_status ^= bit; }
 
 }
 