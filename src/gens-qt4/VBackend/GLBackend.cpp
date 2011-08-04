GerbilSoft/gens-gs-ii

commit 54ae7608a84673794a19bc00db146d9b73d99c06

[libgens] Initial conversion of the Vdp class to non-static.
Lots of stuff is probably broken, and I need to convert GensConfig to
an instanced class instead of one giant class. Qt doesn't support
QObject templates, so the new GensConfig will use QVariant instead.

QVariant is probably slower, and it doesn't enforce types, but it'll work.

src/gens-qt4/VBackend/GLBackend.cpp
@@ -152,6 +152,14 @@ void GLBackend::reallocTexture(void)
 	if (m_tex > 0)
 		glDeleteTextures(1, &m_tex);
 	
+	// If we don't have an emulation context, don't allocate a texture for now.
+	// TODO: Intro effects.
+	if (!m_emuContext)
+	{
+		m_tex = 0;
+		return;
+	}
+	
 	// Create and initialize a GL texture.
 	// TODO: Add support for NPOT textures and/or GL_TEXTURE_RECTANGLE_ARB.
 	glEnable(GL_TEXTURE_2D);
@@ -166,7 +174,7 @@ void GLBackend::reallocTexture(void)
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, filterMethod);
 	
 	// Determine the texture format and type.
-	m_lastBpp = LibGens::Vdp::m_palette.bpp();
+	m_lastBpp = m_emuContext->m_vdp->m_palette.bpp();
 	switch (m_lastBpp)
 	{
 		case LibGens::VdpPalette::BPP_15:
@@ -482,6 +490,13 @@ void GLBackend::glb_paintGL(void)
 	glClearColor(1.0, 0.0, 0.0, 1.0);
 	glClear(GL_COLOR_BUFFER_BIT);
 	
+	if (!m_emuContext)
+	{
+		// No emulation context.
+		// TODO: Intro effects.
+		return;
+	}
+	
 	if (hasAspectRatioConstraintChanged())
 	{
 		// Aspect ratio constraint has changed.
@@ -494,7 +509,8 @@ void GLBackend::glb_paintGL(void)
 		// MD_Screen is dirty.
 		
 		// Check if the Bpp has changed.
-		if (LibGens::Vdp::m_palette.bpp() != m_lastBpp)
+		const LibGens::VdpPalette::ColorDepth bpp = m_emuContext->m_vdp->m_palette.bpp();
+		if (bpp != m_lastBpp)
 		{
 			// Bpp has changed. Reallocate the texture.
 			// VDP palettes will be recalculated on the next frame.
@@ -531,9 +547,9 @@ void GLBackend::glb_paintGL(void)
 		// TODO: Optimize this!
 		GLvoid *screen;
 		LibGens::MdFb *src_fb = (bFromMD
-				? &LibGens::Vdp::MD_Screen
+				? &m_emuContext->m_vdp->MD_Screen
 				: m_intScreen);
-		if (LibGens::Vdp::m_palette.bpp() != LibGens::VdpPalette::BPP_32)
+		if (bpp != LibGens::VdpPalette::BPP_32)
 			screen = src_fb->fb16();
 		else
 			screen = src_fb->fb32();
@@ -589,8 +605,8 @@ void GLBackend::glb_paintGL(void)
 	
 	// Check if the MD resolution has changed.
 	// If it has, recalculate the stretch mode rectangle.
-	const QSize mdResCur(LibGens::Vdp::GetHPix(),
-			     LibGens::Vdp::GetVPix());
+	const QSize mdResCur(m_emuContext->m_vdp->GetHPix(),
+			     m_emuContext->m_vdp->GetVPix());
 	if (mdResCur != m_stretchLastRes)
 		recalcStretchRectF();
 	
@@ -643,9 +659,19 @@ void GLBackend::glb_paintGL(void)
  */
 void GLBackend::recalcStretchRectF(GensConfig::StretchMode_t mode)
 {
-	// Store the current MD screen resolution.
-	m_stretchLastRes.setWidth(LibGens::Vdp::GetHPix());
-	m_stretchLastRes.setHeight(LibGens::Vdp::GetVPix());
+	if (m_emuContext)
+	{
+		// Store the current MD screen resolution.
+		m_stretchLastRes.setWidth(m_emuContext->m_vdp->GetHPix());
+		m_stretchLastRes.setHeight(m_emuContext->m_vdp->GetVPix());
+	}
+	else
+	{
+		// No emulation context.
+		// Assume 320x240 image for now.
+		m_stretchLastRes.setWidth(320);
+		m_stretchLastRes.setWidth(240);
+	}
 	
 	// Default to no stretch.
 	m_stretchRectF = QRectF(
@@ -655,9 +681,11 @@ void GLBackend::recalcStretchRectF(GensConfig::StretchMode_t mode)
 		((double)m_texVisSize.height() / (double)m_texSize.height())	// Height.
 		);
 	
-	// If stretch is disabled, we're done here.
-	// Also, stretch shouldn't be applied if emulation isn't running.
-	if (mode == GensConfig::STRETCH_NONE || !isRunning())
+	// Don't apply any stretch parameters if:
+	// - stretching is disabled
+	// - emulation isn't running
+	// - no emulation context is present (TODO: Combine isRunning() with m_emuContext?)
+	if (mode == GensConfig::STRETCH_NONE || !isRunning() || !m_emuContext)
 		return;
 	
 	// Horizontal stretch.
@@ -665,7 +693,7 @@ void GLBackend::recalcStretchRectF(GensConfig::StretchMode_t mode)
 	    mode == GensConfig::STRETCH_FULL)
 	{
 		// Horizontal stretch.
-		const int h_pix_begin = LibGens::Vdp::GetHPixBegin();
+		const int h_pix_begin = m_emuContext->m_vdp->GetHPixBegin();
 		if (h_pix_begin > 0)
 		{
 			// Less than 320 pixels wide.
@@ -681,7 +709,7 @@ void GLBackend::recalcStretchRectF(GensConfig::StretchMode_t mode)
 	    mode == GensConfig::STRETCH_FULL)
 	{
 		// Vertical stretch.
-		int v_pix = (240 - LibGens::Vdp::GetVPix());
+		int v_pix = (240 - m_emuContext->m_vdp->GetVPix());
 		if (v_pix > 0)
 		{
 			// Less than 240 pixels tall.

src/gens-qt4/EmuManager.cpp
@@ -553,7 +553,6 @@ int EmuManager::closeRom(bool emitStateChanged)
 		{
 			// Intro Effect is disabled.
 			// Clear the screen.
-			LibGens::Vdp::Reset();
 			emit updateVideo();
 		}
 		

src/gens-qt4/EmuManager_qEmu.cpp
@@ -487,24 +487,30 @@ void EmuManager::processQEmuRequest(void)
  */
 QImage EmuManager::getMDScreen(void) const
 {
+	if (!gqt4_emuContext)
+		return QImage();
+	
+	// Get the color depth.
+	const LibGens::VdpPalette::ColorDepth bpp = gqt4_emuContext->m_vdp->m_palette.bpp();
+	
 	// Create the QImage.
 	const uint8_t *start;
-	const int startY = ((240 - LibGens::Vdp::GetVPix()) / 2);
-	const int startX = (LibGens::Vdp::GetHPixBegin());
+	const int startY = ((240 - gqt4_emuContext->m_vdp->GetVPix()) / 2);
+	const int startX = (gqt4_emuContext->m_vdp->GetHPixBegin());
 	int bytesPerLine;
 	QImage::Format imgFormat;
 	
-	if (LibGens::Vdp::m_palette.bpp() == LibGens::VdpPalette::BPP_32)
+	if (bpp == LibGens::VdpPalette::BPP_32)
 	{
-		start = (const uint8_t*)(LibGens::Vdp::MD_Screen.lineBuf32(startY) + startX);
-		bytesPerLine = (LibGens::Vdp::MD_Screen.pxPerLine() * sizeof(uint32_t));
+		start = (const uint8_t*)(gqt4_emuContext->m_vdp->MD_Screen.lineBuf32(startY) + startX);
+		bytesPerLine = (gqt4_emuContext->m_vdp->MD_Screen.pxPerLine() * sizeof(uint32_t));
 		imgFormat = QImage::Format_RGB32;
 	}
 	else
 	{
-		start = (const uint8_t*)(LibGens::Vdp::MD_Screen.lineBuf16(startY) + startX);
-		bytesPerLine = (LibGens::Vdp::MD_Screen.pxPerLine() * sizeof(uint16_t));
-		if (LibGens::Vdp::m_palette.bpp() == LibGens::VdpPalette::BPP_16)
+		start = (const uint8_t*)(gqt4_emuContext->m_vdp->MD_Screen.lineBuf16(startY) + startX);
+		bytesPerLine = (gqt4_emuContext->m_vdp->MD_Screen.pxPerLine() * sizeof(uint16_t));
+		if (bpp == LibGens::VdpPalette::BPP_16)
 			imgFormat = QImage::Format_RGB16;
 		else
 			imgFormat = QImage::Format_RGB555;
@@ -512,8 +518,8 @@ QImage EmuManager::getMDScreen(void) const
 	
 	// TODO: Check for errors.
 	// TODO: Store timestamp, ROM filename, etc.
-	return QImage(start, LibGens::Vdp::GetHPix(),
-			LibGens::Vdp::GetVPix(),
+	return QImage(start, gqt4_emuContext->m_vdp->GetHPix(),
+			gqt4_emuContext->m_vdp->GetVPix(),
 			bytesPerLine, imgFormat);	
 }
 
@@ -833,27 +839,32 @@ void EmuManager::doChangePaletteSetting(EmuRequest_t::PaletteSettingType type, i
 	// NOTE: gens-qt4 uses values [-100,100] for contrast and brightness.
 	// libgens uses [0,200] for contrast and brightness.
 	
+	// TODO: Initialize palette settings on ROM startup.
+	if (!gqt4_emuContext)
+		return;
+	LibGens::VdpPalette *palette = &gqt4_emuContext->m_vdp->m_palette;
+	
 	switch (type)
 	{
 		case EmuRequest_t::RQT_PS_CONTRAST:
-			LibGens::Vdp::m_palette.setContrast(val + 100);
+			palette->setContrast(val + 100);
 			break;
 		
 		case EmuRequest_t::RQT_PS_BRIGHTNESS:
-			LibGens::Vdp::m_palette.setBrightness(val + 100);
+			palette->setBrightness(val + 100);
 			break;
 		
 		case EmuRequest_t::RQT_PS_GRAYSCALE:
-			LibGens::Vdp::m_palette.setGrayscale((bool)(!!val));
+			palette->setGrayscale((bool)(!!val));
 			break;
 		
 		case EmuRequest_t::RQT_PS_INVERTED:
-			LibGens::Vdp::m_palette.setInverted((bool)(!!val));
+			palette->setInverted((bool)(!!val));
 			break;
 		
 		case EmuRequest_t::RQT_PS_COLORSCALEMETHOD:
-			LibGens::Vdp::m_palette.setColorScaleMethod(
-						(LibGens::VdpPalette::ColorScaleMethod_t)val);
+			palette->setColorScaleMethod(
+					(LibGens::VdpPalette::ColorScaleMethod_t)val);
 			break;
 		
 		case EmuRequest_t::RQT_PS_INTERLACEDMODE:

src/gens-qt4/GensWindow.cpp
@@ -456,6 +456,8 @@ void GensWindow::osd(OsdType osd_type, int param)
  */
 void GensWindow::setBpp(LibGens::VdpPalette::ColorDepth newBpp)
 {
+	// TODO: Move to GensConfig/qEmu.
+#if 0
 	// TODO: bpp changes should be pushed to the emulation queue.
 	// TODO: Maybe this should be a slot called by GensConfig.
 	if (LibGens::Vdp::m_palette.bpp() == newBpp)
@@ -485,6 +487,7 @@ void GensWindow::setBpp(LibGens::VdpPalette::ColorDepth newBpp)
 	//: OSD message indicating color depth change.
 	const QString msg = tr("Color depth set to %1-bit.", "osd").arg(bppVal);
 	m_vBackend->osd_printqs(1500, msg);
+#endif
 }
 
 
@@ -495,6 +498,11 @@ void GensWindow::setBpp(LibGens::VdpPalette::ColorDepth newBpp)
  */
 void GensWindow::stateChanged(void)
 {
+	// TODO: Make sure that m_vBackend gets the new gqt4_emuContext in time.
+	// FIXME: This is probably a race condition.
+	// m_vBackend->setEmuContext() should be called when gqt4_emuContext changes.
+	m_vBackend->setEmuContext(gqt4_emuContext);
+	
 	if (m_emuManager->isRomOpen())
 	{
 		// ROM is open.
@@ -705,7 +713,6 @@ void GensWindow::introStyle_changed_slot(int newIntroStyle)
 	{
 		// Intro style was changed to "None", and emulation isn't running.
 		// Clear the screen.
-		LibGens::Vdp::Reset();
 		updateVideo();
 	}
 }

src/gens-qt4/VBackend/VBackend.cpp
@@ -829,5 +829,23 @@ int VBackend::recSetDuration(const QString& component, int duration)
 	return 0;
 }
 
+
+/**
+ * setEmuContext(): Set the emulation context for this video backend.
+ * @param newEmuContext New emulation context.
+ */
+void VBackend::setEmuContext(LibGens::EmuContext *newEmuContext)
+{
+	if (m_emuContext == newEmuContext)
+		return;
+	
+	m_emuContext = newEmuContext;
+	setVbDirty();
+	setMdScreenDirty();
+	
+	// TODO: Should we update the video buffer here?
+	if (!isRunning() || isPaused())
+		vbUpdate();
 }
 
+}

src/gens-qt4/VBackend/VBackend.hpp
@@ -44,6 +44,9 @@
 // TODO: Move somewhere else, or use int?
 #include "Config/GensConfig.hpp"
 
+// Emulation Context.
+#include "libgens/EmuContext.hpp"
+
 
 namespace GensQt4
 {
@@ -88,6 +91,10 @@ class VBackend : public QWidget
 		QColor osdFpsColor(void) const;
 		bool osdMsgEnabled(void) const;
 		QColor osdMsgColor(void) const;
+		
+		/** Emulation Context. **/
+		LibGens::EmuContext *emuContext(void) const;
+		void setEmuContext(LibGens::EmuContext *newEmuContext);
 		
 		/** Format strings. **/
 		
@@ -185,6 +192,9 @@ class VBackend : public QWidget
 		bool bilinearFilter(void) const;
 		bool pauseTint(void) const;
 		GensConfig::StretchMode_t stretchMode(void) const;
+		
+		/** Emulation Context. **/
+		LibGens::EmuContext *m_emuContext;
 	
 	protected slots:
 		/** Properties. **/
@@ -312,6 +322,9 @@ inline bool VBackend::pauseTint(void) const
 	{ return m_pauseTint; }
 inline GensConfig::StretchMode_t VBackend::stretchMode(void) const
 	{ return m_stretchMode; }
+
+inline LibGens::EmuContext *VBackend::emuContext(void) const
+	{ return m_emuContext; }
 
 }
 

src/libgens/CMakeLists.txt
@@ -98,7 +98,7 @@ SET(libgens_SRCS
 	Save/EEPRom_DB.cpp
 	Data/mcd_rom_db.c
 	Data/32X/fw_32x.c
-	Vdp/VdpRend_Err.cpp
+	#Vdp/VdpRend_Err.cpp
 	Vdp/VGA_charset.c
 	Util/MdFb.cpp
 	Vdp/VdpStatus.cpp

src/libgens/Effects/CrazyEffect.cpp
@@ -162,6 +162,8 @@ inline void CrazyEffect::T_doCrazyEffect(pixel *screen)
  */
 void CrazyEffect::run(void)
 {
+	// TODO: Update to not use the Vdp class.
+#if 0
 	switch (Vdp::m_palette.bpp())
 	{
 		case VdpPalette::BPP_15:
@@ -181,6 +183,7 @@ void CrazyEffect::run(void)
 					(Vdp::MD_Screen.fb32());
 			break;
 	}
+#endif
 }
 
 }

commit d6fabe56f7cf9dd7c1e264e27ba19703bb902444

[gens-qt4] GLBackend::glb_paintGL(): Draw the OSD preview and text if emulation isn't running.
Otherwise, onscreen display messages won't show up if the emulator is idle.

This is a regression from commit 54ae7608a84673794a19bc00db146d9b73d99c06.
([libgens] Initial conversion of the Vdp class to non-static.)

src/gens-qt4/VBackend/GLBackend.cpp
@@ -496,6 +496,9 @@ void GLBackend::glb_paintGL(void)
 	{
 		// No emulation context.
 		// TODO: Intro effects.
+		showOsdPreview();	// Display the OSD preview image.
+		printOsdText();		// Print the OSD text to the screen.
+		m_vbDirty = false;	// Video backend is no longer dirty.
 		return;
 	}
 	
@@ -641,17 +644,11 @@ void GLBackend::glb_paintGL(void)
 		}
 	}
 	
-	// Disable 2D textures.
-	glDisable(GL_TEXTURE_2D);
-	
-	// Display the OSD preview image.
-	showOsdPreview();
-	
-	// Print the OSD text to the screen.
-	printOsdText();
-	
-	// Video backend is no longer dirty.
-	m_vbDirty = false;
+	// Finish up drawing.
+	glDisable(GL_TEXTURE_2D);	// Disable 2D textures.
+	showOsdPreview();		// Display the OSD preview image.
+	printOsdText();			// Print the OSD text to the screen.
+	m_vbDirty = false;		// Video backend is no longer dirty.
 }
 
 