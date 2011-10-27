a8f8d66faa6e45e848420f2c027473b11dce9de9
David Korth <gerbilsoft@verizon.net>
2011-10-26 23:38:14 -0400

    [libgens] VdpTypes::VdpReg_t.m4_t: Added register layout descriptions for M4 registers.
    TODO: Add descriptions for each of the bits.

--- src/libgens/Vdp/VdpTypes.hpp
+++ src/libgens/Vdp/VdpTypes.hpp
@@ -76,6 +76,7 @@ namespace VdpTypes
 		{
 			/**
 			 * Mode 5 (MD) registers.
+			 *
 			 * DISP == Display Enable. (1 == on; 0 == off)
 			 * IE0 == Enable V interrupt. (1 == on; 0 == off)
 			 * IE1 == Enable H interrupt. (1 == on; 0 == off)
@@ -96,8 +97,8 @@ namespace VdpTypes
 			 * VSZ1/VSZ2 == Vertical scroll size. (00 == 32 cells; 01 == 64 cells; 10 == invalid; 11 == 128 cells)
 			 * HSZ1/HSZ2 == Vertical scroll size. (00 == 32 cells; 01 == 64 cells; 10 == invalid; 11 == 128 cells)
 			 */
-			uint8_t Set1;		// Mode Set 1.  [   0    0   LCB  IE1    0 PSEL   M3    0]
-			uint8_t Set2;		// Mode Set 2.  [   0 DISP   IE0   M1   M2   M5    0    0]
+			uint8_t Set1;		// Mode Set 1. [   0    0   LCB  IE1    0 PSEL   M3    0]
+			uint8_t Set2;		// Mode Set 2. [   0 DISP   IE0   M1   M2   M5    0    0]
 			uint8_t Pat_ScrA_Adr;	// Pattern name table base address for Scroll A.
 			uint8_t Pat_Win_Adr;	// Pattern name table base address for Window.
 			uint8_t Pat_ScrB_Adr;	// Pattern name table base address for Scroll B.
@@ -107,8 +108,8 @@ namespace VdpTypes
 			uint8_t Reg8;		// unused
 			uint8_t Reg9;		// unused
 			uint8_t H_Int;		// H Interrupt.
-			uint8_t Set3;		// Mode Set 3.  [   0    0    0    0  IE2 VSCR HSCR LSCR]
-			uint8_t Set4;		// Mode Set 4.  [ RS0    0    0    0 S/TE LSM1 LSM0  RS1]
+			uint8_t Set3;		// Mode Set 3. [   0    0    0    0  IE2 VSCR HSCR LSCR]
+			uint8_t Set4;		// Mode Set 4. [ RS0    0    0    0 S/TE LSM1 LSM0  RS1]
 			uint8_t H_Scr_Adr;	// H Scroll Data Table base address.
 			uint8_t Reg14;		// unused
 			uint8_t Auto_Inc;	// Auto Increment.
@@ -128,18 +129,24 @@ namespace VdpTypes
 			* Mode 4 (SMS) registers.
 			* NOTE: Mode 4 is currently not implemented.
 			* This is here for future use.
+			*
+			* On SMS1, address bits with asterisks are bitwise-AND'ed
+			* with the requested cell address. On SMS2/GG, these bits
+			* are ignored.
+			*
+			* TODO: Add register descriptions.
 			*/
-			uint8_t Set1;		// Mode Set 1.
-			uint8_t Set2;		// Mode Set 2.
-			uint8_t NameTbl_Addr;	// Name table base address.
+			uint8_t Set1;		// Mode Set 1. [ VSI  HSI  LCB  IE1   SS   M4   M3   ES]
+			uint8_t Set2;		// Mode Set 2. [   1 DISP  IE0   M1   M2    0   SZ  MAG]
+			uint8_t NameTbl_Addr;	// Name table base address. [0 0 0 0 A13 A12 A11 *A10]
 			uint8_t ColorTbl_Addr;	// Color table base address.
 			uint8_t	Pat_BG_Addr;	// Background Pattern Generator base address.
-			uint8_t Spr_Att_Addr;	// Sprite Attribute Table base address.
-			uint8_t Spr_Pat_addr;	// Sprite Pattern Generator base address.
-			uint8_t BG_Color;	// Background color.
-			uint8_t H_Scroll;	// Horizontal scroll.
-			uint8_t V_Scroll;	// Vertical scroll.
-			uint8_t H_Int;		// H Interrupt.
+			uint8_t Spr_Att_Addr;	// Sprite Attribute Table base address. [0 A13 A12 A11 A10 A9 A8 *A7]
+			uint8_t Spr_Pat_addr;	// Sprite Pattern Generator base address. [0 0 0 0 0 A13 *A12 *A11]
+			uint8_t BG_Color;	// Background color. [0 0 0 0 BG3 BG2 BG1 BG0]
+			uint8_t H_Scroll;	// Horizontal scroll. [8-bit]
+			uint8_t V_Scroll;	// Vertical scroll. [8-bit]
+			uint8_t H_Int;		// H Interrupt. [8-bit]
 		};
 		m4_t m4;
 	};
