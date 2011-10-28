b77d77a3dc2e752e2737d87deabff557e3cfc5dd
David Korth <gerbilsoft@verizon.net>
2011-10-27 21:32:29 -0400

    [libgens] Vdp/VdpReg.hpp: Improved the descriptions for Mode 5 registers.

--- src/libgens/Vdp/VdpReg.hpp
+++ src/libgens/Vdp/VdpReg.hpp
@@ -39,53 +39,171 @@ namespace VdpTypes
 		{
 			/**
 			 * Mode 5 (MD) registers.
-			 *
-			 * DISP == Display Enable. (1 == on; 0 == off)
-			 * IE0 == Enable V interrupt. (1 == on; 0 == off)
-			 * IE1 == Enable H interrupt. (1 == on; 0 == off)
-			 * IE2 == Enable external interrupt. (1 == on; 0 == off)
-			 * M1 == DMA Enable. (1 == on; 0 == off)
-			 * M2 == V cell mode. (1 == V30 [PAL only]; 0 == V28)
-			 * M3 == HV counter latch. (1 == stop HV counter; 0 == enable read, H, V counter)
-			 * M4/PSEL == Palette Select; if clear, masks high two bits of each CRam color component.
-			 *            If M5 is off, acts like M4 instead of PSEL.
-			 * M5 == Mode 4/5 toggle; set for Mode 5, clear for Mode 4.
-			 * LCB == Left Column Blank. SMS VDP holdover; if set, masks the first 8 pixels
-			 *        with the background color.
-			 * VSCR == V Scroll mode. (0 == full; 1 == 2-cell)
-			 * HSCR/LSCR == H Scroll mode. (00 == full; 01 == invalid; 10 == 1-cell; 11 == 1-line)
-			 * RS0/RS1 == H cell mode. (11 == H40; 00 == H32; others == invalid)
-			 * LSM1/LSM0 == Interlace mode. (00 == normal; 01 == interlace mode 1; 10 == invalid; 11 == interlace mode 2)
-			 * S/TE == Highlight/Shadow enable. (1 == on; 0 == off)
-			 * VSZ1/VSZ2 == Vertical scroll size. (00 == 32 cells; 01 == 64 cells; 10 == invalid; 11 == 128 cells)
-			 * HSZ1/HSZ2 == Vertical scroll size. (00 == 32 cells; 01 == 64 cells; 10 == invalid; 11 == 128 cells)
 			 */
-			uint8_t Set1;		// Mode Set 1. [   0    0   LCB  IE1    0 PSEL   M3    0]
-			uint8_t Set2;		// Mode Set 2. [   0 DISP   IE0   M1   M2   M5    0    0]
-			uint8_t Pat_ScrA_Adr;	// Pattern name table base address for Scroll A.
-			uint8_t Pat_Win_Adr;	// Pattern name table base address for Window.
-			uint8_t Pat_ScrB_Adr;	// Pattern name table base address for Scroll B.
-			uint8_t Spr_Att_Adr;	// Sprite Attribute Table base address.
-			uint8_t Reg6;		// unused
-			uint8_t BG_Color;	// Background color.
-			uint8_t Reg8;		// unused
-			uint8_t Reg9;		// unused
-			uint8_t H_Int;		// H Interrupt.
-			uint8_t Set3;		// Mode Set 3. [   0    0    0    0  IE2 VSCR HSCR LSCR]
-			uint8_t Set4;		// Mode Set 4. [ RS0    0    0    0 S/TE LSM1 LSM0  RS1]
-			uint8_t H_Scr_Adr;	// H Scroll Data Table base address.
-			uint8_t Reg14;		// unused
-			uint8_t Auto_Inc;	// Auto Increment.
-			uint8_t Scr_Size;	// Scroll Size. [   0    0 VSZ1 VSZ0    0    0 HSZ1 HSZ0]
-			uint8_t Win_H_Pos;	// Window H position.
-			uint8_t Win_V_Pos;	// Window V position.
-			uint8_t DMA_Length_L;	// DMA Length Counter Low.
-			uint8_t DMA_Length_H;	// DMA Length Counter High.
-			uint8_t DMA_Src_Adr_L;	// DMA Source Address Low.
-			uint8_t DMA_Src_Adr_M;	// DMA Source Address Mid.
-			uint8_t DMA_Src_Adr_H;	// DMA Source Address High.
+			
+			/**
+			 * Register 0: Mode Set 1.
+			 * [   0    0   LCB  IE1    0 PSEL   M3    0]
+			 * 
+			 * LCB: Left Column Blank. SMS VDP leftover; if set, masks the first 8 pixels
+			 *      with the background color.
+			 * IE1: Enable H interrupt. (1 == on; 0 == off)
+			 * M4/PSEL: Palette Select. If clear, masks high two bits of each CRam color component.
+			 *          If M5 is off, acts like M4 instead of PSEL.
+			 * M3: HV counter latch. (1 == stop HV counter; 0 == enable read, H, V counter)
+			 */
+			uint8_t Set1;
+			
+			/**
+			 * Register 1: Mode Set 2.
+			 * [   0 DISP   IE0   M1   M2   M5    0    0]
+			 * 
+			 * DISP: Display Enable. (1 == on; 0 == off)
+			 * IE0: Enable V interrupt. (1 == on; 0 == off)
+			 * M1: DMA Enable. (1 == on; 0 == off)
+			 * M2: V resolution. (1 == V30 [PAL only]; 0 == V28)
+			 * M5: Mode 4/5 toggle. (1 == M5; 0 == M4)
+			 */
+			uint8_t Set2;
+			
+			/**
+			 * Register 2: Pattern name table base address for Scroll A.
+			 * [   x    x SA15 SA14 SA13    x    x    x]
+			 */
+			uint8_t Pat_ScrA_Adr;
+			
+			/**
+			 * Register 3: Pattern name table base address for Window.
+			 * [   x    x WD15 WD14 WD13 WD12 WD11    x]
+			 */
+			uint8_t Pat_Win_Adr;
+			
+			/**
+			 * Register 4: Pattern name table base address for Scroll B.
+			 * [   x    x    x    x    x SB15 SB14 SB13]
+			 */
+			uint8_t Pat_ScrB_Adr;
+			
+			/**
+			 * Register 5: Sprite Attribute Table base address.
+			 * [   x AT15 AT14 AT13 AT12 AT11 AT10  AT9]
+			 */
+			uint8_t Spr_Att_Adr;
+			
+			/**
+			 * Register 6: Unused.
+			 */
+			uint8_t Reg6;
+			
+			/**
+			 * Register 7: Background color.
+			 * [   x    x CPT1 CPT0 COL3 COL2 COL1 COL0]
+			 */
+			uint8_t BG_Color;
+			
+			/**
+			 * Register 8: Unused.
+			 */
+			uint8_t Reg8;
+			
+			/**
+			 * Register 9: Unused.
+			 */
+			uint8_t Reg9;
+			
+			/**
+			 * Register 10: H Interrupt register.
+			 * [HIT7 HIT6 HIT5 HIT4 HIT3 HIT2 HIT1 HIT0]
+			 */
+			uint8_t H_Int;
+			
+			/**
+			 * Register 11: Mode Set 3.
+			 * [   0    0    0    0  IE2 VSCR HSCR LSCR]
+			 * 
+			 * IE2: Enable external interrupt. (1 == on; 0 == off)
+			 * VSCR: V Scroll mode. (0 == full; 1 == 2-cell)
+			 * HSCR/LSCR: H Scroll mode.
+			 *   - 00 == full
+			 *   - 01 == invalid (first 8 entries used for whole screen)
+			 *   - 10 == per-cell
+			 *   - 11 == per-line
+			 */
+			uint8_t Set3;
+			
+			/**
+			 * Register 12: Mode Set 4.
+			 * [ RS0    0    0    0 S/TE LSM1 LSM0  RS1]
+			 * 
+			 * RS0/RS1: H resolution. (00 == H32; 11 == H40; others == invalid)
+			 * S/TE: Shadow/Highlight enable. (1 == on; 0 == off)
+			 * LSM1/LSM0: Interlace mode.
+			 *   - 00 == normal
+			 *   - 01 == interlace mode 1
+			 *   - 10 == invalid (acts like 00)
+			 *   - 11 == interlace mode 2
+			 */
+			uint8_t Set4;
+			
+			/**
+			 * Register 13: H Scroll Data Table base address.
+			 * [   x    x HS15 HS14 HS13 HS12 HS11 HS10]
+			 */
+			uint8_t H_Scr_Adr;
+			
+			/**
+			 * Register 14: Unused.
+			 */
+			uint8_t Reg14;
+			
+			/**
+			 * Register 15: Auto Increment Data.
+			 * [INC7 INC6 INC5 INC4 INC3 INC2 INC1 INC0]
+			 */
+			uint8_t Auto_Inc;
+			
+			/**
+			 * Register 16: Scroll Size.
+			 * [   0    0 VSZ1 VSZ2    0    0 HSZ1 HSZ0]
+			 * 
+			 * VSZ1/VSZ2: Vertical scroll size.
+			 * HSZ1/HSZ2: Horizontal scroll size.
+			 * 
+			 * Scroll sizes:
+			 * - 00 == 32 cells
+			 * - 01 == 64 cells
+			 * - 10 == invalid
+			 * - 11 == 128 cells
+			 */
+			uint8_t Scr_Size;
+			
+			/**
+			 * Register 17: Window H position.
+			 * [RIGT    0    0 WHP5 WHP4 WHP3 WHP2 WHP1]
+			 */
+			uint8_t Win_H_Pos;
+			
+			/**
+			 * Register 18: Window V position.
+			 * [DOWN    0    0 WVP5 WVP4 WVP3 WVP2 WVP1]
+			 */
+			uint8_t Win_V_Pos;
+			
+			/**
+			 * Registers 19, 20: DMA Length counter.
+			 */
+			uint8_t DMA_Length_L;
+			uint8_t DMA_Length_H;
+			
+			/**
+			 * Registers 21, 22, 23: DMA Source address.
+			 */
+			uint8_t DMA_Src_Adr_L;
+			uint8_t DMA_Src_Adr_M;
+			uint8_t DMA_Src_Adr_H;
 		};
 		m5_t m5;
+		
 		struct m4_t
 		{
 			/**
