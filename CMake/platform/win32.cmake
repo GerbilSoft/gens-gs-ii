3e48c4aec14bfa5d070aa547e7e7ef321094df4a
David Korth <gerbilsoft@verizon.net>
2011-11-01 21:20:29 -0400

    CMake/platform/win32.cmake: Added --large-address-aware, --nxcompat, and --dynamicbase.
    --large-address-aware allows Gens/GS II to use more than 2 GB of virtual
    address space on Win64. (Up to 4 GB when compiled as 32-bit, and more
    when compiled as 64-bit.)
    
    --nxcompat enables DEP/NX (Windows XP SP2 or later), and --dynamicbase
    enables ASLR (Windows Vista or later).
    
    This shouldn't break anything, especially considering all of these options
    have been effectively "enabled" on the Linux port already. (Linux doesn't
    require programs to specifically enable things like NX or large address
    spaces; they're enabled by default.)

--- CMake/platform/win32.cmake
+++ CMake/platform/win32.cmake
@@ -11,7 +11,7 @@ ENDIF(CMAKE_BUILD_TYPE MATCHES ^debug)
 # Test for static libgcc/libstdc++.
 # NOTE: libstdc++ check removed for now.
 SET(GENS_LDFLAGS_PLATFORM "")
-FOREACH(FLAG_TEST "-static-libgcc")# "-static-libstdc++")
+FOREACH(FLAG_TEST "-static-libgcc" "-Wl,--large-address-aware" "-Wl,--nxcompat" "-Wl,--dynamicbase") # "-static-libstdc++")
 	CHECK_C_COMPILER_FLAG("${FLAG_TEST}" LDFLAG_${FLAG_TEST})
 	IF(LDFLAG_${FLAG_TEST})
 		SET(GENS_LDFLAGS_PLATFORM "${GENS_LDFLAGS_PLATFORM} ${FLAG_TEST}")
