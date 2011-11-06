0b5a2dc5b05eef05fe9296b47b5969c43cfcd2ab
David Korth <gerbilsoft@verizon.net>
2011-11-05 21:59:34 -0400

    [gens-qt4] VBackend/MsgTimer.cpp: Converted to a regular class with a private class for storing data.

--- src/gens-qt4/VBackend/MsgTimer.cpp
+++ src/gens-qt4/VBackend/MsgTimer.cpp
@@ -1,12 +1,12 @@
 /***************************************************************************
  * gens-qt4: Gens Qt4 UI.                                                  *
- * MsgTimer.hpp: Message timer class.                                      *
+ * MsgTimer.cpp: Message timer class.                                      *
  * This class ensures that OSD messages are removed from the screen after  *
  * their duration expires when emulation isn't running.                    *
  *                                                                         *
  * Copyright (c) 1999-2002 by Stéphane Dallongeville.                      *
  * Copyright (c) 2003-2004 by Stéphane Akhoun.                             *
- * Copyright (c) 2008-2010 by David Korth.                                 *
+ * Copyright (c) 2008-2011 by David Korth.                                 *
  *                                                                         *
  * This program is free software; you can redistribute it and/or modify it *
  * under the terms of the GNU General Public License as published by the   *
@@ -23,49 +23,60 @@
  * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.           *
  ***************************************************************************/
 
-#ifndef __GENS_QT4_VBACKEND_MSGTIMER_HPP__
-#define __GENS_QT4_VBACKEND_MSGTIMER_HPP__
+#include "MsgTimer.hpp"
 
 // Qt includes.
 #include <QtCore/QObject>
 #include <QtCore/QTimer>
 
+// Video Backend.
+#include "VBackend.hpp"
+
 namespace GensQt4
 {
 
-// VBackend.
-class VBackend;
-
-class MsgTimerPrivate;
-
-class MsgTimer : public QObject
+class MsgTimerPrivate
 {
-	Q_OBJECT
-	
 	public:
-		MsgTimer(VBackend *vBackend);
-		~MsgTimer();
-	
-	private:
-		MsgTimerPrivate *d;
-		Q_DISABLE_COPY(MsgTimer);
-	
-	public:
-		// 100 ms is a decent interval when emulation isn't running.
-		static const int MSGTIMER_INTERVAL = 100;
-		
-		/**
-		 * Start the message timer.
-		 */
-		void start(void);
-	
-	private slots:
-		/**
-		 * Check the OSD messages.
-		 */
-		void checkMsg(void);
+		VBackend *vBackend;
+		QTimer *timer;
 };
 
+MsgTimer::MsgTimer(VBackend *vBackend)
+	: d(new MsgTimerPrivate())
+{
+	d->vBackend = vBackend;
+	
+	// Initialize the QTimer.
+	d->timer = new QTimer(this);
+	connect(d->timer, SIGNAL(timeout()),
+		this, SLOT(checkMsg()));
 }
 
-#endif /* __GENS_QT4_VBACKEND_MSGTIMER_HPP__ */
+MsgTimer::~MsgTimer()
+	{ delete d; }
+
+/**
+ * Start the message timer.
+ */
+void MsgTimer::start(void)
+	{ d->timer->start(MSGTIMER_INTERVAL); }
+
+/**
+ * Check the OSD messages.
+ */
+void MsgTimer::checkMsg(void)
+{
+	// Message timer tick.
+	// Check the VBackend for messages.
+	int num = d->vBackend->osd_process();
+	
+	if (num <= 0)
+	{
+		// No more messages.
+		// Stop the timer.
+		d->timer->stop();
+	}
+}
+
+}
