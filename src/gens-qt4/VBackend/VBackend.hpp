GerbilSoft/gens-gs-ii

commit fea787eac31b0d203c86ac6535fcf06ac5c06498

VBackend/FpsManager.cpp; Split the FPS manager code from VBackend into a separate class.

src/gens-qt4/VBackend/VBackend.hpp
@@ -46,6 +46,9 @@
 // Emulation Context.
 #include "libgens/EmuContext.hpp"
 
+// FPS manager.
+#include "FpsManager.hpp"
+
 // gqt4_cfg
 #include "gqt4_main.hpp"
 
@@ -213,8 +216,8 @@ class VBackend : public QWidget
 		virtual void osd_show_preview(int duration, const QImage& img);
 		
 		// FPS manager.
-		void resetFps(void);
-		void pushFps(double fps);
+		void fpsReset(void);
+		void fpsPush(double fps);
 		
 		// Recording OSD.
 		int recSetStatus(const QString& component, bool isRecording);
@@ -244,9 +247,8 @@ class VBackend : public QWidget
 		void setOsdListDirty(void);
 		void clearOsdListDirty(void);
 		
-		// Get the current average FPS.
-		inline double fpsAvg(void)
-			{ return m_fpsAvg; }
+		// FPS manager.
+		FpsManager m_fpsManager;
 		
 		// Preview image.
 		struct PreviewImage
@@ -348,6 +350,12 @@ inline void VBackend::setOsdListDirty(void)
 	{ m_osdListDirty = true; }
 inline void VBackend::clearOsdListDirty(void)
 	{ m_osdListDirty = false; }
+
+// FPS manager.
+inline void VBackend::fpsReset(void)
+	{ m_fpsManager.reset(); }
+inline void VBackend::fpsPush(double fps)
+	{ m_fpsManager.push(fps); }
 
 /** Recording OSD. **/
 inline int VBackend::recStart(const QString& component)

src/gens-qt4/VBackend/VBackend_OSD.cpp
@@ -197,54 +197,6 @@ void VBackend::osd_show_preview(int duration, const QImage& img)
 }
 
 
-/*! FPS **/
-
-
-/**
- * Reset the FPS manager.
- */
-void VBackend::resetFps(void)
-{
-	for (size_t i = 0; i < (sizeof(m_fps)/sizeof(m_fps[0])); i++)
-		m_fps[i] = -1.0;
-	m_fpsAvg = 0.0;
-	m_fpsPtr = 0;
-	
-	if (osdFpsEnabled() && isRunning() && !isPaused())
-		setOsdListDirty();
-}
-
-/**
- * Push an FPS value.
- * @param fps FPS value.
- */
-void VBackend::pushFps(double fps)
-{
-	m_fpsPtr = (m_fpsPtr + 1) % (sizeof(m_fps)/sizeof(m_fps[0]));
-	m_fps[m_fpsPtr] = fps;
-	
-	// Calculate the new average.
-	int count = 0;
-	double sum = 0;
-	for (size_t i = 0; i < (sizeof(m_fps)/sizeof(m_fps[0])); i++)
-	{
-		if (m_fps[i] >= 0.0)
-		{
-			sum += m_fps[i];
-			count++;
-		}
-	}
-	
-	if (count <= 0)
-		m_fpsAvg = 0.0;
-	else
-		m_fpsAvg = (sum / (double)count);
-	
-	if (osdFpsEnabled() && isRunning() && !isPaused())
-		setOsdListDirty();
-}
-
-
 /*! Recording status. **/
 
 

src/gens-qt4/CMakeLists.txt
@@ -107,6 +107,7 @@ SET(gens-qt4_COMMON_SRCS
 	SigHandler.cpp
 	VBackend/VBackend.cpp
 	VBackend/VBackend_OSD.cpp
+	VBackend/FpsManager.cpp
 	VBackend/GensQGLWidget.cpp
 	VBackend/GLBackend.cpp
 	Input/KeyHandlerQt.cpp

src/gens-qt4/GensWindow.cpp
@@ -513,7 +513,7 @@ void GensWindow::stateChanged(void)
 		
 		m_vBackend->osd_show_preview(0, QImage());
 		m_vBackend->setPaused(unPause);
-		m_vBackend->resetFps();
+		m_vBackend->fpsReset();
 	}
 	
 	// Check the idle thread state.

src/gens-qt4/GensWindow.hpp
@@ -155,7 +155,7 @@ class GensWindow : public QMainWindow
 		 * updateFps(): Update the FPS counter.
 		 */
 		void updateFps(double fps)
-			{ m_vBackend->pushFps(fps); }
+			{ m_vBackend->fpsPush(fps); }
 		
 		/**
 		 * stateChanged(): Emulation state changed.

src/gens-qt4/VBackend/FpsManager.cpp
@@ -1,0 +1,82 @@
+/***************************************************************************
+ * gens-qt4: Gens Qt4 UI.                                                  *
+ * FpsManager.cpp: FPS Manager class.                                      *
+ *                                                                         *
+ * Copyright (c) 1999-2002 by Stéphane Dallongeville.                      *
+ * Copyright (c) 2003-2004 by Stéphane Akhoun.                             *
+ * Copyright (c) 2008-2012 by David Korth.                                 *
+ *                                                                         *
+ * This program is free software; you can redistribute it and/or modify it *
+ * under the terms of the GNU General Public License as published by the   *
+ * Free Software Foundation; either version 2 of the License, or (at your  *
+ * option) any later version.                                              *
+ *                                                                         *
+ * This program is distributed in the hope that it will be useful, but     *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ * GNU General Public License for more details.                            *
+ *                                                                         *
+ * You should have received a copy of the GNU General Public License along *
+ * with this program; if not, write to the Free Software Foundation, Inc., *
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.           *
+ ***************************************************************************/
+
+#include "FpsManager.hpp"
+
+// C includes. (C++ namespace)
+#include <cstddef>
+
+namespace GensQt4
+{
+
+FpsManager::FpsManager()
+	: m_fpsAvg(0.0)
+	, m_fpsPtr(0)
+{
+	// Reset the FPS array.
+	for (size_t i = 0; i < (sizeof(m_fps)/sizeof(m_fps[0])); i++)
+		m_fps[i] = -1.0;
+}
+
+/**
+ * Reset the FPS manager.
+ */
+void FpsManager::reset(void)
+{
+	// Clear the FPS average and reset the pointer.
+	m_fpsAvg = 0.0;
+	m_fpsPtr = 0;
+	
+	// Reset the FPS array.
+	for (size_t i = 0; i < (sizeof(m_fps)/sizeof(m_fps[0])); i++)
+		m_fps[i] = -1.0;
+}
+
+/**
+ * Push an FPS value.
+ * @param fps FPS value.
+ */
+void FpsManager::push(double fps)
+{
+	m_fpsPtr = (m_fpsPtr + 1) % (sizeof(m_fps)/sizeof(m_fps[0]));
+	m_fps[m_fpsPtr] = fps;
+	
+	// Calculate the new average.
+	int count = 0;
+	double sum = 0;
+	for (size_t i = 0; i < (sizeof(m_fps)/sizeof(m_fps[0])); i++)
+	{
+		if (m_fps[i] >= 0.0)
+		{
+			sum += m_fps[i];
+			count++;
+		}
+	}
+	
+	if (count <= 0)
+		m_fpsAvg = 0.0;
+	else
+		m_fpsAvg = (sum / (double)count);
+}
+
+}

src/gens-qt4/VBackend/FpsManager.hpp
@@ -1,0 +1,67 @@
+/***************************************************************************
+ * gens-qt4: Gens Qt4 UI.                                                  *
+ * FpsManager.hpp: FPS Manager class.                                      *
+ *                                                                         *
+ * Copyright (c) 1999-2002 by Stéphane Dallongeville.                      *
+ * Copyright (c) 2003-2004 by Stéphane Akhoun.                             *
+ * Copyright (c) 2008-2012 by David Korth.                                 *
+ *                                                                         *
+ * This program is free software; you can redistribute it and/or modify it *
+ * under the terms of the GNU General Public License as published by the   *
+ * Free Software Foundation; either version 2 of the License, or (at your  *
+ * option) any later version.                                              *
+ *                                                                         *
+ * This program is distributed in the hope that it will be useful, but     *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ * GNU General Public License for more details.                            *
+ *                                                                         *
+ * You should have received a copy of the GNU General Public License along *
+ * with this program; if not, write to the Free Software Foundation, Inc., *
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.           *
+ ***************************************************************************/
+
+#ifndef __GENS_QT4_VBACKEND_FPSMANAGER_HPP__
+#define __GENS_QT4_VBACKEND_FPSMANAGER_HPP__
+
+namespace GensQt4
+{
+
+class FpsManager
+{
+	public:
+		FpsManager();
+		
+		/**
+		 * Reset the FPS manager.
+		 */
+		void reset(void);
+		
+		/**
+		 * Push an FPS value.
+		 * @param fps FPS value.
+		 */
+		void push(double fps);
+		
+		/**
+		 * Get the current FPS.
+		 * @return Current FPS.
+		 */
+		double get(void);
+	
+	private:
+		double m_fps[8];
+		double m_fpsAvg;	// Average fps.
+		int m_fpsPtr;		// Pointer to next fps slot to use.
+};
+
+/**
+ * Get the current FPS.
+ * @return Current FPS.
+ */
+inline double FpsManager::get(void)
+	{ return m_fpsAvg; }
+
+}
+
+#endif /* __GENS_QT4_VBACKEND_FPSMANAGER_HPP__ */

src/gens-qt4/VBackend/GLBackend.cpp
@@ -811,7 +811,7 @@ void GLBackend::printOsdText(void)
 	// Check if the FPS should be drawn.
 	if (isRunning() && !isPaused() && osdFpsEnabled())
 	{
-		const QString sFps = QString::number(fpsAvg(), 'f', 1);
+		const QString sFps = QString::number(m_fpsManager.get(), 'f', 1);
 		
 		// Next line.
 		y -= ms_Osd_chrH;

commit 34d30a066d75dd5d2f525bb79ae012b7a1e7764a

[gens-qt4] VBackend/VBackend.hpp: Make sure the OSD list is marked as dirty when the FPS counter is updated.
This is a regression from commit fea787eac31b0d203c86ac6535fcf06ac5c06498.
(VBackend/FpsManager.cpp; Split the FPS manager code from VBackend into a separate class.)

src/gens-qt4/VBackend/VBackend.hpp
@@ -351,12 +351,6 @@ inline void VBackend::setOsdListDirty(void)
 inline void VBackend::clearOsdListDirty(void)
 	{ m_osdListDirty = false; }
 
-// FPS manager.
-inline void VBackend::fpsReset(void)
-	{ m_fpsManager.reset(); }
-inline void VBackend::fpsPush(double fps)
-	{ m_fpsManager.push(fps); }
-
 /** Recording OSD. **/
 inline int VBackend::recStart(const QString& component)
 	{ return recSetStatus(component, true); }

src/gens-qt4/VBackend/VBackend_OSD.cpp
@@ -197,6 +197,30 @@ void VBackend::osd_show_preview(int duration, const QImage& img)
 }
 
 
+/*! FPS manager. **/
+
+/**
+ * Reset the FPS manager.
+ */
+void VBackend::fpsReset(void)
+{
+	m_fpsManager.reset();
+	if (osdFpsEnabled() && isRunning() && !isPaused())
+		setOsdListDirty();
+}
+
+/**
+ * Push an FPS value.
+ * @param fps FPS value.
+ */
+void VBackend::fpsPush(double fps)
+{
+	m_fpsManager.push(fps);
+	if (osdFpsEnabled() && isRunning() && !isPaused())
+		setOsdListDirty();
+}
+
+
 /*! Recording status. **/
 
 