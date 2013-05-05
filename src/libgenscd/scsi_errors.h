55aed96ec840f06a33bced0988144d67464c8af5
David Korth <gerbilsoft@gerbilsoft.com>
2013-05-05 13:19:06 -0400

    [libgenscd] scsi_errors.c: List of all known SCSI errors as per MMC-6.
    TODO: Make use of this somewhere.

---
+++ src/libgenscd/scsi_errors.h
@@ -0,0 +1,42 @@
+/***************************************************************************
+ * libgenscd: Gens/GS II CD-ROM Handler Library.                           *
+ * scsi_errors.h: SCSI error descriptions.                                 *
+ *                                                                         *
+ * Copyright (c) 2013 by David Korth.                                      *
+ *                                                                         *
+ * This program is free software; you can redistribute it and/or modify it *
+ * under the terms of the GNU General Public License as published by the   *
+ * Free Software Foundation; either version 2 of the License, or (at your  *
+ * option) any later version.                                              *
+ *                                                                         *
+ * This program is distributed in the hope that it will be useful, but     *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ * GNU General Public License for more details.                            *
+ *                                                                         *
+ * You should have received a copy of the GNU General Public License along *
+ * with this program; if not, write to the Free Software Foundation, Inc., *
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.           *
+ ***************************************************************************/
+
+/**
+ * Reference:
+ * - SCSI Multi-Media Commands - 6 (MMC-6)
+ *   - 14 November 2006
+ *   - T10/1836-D Revision 0
+ */
+
+#ifndef __LIBGENSCD_SCSI_ERRORS_H__
+#define __LIBGENSCD_SCSI_ERRORS_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// TODO: External interface.
+	
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __LIBGENSCD_SCSI_ERRORS_H__ */
