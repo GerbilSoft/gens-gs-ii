fc732f72187eddb1de245df099bd79d67147e06e
David Korth <gerbilsoft@gerbilsoft.com>
2013-05-05 21:37:15 -0400

    [cmake] QtDBusInterfaceNoNS.cmake: Added an INCLUDE option using CMAKE_PARSE_ARGUMENTS().
    This will be needed for QtDBus metatypes.

--- cmake/macros/QtDBusInterfaceNoNS.cmake
+++ cmake/macros/QtDBusInterfaceNoNS.cmake
@@ -16,7 +16,7 @@
 # (To distribute this file outside of CMake, substitute the full
 #  License text for the above reference.)
 
-MACRO(QT4_ADD_DBUS_INTERFACE_NONS _sources _interface _basename)
+MACRO(QT4_ADD_DBUS_INTERFACE_NONS _sources _interface _basename _include)
   GET_FILENAME_COMPONENT(_infile ${_interface} ABSOLUTE)
   SET(_header ${CMAKE_CURRENT_BINARY_DIR}/${_basename}.h)
   SET(_impl   ${CMAKE_CURRENT_BINARY_DIR}/${_basename}.cpp)
@@ -25,6 +25,9 @@ MACRO(QT4_ADD_DBUS_INTERFACE_NONS _sources _interface _basename)
   # handling more arguments (as in FindQt4.cmake from KDE4) will come soon, then
   # _params will be used for more than just -m
   SET(_params -m -N)
+  IF(NOT "${_include}" STREQUAL "")
+    SET(_params ${_params} -i "${_include}")
+  ENDIF()
 
   ADD_CUSTOM_COMMAND(OUTPUT ${_impl} ${_header}
       COMMAND ${QT_DBUSXML2CPP_EXECUTABLE} ${_params} -p ${_basename} ${_infile}
@@ -39,12 +42,14 @@ MACRO(QT4_ADD_DBUS_INTERFACE_NONS _sources _interface _basename)
 
 ENDMACRO(QT4_ADD_DBUS_INTERFACE_NONS)
 
+INCLUDE(CMakeParseArguments)
 MACRO(QT4_ADD_DBUS_INTERFACES_NONS _sources)
-  FOREACH (_current_FILE ${ARGN})
+  CMAKE_PARSE_ARGUMENTS(DBUSXML2CPP "" "INCLUDE" "" ${ARGN})
+  FOREACH (_current_FILE ${DBUSXML2CPP_UNPARSED_ARGUMENTS})
     GET_FILENAME_COMPONENT(_infile ${_current_FILE} ABSOLUTE)
     # get the part before the ".xml" suffix
     STRING(REGEX REPLACE "(.*[/\\.])?([^\\.]+)\\.xml" "\\2" _basename ${_current_FILE})
     STRING(TOLOWER ${_basename} _basename)
-    QT4_ADD_DBUS_INTERFACE_NONS(${_sources} ${_infile} ${_basename}interface)
+    QT4_ADD_DBUS_INTERFACE_NONS(${_sources} ${_infile} ${_basename}interface "${DBUSXML2CPP_INCLUDE}")
   ENDFOREACH (_current_FILE)
 ENDMACRO(QT4_ADD_DBUS_INTERFACES_NONS)
