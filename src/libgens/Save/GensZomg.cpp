GerbilSoft/gens-gs-ii

commit f59c5e34af43acf3792397c7edd86f5e8b3b962d

[libgens] GensZomg::ZomgSave(): Trim parent directories from the ROM filename.

src/libgens/Save/GensZomg.cpp
@@ -248,8 +248,31 @@ int ZomgSave(const utf8_str *filename, const EmuContext *context,
 
 	// TODO: Get username for debugging builds. Make this optional later.
 	zomgIni.setAuthor("Joe User");
-	// TODO: Trim base path from the filename.
-	zomgIni.setRomFilename(context->rom()->filename());
+
+	// TODO: Move base path triming code to LibGensText later?
+	string rom_filename(context->rom()->filename());
+	rom_filename += "/";
+#ifdef _WIN32
+	const char chr_slash = '\\';
+#else
+	const char chr_slash = '/';
+#endif
+	size_t slash_pos = rom_filename.find_last_of(chr_slash);
+	if (slash_pos != string::npos) {
+		if ((slash_pos + 1) <= rom_filename.size() && slash_pos > 0) {
+			// Check for another slash.
+			slash_pos = rom_filename.find_last_of(chr_slash, slash_pos - 1);
+			if (slash_pos != string::npos) {
+				// Trim the filename.
+				rom_filename = rom_filename.substr(slash_pos + 1);
+			}
+		} else {
+			// Trim the filename.
+			rom_filename = rom_filename.substr(slash_pos + 1);
+		}
+	}
+	zomgIni.setRomFilename(rom_filename);
+
 	zomgIni.setDescription("Some description; should probably\nbe left\\blank.");
 	zomgIni.setExtensions("EXT,THAT,DOESNT,EXIST,LOL");
 

commit b176dcdbf02ec9d858d316c01068db2d9363dbb0

[libgens] GensZomg::ZomgSave(): Removed a debug statement that added a trailing slash to filenames.
This is a regression from commit f59c5e34af43acf3792397c7edd86f5e8b3b962d.
([libgens] GensZomg::ZomgSave(): Trim parent directories from the ROM filename.)

src/libgens/Save/GensZomg.cpp
@@ -229,7 +229,6 @@ int ZomgSave(const utf8_str *filename, const EmuContext *context,
 
 	// TODO: Move base path triming code to LibGensText later?
 	string rom_filename(context->rom()->filename());
-	rom_filename += "/";
 #ifdef _WIN32
 	const char chr_slash = '\\';
 #else