GerbilSoft/gens-gs-ii

commit 27889e4d57ea2eaf3ff0787578b23244fd013a59

[libgens] Initial reworking of LibGens to use RomCartridgeMD.
Notes:

- SRAM and EEPROM are currently not supported. They need to be moved
  from EmuContext to RomCartridgeMD.

- Only flat addressing is currently supported. ROM fixups aren't applied.
  SSF2 won't run until this is fixed.

[gens-qt4] EmuManager: EmuMD has been applying autofix checksum
for quite a while now, so we don't need to apply it here. Also,
the function has been moved to RomCartridgeMD, so this code
doesn't compile after the RomCartridgeMD changes.

src/libgens/Cartridge/RomCartridgeMD.cpp
@@ -279,9 +279,14 @@ int RomCartridgeMD::loadRom(void)
 
 	// TODO: Initialize SRAM/EEPROM.
 
-	// Load the ROM image.
+	// Allocate memory for the ROM image.
+	// NOTE: malloc() is rounded up to the nearest 512 KB.
+	// TODO: Store the rounded-up size.
 	m_romData_size = d->rom->romSize();
-	m_romData = malloc(m_romData_size);
+	const uint32_t rnd_512k = ((m_romData_size + 0x7FFFF) & ~0x7FFFF);
+	m_romData = malloc(rnd_512k);
+
+	// Load the ROM image.
 	int ret = d->rom->loadRom(m_romData, m_romData_size);
 	if (ret != (int)m_romData_size) {
 		// Error loading the ROM.
@@ -306,6 +311,89 @@ int RomCartridgeMD::loadRom(void)
 bool RomCartridgeMD::isRomLoaded(void) const
 {
 	return (m_romData != nullptr);
+}
+
+/**
+ * Update M68K CPU program access structs for bankswitching purposes.
+ * @param M68K_Fetch Pointer to first STARSCREAM_PROGRAMREGION to update.
+ * @param banks Maximum number of banks to update.
+ * @return Number of banks updated.
+ */
+int RomCartridgeMD::updateSysBanking(STARSCREAM_PROGRAMREGION *M68K_Fetch, int banks)
+{
+	int banksUpdated = 0;
+	if (banks > ARRAY_SIZE(m_cartBanks))
+		banks = ARRAY_SIZE(m_cartBanks);
+
+	// TODO: This is not 64-bit clean!
+	for (int i = 0; i < banks; i++) {
+		if (/*m_cartBanks[i] >= BANK_ROM_00 &&*/
+		    m_cartBanks[i] <= BANK_ROM_3F) {
+			// ROM bank.
+			// NOTE: m_romData is always a multiple of 512 KB.
+			const uint32_t romAddrStart = (0x80000 * (m_cartBanks[i] - BANK_ROM_00));
+			if (romAddrStart < m_romData_size) {
+				// Valid bank. Map it.
+				(*M68K_Fetch).lowaddr = romAddrStart;
+				(*M68K_Fetch).highaddr = (romAddrStart + 0x7FFFF);
+				(*M68K_Fetch).offset = ((uint32_t)m_romData);
+				M68K_Fetch++;
+				banksUpdated++;
+			}
+		}
+	}
+
+	// Updated.
+	return banksUpdated;
+}
+
+/**
+ * Fix the ROM checksum.
+ * This function uses the standard Sega checksum formula.
+ * TODO: Add certain non-standard checksums, and fixups
+ * for games that store code in the header area.
+ * @return 0 on success; non-zero on error.
+ */
+int RomCartridgeMD::fixChecksum(void)
+{
+	if (!m_romData || m_romData_size <= 0x200)
+		return -1;
+
+	// Calculate the ROM checksum.
+	// NOTE: ROM is byteswapped. (Header data is read before byteswapping.)
+	// NOTE: If ROM is an odd number of bytes, it'll be padded by 1 byte.
+	uint16_t checksum = 0;
+	const uint16_t *rom_ptr = &(reinterpret_cast<uint16_t*>(m_romData))[0x200>>1];
+	const uint16_t *end_ptr = rom_ptr + ((m_romData_size - 0x200) >> 1);
+	if (m_romData_size & 1)
+		end_ptr++;
+
+	for (; rom_ptr != end_ptr; rom_ptr++) {
+		checksum += *rom_ptr;
+	}
+
+	// Set the new checksum.
+	uint16_t *chk_ptr = &(reinterpret_cast<uint16_t*>(m_romData))[0x18E>>1];
+	*chk_ptr = checksum;
+	return 0;
+}
+
+/**
+ * Restore the ROM checksum.
+ * This restores the ROM checksum in m_romData
+ * from the previously-loaded header information.
+ * @return 0 on success; non-zero on error.
+ */
+int RomCartridgeMD::restoreChecksum(void)
+{
+	if (!m_romData || m_romData_size <= 0x200)
+		return -1;
+
+	// Restore the ROM checksum.
+	// NOTE: ROM is byteswapped. (Header data is read before byteswapping.)
+	uint16_t *chk_ptr = &(reinterpret_cast<uint16_t*>(m_romData))[0x18E>>1];
+	*chk_ptr = d->rom->checksum();
+	return 0;
 }
 
 

src/libgens/cpu/M68K.cpp
@@ -24,8 +24,11 @@
 #include "M68K.hpp"
 #include "M68K_Mem.hpp"
 
-// C includes.
-#include <string.h>
+#include "macros/common.h"
+#include "Cartridge/RomCartridgeMD.hpp"
+
+// C includes. (C++ namespace)
+#include <cstring>
 
 namespace LibGens
 {
@@ -36,45 +39,33 @@ S68000CONTEXT M68K::ms_Context;
 STARSCREAM_PROGRAMREGION M68K::M68K_Fetch[] =
 {
 #ifdef GENS_ENABLE_EMULATION
-	// RAM, including mirrors.
-	{0xE00000, 0xE0FFFF, (unsigned int)&Ram_68k.u8[0] - 0xE00000},
-	{0xE10000, 0xE1FFFF, (unsigned int)&Ram_68k.u8[0] - 0xE10000},
-	{0xE20000, 0xE2FFFF, (unsigned int)&Ram_68k.u8[0] - 0xE20000},
-	{0xE30000, 0xE3FFFF, (unsigned int)&Ram_68k.u8[0] - 0xE30000},
-	{0xE40000, 0xE4FFFF, (unsigned int)&Ram_68k.u8[0] - 0xE40000},
-	{0xE50000, 0xE5FFFF, (unsigned int)&Ram_68k.u8[0] - 0xE50000},
-	{0xE60000, 0xE6FFFF, (unsigned int)&Ram_68k.u8[0] - 0xE60000},
-	{0xE70000, 0xE7FFFF, (unsigned int)&Ram_68k.u8[0] - 0xE70000},
-	{0xE80000, 0xE8FFFF, (unsigned int)&Ram_68k.u8[0] - 0xE80000},
-	{0xE90000, 0xE9FFFF, (unsigned int)&Ram_68k.u8[0] - 0xE90000},
-	{0xEA0000, 0xEAFFFF, (unsigned int)&Ram_68k.u8[0] - 0xEA0000},
-	{0xEB0000, 0xEBFFFF, (unsigned int)&Ram_68k.u8[0] - 0xEB0000},
-	{0xEC0000, 0xECFFFF, (unsigned int)&Ram_68k.u8[0] - 0xEC0000},
-	{0xED0000, 0xEDFFFF, (unsigned int)&Ram_68k.u8[0] - 0xED0000},
-	{0xEE0000, 0xEEFFFF, (unsigned int)&Ram_68k.u8[0] - 0xEE0000},
-	{0xEF0000, 0xEFFFFF, (unsigned int)&Ram_68k.u8[0] - 0xEF0000},
-	{0xF00000, 0xF0FFFF, (unsigned int)&Ram_68k.u8[0] - 0xF00000},
-	{0xF10000, 0xF1FFFF, (unsigned int)&Ram_68k.u8[0] - 0xF10000},
-	{0xF20000, 0xF2FFFF, (unsigned int)&Ram_68k.u8[0] - 0xF20000},
-	{0xF30000, 0xF3FFFF, (unsigned int)&Ram_68k.u8[0] - 0xF30000},
-	{0xF40000, 0xF4FFFF, (unsigned int)&Ram_68k.u8[0] - 0xF40000},
-	{0xF50000, 0xF5FFFF, (unsigned int)&Ram_68k.u8[0] - 0xF50000},
-	{0xF60000, 0xF6FFFF, (unsigned int)&Ram_68k.u8[0] - 0xF60000},
-	{0xF70000, 0xF7FFFF, (unsigned int)&Ram_68k.u8[0] - 0xF70000},
-	{0xF80000, 0xF8FFFF, (unsigned int)&Ram_68k.u8[0] - 0xF80000},
-	{0xF90000, 0xF9FFFF, (unsigned int)&Ram_68k.u8[0] - 0xF90000},
-	{0xFA0000, 0xFAFFFF, (unsigned int)&Ram_68k.u8[0] - 0xFA0000},
-	{0xFB0000, 0xFBFFFF, (unsigned int)&Ram_68k.u8[0] - 0xFB0000},
-	{0xFC0000, 0xFCFFFF, (unsigned int)&Ram_68k.u8[0] - 0xFC0000},
-	{0xFD0000, 0xFDFFFF, (unsigned int)&Ram_68k.u8[0] - 0xFD0000},
-	{0xFE0000, 0xFEFFFF, (unsigned int)&Ram_68k.u8[0] - 0xFE0000},
-	{0xFF0000, 0xFFFFFF, (unsigned int)&Ram_68k.u8[0] - 0xFF0000},
-	
-	// The following four entries are available for the various different systems.
-	{-1, -1, 0},	// 32
-	{-1, -1, 0},	// 33
-	{-1, -1, 0},	// 34
-	{-1, -1, 0},	// 35
+	// 32 entries for RAM, including mirrors.
+	{-1, -1, 0}, {-1, -1, 0}, {-1, -1, 0}, {-1, -1, 0},
+	{-1, -1, 0}, {-1, -1, 0}, {-1, -1, 0}, {-1, -1, 0},
+	{-1, -1, 0}, {-1, -1, 0}, {-1, -1, 0}, {-1, -1, 0},
+	{-1, -1, 0}, {-1, -1, 0}, {-1, -1, 0}, {-1, -1, 0},
+	{-1, -1, 0}, {-1, -1, 0}, {-1, -1, 0}, {-1, -1, 0},
+	{-1, -1, 0}, {-1, -1, 0}, {-1, -1, 0}, {-1, -1, 0},
+	{-1, -1, 0}, {-1, -1, 0}, {-1, -1, 0}, {-1, -1, 0},
+	{-1, -1, 0}, {-1, -1, 0}, {-1, -1, 0}, {-1, -1, 0},
+
+	// 64 entries for ROM handlers.
+	{-1, -1, 0}, {-1, -1, 0}, {-1, -1, 0}, {-1, -1, 0},
+	{-1, -1, 0}, {-1, -1, 0}, {-1, -1, 0}, {-1, -1, 0},
+	{-1, -1, 0}, {-1, -1, 0}, {-1, -1, 0}, {-1, -1, 0},
+	{-1, -1, 0}, {-1, -1, 0}, {-1, -1, 0}, {-1, -1, 0},
+	{-1, -1, 0}, {-1, -1, 0}, {-1, -1, 0}, {-1, -1, 0},
+	{-1, -1, 0}, {-1, -1, 0}, {-1, -1, 0}, {-1, -1, 0},
+	{-1, -1, 0}, {-1, -1, 0}, {-1, -1, 0}, {-1, -1, 0},
+	{-1, -1, 0}, {-1, -1, 0}, {-1, -1, 0}, {-1, -1, 0},
+	{-1, -1, 0}, {-1, -1, 0}, {-1, -1, 0}, {-1, -1, 0},
+	{-1, -1, 0}, {-1, -1, 0}, {-1, -1, 0}, {-1, -1, 0},
+	{-1, -1, 0}, {-1, -1, 0}, {-1, -1, 0}, {-1, -1, 0},
+	{-1, -1, 0}, {-1, -1, 0}, {-1, -1, 0}, {-1, -1, 0},
+	{-1, -1, 0}, {-1, -1, 0}, {-1, -1, 0}, {-1, -1, 0},
+	{-1, -1, 0}, {-1, -1, 0}, {-1, -1, 0}, {-1, -1, 0},
+	{-1, -1, 0}, {-1, -1, 0}, {-1, -1, 0}, {-1, -1, 0},
+	{-1, -1, 0}, {-1, -1, 0}, {-1, -1, 0}, {-1, -1, 0},
 #endif /* GENS_ENABLE_EMULATION */
 	
 	// Terminator.
@@ -85,9 +76,10 @@ STARSCREAM_PROGRAMREGION M68K::M68K_Fetch[] =
 STARSCREAM_DATAREGION M68K::M68K_Read_Byte[4] =
 {
 #ifdef GENS_ENABLE_EMULATION
-	{0x000000, 0x3FFFFF, NULL, NULL},
+	// TODO: 0x9FFFFF is valid for MD only.
+	{0x000000, 0x9FFFFF, NULL, NULL},
 	{0xFF0000, 0xFFFFFF, NULL, &Ram_68k.u8[0]},
-	{0x400000, 0xFEFFFF, (void*)M68K_Mem::M68K_RB, NULL},
+	{0xA00000, 0xFEFFFF, (void*)M68K_Mem::M68K_RB, NULL},
 #endif /* GENS_ENABLE_EMULATION */
 	{-1, -1, NULL, NULL}
 };
@@ -96,9 +88,10 @@ STARSCREAM_DATAREGION M68K::M68K_Read_Byte[4] =
 STARSCREAM_DATAREGION M68K::M68K_Read_Word[4] =
 {
 #ifdef GENS_ENABLE_EMULATION
-	{0x000000, 0x3FFFFF, NULL, NULL},
+	// TODO: 0x9FFFFF is valid for MD only.
+	{0x000000, 0x9FFFFF, NULL, NULL},
 	{0xFF0000, 0xFFFFFF, NULL, &Ram_68k.u8[0]},
-	{0x400000, 0xFEFFFF, (void*)M68K_Mem::M68K_RW, NULL},
+	{0xA00000, 0xFEFFFF, (void*)M68K_Mem::M68K_RW, NULL},
 #endif /* GENS_ENABLE_EMULATION */
 	{-1, -1, NULL, NULL}
 };
@@ -122,6 +115,9 @@ STARSCREAM_DATAREGION M68K::M68K_Write_Word[3] =
 #endif /* GENS_ENABLE_EMULATION */
 	{-1, -1, NULL, NULL}
 };
+
+// Last system ID.
+M68K::SysID M68K::ms_LastSysID = SYSID_NONE;
 
 
 /**
@@ -178,32 +174,65 @@ void M68K::End(void)
 
 
 /**
- * InitSys(): Initialize a specific system for the M68K CPU emulator.
+ * Initialize a specific system for the M68K CPU emulator.
  * @param system System ID.
  */
 void M68K::InitSys(SysID system)
 {
 	// TODO: This is not 64-bit clean!
-	
+	ms_LastSysID = system;
+
 	// Clear M68K RAM.
 	memset(Ram_68k.u8, 0x00, sizeof(Ram_68k.u8));
-	
+
+	// Initialize M68K RAM handlers.
+	for (int i = 0; i < 32; i++) {
+		uint32_t ram_addr = (0xE00000 | (i << 16));
+		M68K_Fetch[i].lowaddr = ram_addr;
+		M68K_Fetch[i].highaddr = (ram_addr | 0xFFFF);
+		M68K_Fetch[i].offset = ((uint32_t)(&Ram_68k.u8[0]) - ram_addr);
+	}
+
 #ifdef GENS_ENABLE_EMULATION
-	// Set the ROM fetch.
-	M68K_Fetch[32].lowaddr  = 0x000000;
-	M68K_Fetch[32].highaddr = (M68K_Mem::Rom_Size - 1);
-	M68K_Fetch[32].offset   = (unsigned int)(&M68K_Mem::Rom_Data.u8[0]) - 0x000000;
+	// Update the system-specific banking setup.
+	UpdateSysBanking();
+
+	// Reset the M68K CPU.
+	main68k_reset();
+#endif /* GENS_ENABLE_EMULATION */
 	
-	switch (system)
-	{
+	// Initialize the M68K memory handlers.
+	M68K_Mem::InitSys(system);
+}
+
+
+/**
+ * Shut down M68K emulation.
+ */
+void M68K::EndSys(void)
+{
+	for (int i = 0; i < ARRAY_SIZE(M68K_Fetch); i++) {
+		M68K_Fetch[i].lowaddr = -1;
+		M68K_Fetch[i].highaddr = -1;
+		M68K_Fetch[i].offset = 0;
+	}
+}
+
+
+/**
+ * Update system-specific memory banking.
+ * Uses the last system initialized via InitSys().
+ */
+void M68K::UpdateSysBanking(void)
+{
+	// Start at M68K_Fetch[0x20].
+	int cur_fetch = 0x20;
+	switch (ms_LastSysID) {
 		case SYSID_MD:
 			// Sega Genesis / Mega Drive.
-			// Nothing else is required. Terminate the list.
-			M68K_Fetch[33].lowaddr  = -1;
-			M68K_Fetch[33].highaddr = -1;
-			M68K_Fetch[33].offset   = (unsigned int)NULL;
+			cur_fetch += M68K_Mem::ms_RomCartridge->updateSysBanking(&M68K_Fetch[cur_fetch], 10);
 			break;
-		
+
 		case SYSID_MCD:
 			// Sega CD.
 			// TODO
@@ -241,13 +270,11 @@ void M68K::InitSys(SysID system)
 		default:
 			break;
 	}
-	
-	// Reset the M68K CPU.
-	main68k_reset();
-#endif /* GENS_ENABLE_EMULATION */
-	
-	// Initialize the M68K memory handlers.
-	M68K_Mem::InitSys(system);
+
+	// Set the terminator.
+	M68K_Fetch[cur_fetch].lowaddr = -1;
+	M68K_Fetch[cur_fetch].highaddr = -1;
+	M68K_Fetch[cur_fetch].offset = 0;
 }
 
 

src/gens-qt4/EmuManager.cpp
@@ -352,6 +352,7 @@ int EmuManager::loadRom_int(LibGens::Rom *rom)
 	}
 
 	// Check the ROM format.
+	// TODO: Remove this once all ROM formats are supported.
 	if (rom->romFormat() != LibGens::Rom::RFMT_BINARY) {
 		// Only binary ROM images are supported.r
 		LibGens::Rom::RomFormat errRomFormat = rom->romFormat();

src/gens-qt4/EmuManager.hpp
@@ -407,18 +407,18 @@ class EmuManager : public QObject
 	
 	public slots:
 		/**
-		 * resetEmulator(): Reset the emulator.
+		 * Reset the emulator.
 		 * @param hardReset If true, do a hard reset; otherwise, do a soft reset.
 		 */
 		void resetEmulator(bool hardReset);
-		
+
 		/**
 		 * changePaletteSetting(): Change a palette setting.
 		 * @param type Type of palette setting.
 		 * @param val New value.
 		 */
 		void changePaletteSetting(EmuRequest_t::PaletteSettingType type, int val);
-	
+
 	protected slots:
 		/** Graphics settings. **/
 		void interlacedMode_changed_slot(QVariant interlacedMode);

src/gens-qt4/EmuManager_qEmu.cpp
@@ -775,39 +775,26 @@ void EmuManager::doPauseRequest(paused_t newPaused)
 
 
 /**
- * doResetEmulator(): Reset the emulator.
+ * Reset the emulator.
  * @param hardReset If true, do a hard reset; otherwise, do a soft reset.
  */
 void EmuManager::doResetEmulator(bool hardReset)
 {
-	// If autofix checksum is enabled, re-fix the checksum.
-	// Otherwise, restore the checksum.
-	// TODO: Move this call to EmuMD::hardReset() / EmuMD::softReset()?
-	// (That'll require setting a static option in EmuContext.)
-	// TODO: Automatically fix/unfix checksum when the option is changed?
-	if (gqt4_cfg->get(QLatin1String("autoFixChecksum")).toBool())
-		gqt4_emuContext->fixChecksum();
-	else
-		gqt4_emuContext->restoreChecksum();
-	
 	QString msg;
-	if (hardReset)
-	{
+	if (hardReset) {
 		// Do a hard reset.
 		gqt4_emuContext->hardReset();
-		
+
 		//: OSD message indicating a Hard Reset was performed.
 		msg = tr("Hard Reset.", "osd");
-	}
-	else
-	{
+	} else {
 		// Do a soft reset.
 		gqt4_emuContext->softReset();
-		
+
 		//: OSD message indicating a Soft Reset was performed.
 		msg = tr("Soft Reset.", "osd");
 	}
-	
+
 	// Print the message to the OSD.
 	emit osdPrintMsg(2500, msg);
 }

src/libgens/Cartridge/RomCartridgeMD.hpp
@@ -27,6 +27,9 @@
 // C includes.
 #include <stdint.h>
 
+// Starscream.
+#include "libgens/cpu/star_68k.h"
+
 // SRam and EEPRom.
 #include "Save/SRam.hpp"
 #include "Save/EEPRom.hpp"
@@ -72,6 +75,31 @@ class RomCartridgeMD
 		 * @return True if loaded; false if not.
 		 */
 		bool isRomLoaded(void) const;
+
+		/**
+		 * Update M68K CPU program access structs for bankswitching purposes.
+		 * @param M68K_Fetch Pointer to first STARSCREAM_PROGRAMREGION to update.
+		 * @param banks Maximum number of banks to update.
+		 * @return Number of banks updated.
+		 */
+		int updateSysBanking(STARSCREAM_PROGRAMREGION *M68K_Fetch, int banks);
+
+		/**
+		 * Fix the ROM checksum.
+		 * This function uses the standard Sega checksum formula.
+		 * TODO: Add certain non-standard checksums, and fixups
+		 * for games that store code in the header area.
+		 * @return 0 on success; non-zero on error.
+		 */
+		int fixChecksum(void);
+
+		/**
+		 * Restore the ROM checksum.
+		 * This restores the ROM checksum in m_romData
+		 * from the previously-loaded header information.
+		 * @return 0 on success; non-zero on error.
+		 */
+		int restoreChecksum(void);
 
 	private:
 		// ROM access.

src/libgens/EmuContext.cpp
@@ -148,64 +148,6 @@ EmuContext::~EmuContext()
 
 
 /**
- * fixChecksum(): Fix the ROM checksum.
- * This function uses the standard Sega checksum formula.
- * Results are applied to M68K_Mem::Rom_Data[] only.
- * The ROM header checksum will remain the same.
- * 
- * NOTE: This function may be overridden for e.g. SMS,
- * which uses a different checksum method.
- * 
- * @return 0 on success; non-zero on error.
- */
-int EmuContext::fixChecksum(void)
-{
-	if (!m_rom || M68K_Mem::Rom_Size <= 0x200)
-		return -1;
-	
-	// Calculate the ROM checksum.
-	// NOTE: ROM is byteswapped. (Header data is read before calling Rom::loadRom().)
-	// NOTE: If ROM is an odd number of bytes, it'll be padded by 1 byte.
-	uint16_t checksum = 0;
-	uint16_t *rom_ptr = &M68K_Mem::Rom_Data.u16[0x200>>1];
-	uint16_t *end_ptr = rom_ptr + ((M68K_Mem::Rom_Size - 0x200) >> 1);
-	if (M68K_Mem::Rom_Size & 1)
-		end_ptr++;
-	
-	for (; rom_ptr != end_ptr; rom_ptr++)
-	{
-		checksum += *rom_ptr;
-	}
-	
-	// Set the new checksum.
-	M68K_Mem::Rom_Data.u16[0x18E>>1] = checksum;
-	return 0;
-}
-
-
-/**
- * restoreChecksum(): Restore the ROM checksum.
- * This restores the ROM checksum in M68K_Mem::Rom_Data[]
- * from the previously-loaded header information.
- * 
- * NOTE: This function may be overridden for e.g. SMS,
- * which uses a different checksum method.
- * 
- * @return 0 on success; non-zero on error.
- */
-int EmuContext::restoreChecksum(void)
-{
-	if (!m_rom || M68K_Mem::Rom_Size <= 0x200)
-		return -1;
-	
-	// Restore the ROM checksum.
-	// NOTE: ROM is byteswapped. (Header data is read before calling Rom::loadRom().)
-	M68K_Mem::Rom_Data.u16[0x18E>>1] = m_rom->checksum();
-	return 0;
-}
-
-	
-/**
  * saveData(): Save SRam/EEPRom.
  * @return 1 if SRam was saved; 2 if EEPRom was saved; 0 if nothing was saved. (TODO: Enum?)
  */

src/libgens/EmuContext.hpp
@@ -53,114 +53,89 @@ class EmuContext
 		EmuContext(Rom *rom, SysVersion::RegionCode_t region = SysVersion::REGION_US_NTSC);
 		EmuContext(MdFb *fb, Rom *rom, SysVersion::RegionCode_t region = SysVersion::REGION_US_NTSC);
 		virtual ~EmuContext();
-	
+
 	private:
 		void init(MdFb *fb, Rom *rom, SysVersion::RegionCode_t region);
 
 	public:	
 		// Get the current EmuContext instance.
 		static EmuContext *Instance(void);
-		
-		/**
-		 * fixChecksum(): Fix the ROM checksum.
-		 * This function uses the standard Sega checksum formula.
-		 * Results are applied to M68K_Mem::Rom_Data[] only.
-		 * The ROM header checksum will remain the same.
-		 * 
-		 * NOTE: This function may be overridden for e.g. SMS,
-		 * which uses a different checksum method.
-		 * 
-		 * @return 0 on success; non-zero on error.
-		 */
-		virtual int fixChecksum(void);
-		
-		/**
-		 * restoreChecksum(): Restore the ROM checksum.
-		 * This restores the ROM checksum in M68K_Mem::Rom_Data[]
-		 * from the previously-loaded header information.
-		 * 
-		 * NOTE: This function may be overridden for e.g. SMS,
-		 * which uses a different checksum method.
-		 * 
-		 * @return 0 on success; non-zero on error.
-		 */
-		virtual int restoreChecksum(void);
-		
+
 		/**
 		 * saveData(): Save SRam/EEPRom.
 		 * @return 1 if SRam was saved; 2 if EEPRom was saved; 0 if nothing was saved. (TODO: Enum?)
 		 */
 		int saveData(void);
-		
+
 		/**
 		 * autoSaveData(): AutoSave SRam/EEPRom.
 		 * @param frames Number of frames elapsed, or -1 for paused. (force autosave)
 		 * @return 1 if SRam was saved; 2 if EEPRom was saved; 0 if nothing was saved. (TODO: Enum?)
 		 */
 		int autoSaveData(int framesElapsed);
-		
+
 		/**
 		 * softReset(): Perform a soft reset.
 		 * @return 0 on success; non-zero on error.
 		 */
 		virtual int softReset(void) = 0;
-		
+
 		/**
 		 * hardReset(): Perform a hard reset.
 		 * @return 0 on success; non-zero on error.
 		 */
 		virtual int hardReset(void) = 0;
-		
+
 		/**
 		 * setRegion(): Set the region code.
 		 * @param region Region code.
 		 * @return 0 on success; non-zero on error.
 		 */
 		virtual int setRegion(SysVersion::RegionCode_t region) = 0;
-		
+
 		/** Frame execution functions. **/
 		virtual void execFrame(void) = 0;
 		virtual void execFrameFast(void) = 0;
-		
+
 		// Accessors.
 		inline bool isRomOpened(void)
 			{ return (m_rom != nullptr); }
-		
+
 		// Controller I/O manager.
 		// TODO: Make this non-static!
 		static IoManager *m_ioManager;
-		
+
 		// SRam / EEPRom access.
 		// TODO: Providing pointers like this is bad...
 		inline SRam *getSRam(void) { return &m_SRam; }
 		inline EEPRom *getEEPRom(void) { return &m_EEPRom; }
-		
+
 		inline const SRam *getSRam(void) const { return &m_SRam; }
 		inline const EEPRom *getEEPRom(void) const { return &m_EEPRom; }
-		
+
 		/**
 		 * Read the system version register. (MD)
 		 * @return MD version register.
 		 */
 		uint8_t readVersionRegister_MD(void) const;
-		
+
 		/**
 		 * Get a const pointer to the version register.
 		 * @return Const pointer to the version register.
 		 */
 		const SysVersion *versionRegisterObject(void) const;
-		
+
 		// TODO: Add isPal(), isNtsc(), regionCode(), etc. wrapper functions/
-		
+
 		// Save data enable bit.
 		bool saveDataEnable(void);
 		void setSaveDataEnable(bool newSaveDataEnable);
-		
+
 		// Static functions. Temporarily needed for SRam/EEPRom.
 		static inline bool GetSaveDataEnable(void) { return m_instance->m_saveDataEnable; }
 		static inline SRam *GetSRam(void) { return m_instance->getSRam(); }
 		static inline EEPRom *GetEEPRom(void) { return m_instance->getEEPRom(); }
-		
+
 		/**
 		 * Global settings.
 		 */
@@ -169,14 +144,14 @@ class EmuContext
 			{ return ms_AutoFixChecksum; }
 		static inline void SetAutoFixChecksum(bool newAutoFixChecksum)
 			{ ms_AutoFixChecksum = newAutoFixChecksum; }
-		
+
 		/**
 		 * Pathnames.
 		 */
 		static inline const char *PathSRam(void)
 			{ return ms_PathSRam.c_str(); }
 		static void SetPathSRam(const char *newPathSRam);
-		
+
 		/** VDP (TODO) **/
 		Vdp *m_vdp;
 
@@ -189,26 +164,26 @@ class EmuContext
 	protected:
 		Rom *m_rom;
 		bool m_saveDataEnable;
-		
+
 		// SRam and EEPRom.
 		// TODO: Add a function to reset all memory handling.
 		SRam m_SRam;
 		EEPRom m_EEPRom;
-		
+
 		/**
 		 * System version register.
 		 */
 		SysVersion m_sysVersion;
-		
+
 		// Static pointer. Temporarily needed for SRam/EEPRom.
 		static EmuContext *m_instance;
-		
+
 		/**
 		 * Global settings.
 		 */
 		static bool ms_AutoFixChecksum;
 		static std::string ms_PathSRam;
-	
+
 	private:
 		static int ms_RefCount;
 };

commit 3ff2e856d9fdb889fd4dc30f4ebab07fe9099983

[libgens] Fixed 64-bit no-emulation builds.
We can't initialize M68K_Fetch[] at all, since that would result in
converting a 64-bit pointer to a uint32_t.

This is a regression from commit 27889e4d57ea2eaf3ff0787578b23244fd013a59.
([libgens] Initial reworking of LibGens to use RomCartridgeMD.)

src/libgens/Cartridge/RomCartridgeMD.cpp
@@ -23,6 +23,8 @@
 
 #include "RomCartridgeMD.hpp"
 
+#include <libgens/config.libgens.h>
+
 #include "../EmuContext.hpp"
 #include "../Util/byteswap.h"
 #include "../macros/common.h"
@@ -406,6 +408,7 @@ bool RomCartridgeMD::isRomLoaded(void) const
  */
 int RomCartridgeMD::updateSysBanking(STARSCREAM_PROGRAMREGION *M68K_Fetch, int banks)
 {
+#ifdef GENS_ENABLE_EMULATION
 	int banksUpdated = 0;
 	if (banks > ARRAY_SIZE(m_cartBanks))
 		banks = ARRAY_SIZE(m_cartBanks);
@@ -430,6 +433,10 @@ int RomCartridgeMD::updateSysBanking(STARSCREAM_PROGRAMREGION *M68K_Fetch, int b
 
 	// Updated.
 	return banksUpdated;
+#else /* !GENS_ENABLE_EMULATION */
+	// Emulation is disabled.
+	return 0;
+#endif /* GENS_ENABLE_EMULATION */
 }
 
 /**

src/libgens/cpu/M68K.cpp
@@ -185,6 +185,7 @@ void M68K::InitSys(SysID system)
 	// Clear M68K RAM.
 	memset(Ram_68k.u8, 0x00, sizeof(Ram_68k.u8));
 
+#ifdef GENS_ENABLE_EMULATION
 	// Initialize M68K RAM handlers.
 	for (int i = 0; i < 32; i++) {
 		uint32_t ram_addr = (0xE00000 | (i << 16));
@@ -193,7 +194,6 @@ void M68K::InitSys(SysID system)
 		M68K_Fetch[i].offset = ((uint32_t)(&Ram_68k.u8[0]) - ram_addr);
 	}
 
-#ifdef GENS_ENABLE_EMULATION
 	// Update the system-specific banking setup.
 	UpdateSysBanking();
 