6c598edc90404d38e22bffec7f6c566543a09c16
David Korth <gerbilsoft@gerbilsoft.com>
2014-08-02 21:57:45 -0400

    [mdZ80] Added z80dis.c from Gens/GS.
    It's not being used right now, but I might as well add it to the repository.

---
+++ src/libgens/mdZ80/z80dis.h
@@ -0,0 +1,36 @@
+/***************************************************************************
+ * Gens: Z80 disassembler.                                                 *
+ *                                                                         *
+ * Copyright (c) 1999-2002 by Stéphane Dallongeville                       *
+ * Copyright (c) 2003-2004 by Stéphane Akhoun                              *
+ * Copyright (c) 2008-2009 by David Korth                                  *
+ *                                                                         *
+ * This program is free software; you can redistribute it and/or modify it *
+ * under the terms of the GNU General Public License as published by the   *
+ * Free Software Foundation; either version 2 of the License, or (at your  *
+ * option) any later version.                                              *
+ *                                                                         *
+ * This program is distributed in the hope that it will be useful, but     *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ * GNU General Public License for more details.                            *
+ *                                                                         *
+ * You should have received a copy of the GNU General Public License along *
+ * with this program; if not, write to the Free Software Foundation, Inc., *
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.           *
+ ***************************************************************************/
+
+#ifndef GENS_Z80DIS_H
+#define GENS_Z80DIS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int z80dis(unsigned char *buf, unsigned int *Counter, char *str, int size);
+
+#ifdef __cplusplus
+}
+#endif /* GENS_DEBUGGER */
+
+#endif /* GENS_Z80DIS_H */
