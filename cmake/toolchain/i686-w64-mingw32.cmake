8454ae59508beb5478e03ff82960e4e32fc439f5
David Korth <gerbilsoft@gerbilsoft.com>
2014-08-17 13:08:54 -0400

    cmake/macros/SetMSVCDebugPath.cmake: New macro to set the MSVC debug path.
    Updated all projects to uset SET_MSVC_DEBUG_PATH().
    
    gens-qt4: s/mcrecover/gens-qt4/
    Copypasta error from GCN MemCard Recover.

--- cmake/toolchain/i686-w64-mingw32.cmake
+++ cmake/toolchain/i686-w64-mingw32.cmake
@@ -2,6 +2,7 @@ SET(HOST_SYSTEM i686-w64-mingw32)
 
 # the name of the target operating system
 SET(CMAKE_SYSTEM_NAME Windows)
+SET(CMAKE_SYSTEM_PROCESSOR i686)
 
 # which compilers to use for C and C++
 SET(CMAKE_C_COMPILER	${HOST_SYSTEM}-gcc)
@@ -14,15 +15,15 @@ SET(CMAKE_FIND_ROOT_PATH /usr/${HOST_SYSTEM})
 # adjust the default behaviour of the FIND_XXX() commands:
 # search headers and libraries in the target environment, search
 # programs in the host environment
-SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM BOTH)
 SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 
 # FindQt4.cmake querys qmake to get information, this doesn't work when crosscompiling
-SET(QT_BINARY_DIR		/usr/bin)
-SET(QT_HEADERS_DIR		/usr/include/qt4)
-SET(QT_INCLUDE_DIR		/usr/include/qt4)
-SET(QT_LIBRARY_DIR		${HOST_SYSTEM}/bin)
+SET(QT_BINARY_DIR		$ENV{QTDIR}/bin)
+SET(QT_HEADERS_DIR		$ENV{QTDIR}/include)
+SET(QT_INCLUDE_DIR		$ENV{QTDIR}/include)
+SET(QT_LIBRARY_DIR		$ENV{QTDIR}/bin)
 
 SET(QT_QTCORE_INCLUDE_DIR	${QT_INCLUDE_DIR}/QtCore)
 SET(QT_QTGUI_INCLUDE_DIR	${QT_INCLUDE_DIR}/QtGui)
