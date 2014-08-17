79e7b21c226b8f2b079622bd3779fef8ffeeb8c6
David Korth <gerbilsoft@gerbilsoft.com>
2014-08-17 12:52:55 -0400

    Backported many CMake changes from GCN MemCard Recover.
    Includes lots of CFLAGS changes as well as some PDB improvements.
    In particular, -Zi is used during compilation so PDBs are used
    instead of including the debug symbols in the objects themselves.
    
    gtest: Set gtest_force_shared_crt to force gtest to link to the
    dynamic CRT on MSVC instead of the static CRT, since it usually
    links to the static CRT when compiled as a static library.
    
    TODO: CMake uses a dumb default filename for PDBs.
    I'm using some ad-hoc code to fix this right now, but it needs
    to be abstracted into a macro.

--- cmake/platform/win32-gcc.cmake
+++ cmake/platform/win32-gcc.cmake
@@ -1,27 +1,40 @@
 # Win32-specific CFLAGS/CXXFLAGS.
 # For MinGW compilers.
 
-# Set minimum Windows version to Windows 2000. (Windows NT 5.0)
-SET(GENS_CFLAG_WIN32_WINNT "-D_WIN32_WINNT=0x0500")
+# Basic platform flags:
+# - wchar_t is short.
+# - Enable strict type checking in the Windows headers.
+# - Set minimum Windows version to Windows 2000. (Windows NT 5.0)
+SET(GENS_C_FLAGS_WIN32 "-fshort-wchar -DSTRICT -D_WIN32_WINNT=0x0500")
 
 # Release build: Prefer static libraries.
 IF(CMAKE_BUILD_TYPE MATCHES ^release)
 	SET(CMAKE_FIND_LIBRARY_SUFFIXES .lib .a ${CMAKE_FIND_LIBRARY_SUFFIXES})
 ENDIF(CMAKE_BUILD_TYPE MATCHES ^release)
 
-# Initialize CFLAGS/CXXFLAGS.
-SET(GENS_CFLAGS_PLATFORM "-fshort-wchar -DSTRICT ${GENS_CFLAG_WIN32_WINNT}")
-SET(GENS_CXXFLAGS_PLATFORM "-fshort-wchar -DSTRICT ${GENS_CFLAG_WIN32_WINNT}")
-
 # Test for static libgcc/libstdc++.
-SET(GENS_LDFLAGS_PLATFORM "")
-FOREACH(FLAG_TEST "-static-libgcc" "-static-libstdc++" "-Wl,--large-address-aware" "-Wl,--nxcompat" "-Wl,--dynamicbase" "-Wl,--tsaware")
+SET(GENS_EXE_LINKER_FLAGS_WIN32 "")
+FOREACH(FLAG_TEST "-static-libgcc" "-static-libstdc++" "-Wl,--large-address-aware" "-Wl,--nxcompat" "-Wl,--dynamicbase")
 	# CMake doesn't like "+" characters in variable names.
 	STRING(REPLACE "+" "_" FLAG_TEST_VARNAME "${FLAG_TEST}")
 
 	CHECK_C_COMPILER_FLAG("${FLAG_TEST}" LDFLAG_${FLAG_TEST_VARNAME})
 	IF(LDFLAG_${FLAG_TEST_VARNAME})
-		SET(GENS_LDFLAGS_PLATFORM "${GENS_LDFLAGS_PLATFORM} ${FLAG_TEST}")
+		SET(GENS_EXE_LINKER_FLAGS_WIN32 "${GENS_EXE_LINKER_FLAGS_WIN32} ${FLAG_TEST}")
+	ENDIF(LDFLAG_${FLAG_TEST_VARNAME})
+	UNSET(LDFLAG_${FLAG_TEST_VARNAME})
+	UNSET(FLAG_TEST_VARNAME)
+ENDFOREACH()
+
+# --tsaware is only valid for EXEs. (not DLLs)
+SET(GENS_SHARED_LINKER_FLAGS_WIN32 "")
+FOREACH(FLAG_TEST "-Wl,--tsaware")
+	# CMake doesn't like "+" characters in variable names.
+	STRING(REPLACE "+" "_" FLAG_TEST_VARNAME "${FLAG_TEST}")
+
+	CHECK_C_COMPILER_FLAG("${FLAG_TEST}" LDFLAG_${FLAG_TEST_VARNAME})
+	IF(LDFLAG_${FLAG_TEST_VARNAME})
+		SET(GENS_SHARED_LINKER_FLAGS_WIN32 "${GENS_SHARED_LINKER_FLAGS_WIN32} ${FLAG_TEST}")
 	ENDIF(LDFLAG_${FLAG_TEST_VARNAME})
 	UNSET(LDFLAG_${FLAG_TEST_VARNAME})
 	UNSET(FLAG_TEST_VARNAME)
@@ -29,16 +42,19 @@ ENDFOREACH()
 
 # Enable windres support on MinGW.
 # http://www.cmake.org/Bug/view.php?id=4068
-IF(MINGW)
-	SET(CMAKE_RC_COMPILER_INIT windres)
-	ENABLE_LANGUAGE(RC)
-	
-	# NOTE: Setting CMAKE_RC_OUTPUT_EXTENSION doesn't seem to work.
-	# Force windres to output COFF, even though it'll use the .res extension.
-	SET(CMAKE_RC_OUTPUT_EXTENSION .obj)
-	SET(CMAKE_RC_COMPILE_OBJECT
-		"<CMAKE_RC_COMPILER> --output-format=coff <FLAGS> <DEFINES> -o <OBJECT> <SOURCE>")
-ENDIF(MINGW)
+SET(CMAKE_RC_COMPILER_INIT windres)
+ENABLE_LANGUAGE(RC)
 
-# Unset variables we don't need elsewhere.
-UNSET(GENS_CFLAG_WIN32_WINNT)
+# NOTE: Setting CMAKE_RC_OUTPUT_EXTENSION doesn't seem to work.
+# Force windres to output COFF, even though it'll use the .res extension.
+SET(CMAKE_RC_OUTPUT_EXTENSION .obj)
+SET(CMAKE_RC_COMPILE_OBJECT
+	"<CMAKE_RC_COMPILER> --output-format=coff <FLAGS> <DEFINES> -o <OBJECT> <SOURCE>")
+
+# Append the CFLAGS and LDFLAGS.
+SET(GENS_C_FLAGS_COMMON "${GENS_C_FLAGS_COMMON} ${GENS_C_FLAGS_WIN32}")
+SET(GENS_CXX_FLAGS_COMMON "${GENS_CXX_FLAGS_COMMON} ${GENS_C_FLAGS_WIN32}")
+SET(GENS_EXE_LINKER_FLAGS_COMMON "${GENS_EXE_LINKER_FLAGS_COMMON} ${GENS_EXE_LINKER_FLAGS_WIN32}")
+SET(GENS_SHARED_LINKER_FLAGS_COMMON "${GENS_SHARED_LINKER_FLAGS_COMMON} ${GENS_EXE_LINKER_FLAGS_WIN32} ${GENS_SHARED_LINKER_FLAGS_WIN32}")
+UNSET(GENS_C_FLAGS_WIN32)
+UNSET(GENS_EXE_LINKER_FLAGS_WIN32)
--- cmake/platform/win32-msvc.cmake
+++ cmake/platform/win32-msvc.cmake
@@ -1,17 +1,18 @@
 # Win32-specific CFLAGS/CXXFLAGS.
 # For Microsoft Visual C++ compilers.
 
-# Set minimum Windows version to Windows 2000. (Windows NT 5.0)
-SET(GENS_CFLAG_WIN32_WINNT "-D_WIN32_WINNT=0x0500")
+# Basic platform flags:
+# - wchar_t should be a distinct type.
+# - Enable strict type checking in the Windows headers.
+# - Set minimum Windows version to Windows 2000. (Windows NT 5.0)
+SET(GENS_C_FLAGS_WIN32 "-Zc:wchar_t -DSTRICT -D_WIN32_WINNT=0x0500")
 
 # Release build: Prefer static libraries.
 IF(CMAKE_BUILD_TYPE MATCHES ^release)
 	SET(CMAKE_FIND_LIBRARY_SUFFIXES .lib .a ${CMAKE_FIND_LIBRARY_SUFFIXES})
 ENDIF(CMAKE_BUILD_TYPE MATCHES ^release)
 
-# Initialize CFLAGS/CXXFLAGS.
-SET(GENS_CFLAGS_PLATFORM "-Zc:wchar_t -DSTRICT ${GENS_CFLAG_WIN32_WINNT}")
-SET(GENS_CXXFLAGS_PLATFORM "-Zc:wchar_t -DSTRICT ${GENS_CFLAG_WIN32_WINNT}")
-
-# Unset variables we don't need elsewhere.
-UNSET(GENS_CFLAG_WIN32_WINNT)
+# Append the CFLAGS.
+SET(GENS_C_FLAGS_COMMON "${GENS_C_FLAGS_COMMON} ${GENS_C_FLAGS_WIN32}")
+SET(GENS_CXX_FLAGS_COMMON "${GENS_CXX_FLAGS_COMMON} ${GENS_C_FLAGS_WIN32}")
+UNSET(GENS_C_FLAGS_WIN32)
