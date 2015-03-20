GerbilSoft/gens-gs-ii

commit 9237afc480aaed1e67dc9065d2e33a41a8e4b224

[libgens] Vdp::T_DMA_Loop<>(): Reset the DMA length registers after the loop is done.
This fixes VDPFIFOTesting test #24: "DMA Transfer Length Reg Update"

src/libgens/Vdp/VdpIo.cpp
@@ -1097,6 +1097,9 @@ inline void Vdp::T_DMA_Loop(void)
 	// Ergo, it simply added length to DMA_Address.
 	inc_DMA_Src_Adr(DMAT_Length);
 
+	// Reset the DMA length registers.
+	set_DMA_Length(0);
+
 	// Update DMA.
 	Update_DMA();
 

commit fcbe31a856168e69f6565a2e046e07e1d09e1040

[libgens] Vdp::writeCtrlMD(): Disable d->set_DMA_Length(0) for now.
This is needed in order to fix VDPFIFOTesting Test #24,
"DMA Transfer Length Reg Update", but it ends up breaking
"Frank Thomas Big Hurt Baseball", which clearly means that
something's wrong in the DMA code.

Revert the change for now.

This is a regression from commit 9237afc480aaed1e67dc9065d2e33a41a8e4b224.
([libgens] Vdp::T_DMA_Loop<>(): Reset the DMA length registers after the loop is done.)

src/libgens/Vdp/VdpIo.cpp
@@ -1043,7 +1043,10 @@ void Vdp::writeCtrlMD(uint16_t ctrl)
 	// as we go along, but the M68K isn't properly "locked"
 	// for the entire time period.
 	DMAT_Length = length;
-	d->set_DMA_Length(0);
+	// FIXME: This is needed in order to fix VDPFIFOTesting
+	// Test #24: "DMA Transfer Length Reg Update", but it
+	// breaks "Frank Thomas Big Hurt Baseball".
+	//d->set_DMA_Length(0);
 
 	switch (DMA_TYPE(src_component, dest_component)) {
 		case DMA_TYPE(VdpPrivate::DMA_SRC_ROM, VdpPrivate::DMA_DEST_VRAM):