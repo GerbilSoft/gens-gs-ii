34e30d902da6a6df7ff119a81bb925ae932c2740
David Korth <gerbilsoft@gerbilsoft.com>
2015-03-21 13:35:06 -0400

    [libgens/tests] EEPRomI2CTest: Added an X24C01 random read test for empty EEPROMs.
    NOTE: The actual I2C protocol allows for repeated start conditions,
    so we can send a new initial command byte without releasing the bus.
    EEPRomI2C doesn't support that right now.

--- src/libgens/tests/EEPRomI2CTest.cpp
+++ src/libgens/tests/EEPRomI2CTest.cpp
@@ -36,6 +36,8 @@
 
 // C includes. (C++ namespace)
 #include <cstdio>
+#include <cstdlib>
+#include <ctime>
 
 namespace LibGens { namespace Tests {
 
@@ -270,11 +272,63 @@ INSTANTIATE_TEST_CASE_P(X24C01_seqReadFull, EEPRomI2CTest,
 	::testing::Values(0x00, 0x12, 0x4F, 0x72, 0x90, 0xA3, 0xC4, 0xFF)
 	);
 
+/**
+ * X24C01: Test random reading of an empty EEPROM.
+ * Reads 64 bytes at random addresses.
+ */
+TEST_F(EEPRomI2CTest, X24C01_randomReadEmpty)
+{
+	// Set the EEPROM as X24C01.
+	// TODO: Move to SetUp().
+	m_eeprom->dbg_setEEPRomType(EEPRomI2C::EPR_X24C01);
+	unsigned int eepromSize;
+	m_eeprom->dbg_getEEPRomSize(&eepromSize);
+	ASSERT_EQ(128U, eepromSize) << "X24C01 should be 128 bytes.";
+	unsigned int eepromMask = eepromSize - 1;
+
+	// Make sure we're in a STOP condition at first.
+	doStop();
+
+	// EEPROM values.
+	uint8_t cmd, response;
+
+	// Read up to 64 random addresses.
+	for (int i = 64; i > 0; i--) {
+		const unsigned int addr = (rand() & eepromMask);
+
+		// START an I2C transfer.
+		// We'll request a READ from address 0x00.
+		// Mode 1 word address: [A6 A5 A4 A3 A2 A1 A0 RW]
+		m_eeprom->dbg_setSDA(0);	// START
+		cmd = (addr << 1) | 1;		// RW=1
+		response = sendData(cmd);
+
+		// Check for ACK.
+		m_eeprom->dbg_getSDA(&response);
+		EXPECT_EQ(0, response) << "NACK received; expected ACK.";
+
+		// Read the data from the EEPROM.
+		uint8_t data = recvData();
+		EXPECT_EQ(0xFF, data) <<
+			"EEPROM address 0x" <<
+			std::hex << std::setw(2) << std::setfill('0') << std::uppercase << addr <<
+			" should be 0xFF (empty ROM).";
+
+		// STOP the transfer.
+		doStop();
+	}
+}
+
 } }
 
 int main(int argc, char *argv[])
 {
-	fprintf(stderr, "LibGens test suite: EEPRomI2C tests.\n\n");
+	fprintf(stderr, "LibGens test suite: EEPRomI2C tests.\n");
+
+	// Initialize the random number seed.
+	unsigned int seed = time(nullptr);
+	srand(seed);
+	fprintf(stderr, "Random number seed is: %08X\n\n", seed);
 
 	::testing::InitGoogleTest(&argc, argv);
 	LibGens::Init();
