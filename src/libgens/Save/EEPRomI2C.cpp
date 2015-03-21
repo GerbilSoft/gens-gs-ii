GerbilSoft/gens-gs-ii

commit 609da84e0d8c2423ba518e6f631a423328d797c4

[libgens] EEPRomI2C_Debug.cpp: Initial set of debugging functions.
These functions will be used to implement an EEPROM test suite.

Related changes:
- Moved EEPRomType_t from EEPRomI2CPrivate to EEPRomI2C.
- Don't store the entire GameEEPRomInfo_t, since we don't need the
  game's serial number and checksum once the EEPRom is initialized.
  Instead, only store the EEPRomMap_t.

src/libgens/Save/EEPRomI2C.cpp
@@ -43,7 +43,7 @@ EEPRomI2CPrivate::EEPRomI2CPrivate(EEPRomI2C *q)
 {
 	// Clear the EEPRom type.
 	memset(&eprSpec, 0, sizeof(eprSpec));
-	memset(&eprType, 0, sizeof(eprType));
+	memset(&eprMapper, 0, sizeof(eprMapper));
 
 	// Reset the EEPRom.
 	reset();
@@ -85,7 +85,7 @@ void EEPRomI2CPrivate::processI2Cbit(void)
 	// Save the current /SDA out.
 	sda_out_prev = sda_out;
 
-	if (eprType.type.epr_type == EPR_NONE) {
+	if (eprMapper.epr_type == EEPRomI2C::EPR_NONE) {
 		// No EEPRom.
 		goto done;
 	}
@@ -110,7 +110,7 @@ void EEPRomI2CPrivate::processI2Cbit(void)
 				LOG_MSG(eeprom_i2c, LOG_MSG_LEVEL_DEBUG1,
 					"EPR_STANDBY: Received START condition.");
 				counter = 0;
-				if (eprType.type.epr_type == EPR_X24C01) {
+				if (eprMapper.epr_type == EEPRomI2C::EPR_X24C01) {
 					// Mode 1.
 					state = EPR_MODE1_WORD_ADDRESS;
 				} else {
@@ -291,8 +291,8 @@ int EEPRomI2C::setEEPRomType(int type)
 	}
 
 	// Set the EEPRom type.
-	memcpy(&d->eprType, &d->rom_db[type], sizeof(d->eprType));
-	memcpy(&d->eprSpec, &d->eeprom_spec[d->eprType.type.epr_type], sizeof(d->eprSpec));
+	memcpy(&d->eprMapper, &d->rom_db[type], sizeof(d->eprMapper));
+	memcpy(&d->eprSpec, &d->eeprom_spec[d->eprMapper.epr_type], sizeof(d->eprSpec));
 	return 0;
 }
 
@@ -322,25 +322,25 @@ bool EEPRomI2C::isEEPRomTypeSet(void) const
 
 bool EEPRomI2C::isReadBytePort(uint32_t address) const
 {
-	return (address == d->eprType.type.sda_out_adr);
+	return (address == d->eprMapper.sda_out_adr);
 }
 
 bool EEPRomI2C::isReadWordPort(uint32_t address) const
 {
-	return ((address | 1) == (d->eprType.type.sda_out_adr | 1));
+	return ((address | 1) == (d->eprMapper.sda_out_adr | 1));
 }
 
 bool EEPRomI2C::isWriteBytePort(uint32_t address) const
 {
-	return (address == d->eprType.type.scl_adr ||
-		address == d->eprType.type.sda_in_adr);
+	return (address == d->eprMapper.scl_adr ||
+		address == d->eprMapper.sda_in_adr);
 }
 
 bool EEPRomI2C::isWriteWordPort(uint32_t address) const
 {
 	address |= 1;
-	return ((address == (d->eprType.type.scl_adr | 1)) ||
-		(address == (d->eprType.type.sda_in_adr | 1)));
+	return ((address == (d->eprMapper.scl_adr | 1)) ||
+		(address == (d->eprMapper.sda_in_adr | 1)));
 }
 
 /**
@@ -357,14 +357,14 @@ bool EEPRomI2C::isDirty(void) const
  */
 uint8_t EEPRomI2C::readByte(uint32_t address)
 {
-	if (address != d->eprType.type.sda_out_adr) {
+	if (address != d->eprMapper.sda_out_adr) {
 		// Wrong address.
 		return 0xFF;
 	}
 
 	// Return /SDA, shifted over to the appropriate position.
 	// TODO: Other bits should be prefetch?
-	return (d->getSDA() << d->eprType.type.sda_out_bit);
+	return (d->getSDA() << d->eprMapper.sda_out_bit);
 }
 
 
@@ -377,15 +377,15 @@ uint16_t EEPRomI2C::readWord(uint32_t address)
 {
 	// TODO: address probably doesn't need to be masked,
 	// since M68K is word-aligned...
-	if ((address & ~1) != (d->eprType.type.sda_out_adr & ~1)) {
+	if ((address & ~1) != (d->eprMapper.sda_out_adr & ~1)) {
 		// Wrong address.
 		return 0xFFFF;
 	}
 
 	// Return /SDA, shifted over to the appropriate position.
 	// TODO: Other bits should be prefetch?
-	uint8_t sda_out_bit = d->eprType.type.sda_out_bit;
-	sda_out_bit += ((d->eprType.type.sda_out_adr & 1) * 8);
+	uint8_t sda_out_bit = d->eprMapper.sda_out_bit;
+	sda_out_bit += ((d->eprMapper.sda_out_adr & 1) * 8);
 	return (d->getSDA() << sda_out_bit);
 }
 
@@ -396,23 +396,23 @@ uint16_t EEPRomI2C::readWord(uint32_t address)
  */
 void EEPRomI2C::writeByte(uint32_t address, uint8_t data)
 {
-	if (address != d->eprType.type.scl_adr &&
-	    address != d->eprType.type.sda_in_adr)
+	if (address != d->eprMapper.scl_adr &&
+	    address != d->eprMapper.sda_in_adr)
 	{
 		// Invalid address.
 		return;
 	}
 
 	// Check if this is the clock line. (/SCL)
-	if (address == d->eprType.type.scl_adr) {
-		d->scl = (data >> (d->eprType.type.scl_bit)) & 1;
+	if (address == d->eprMapper.scl_adr) {
+		d->scl = (data >> (d->eprMapper.scl_bit)) & 1;
 	} else {
 		d->scl = d->scl_prev;
 	}
 
 	// Check if this is the data line. (/SDA)
-	if (address == d->eprType.type.sda_in_adr) {
-		d->sda_in = (data >> (d->eprType.type.sda_in_bit)) & 1;
+	if (address == d->eprMapper.sda_in_adr) {
+		d->sda_in = (data >> (d->eprMapper.sda_in_bit)) & 1;
 	} else {
 		d->sda_in = d->sda_in_prev;
 	}
@@ -435,19 +435,19 @@ void EEPRomI2C::writeWord(uint32_t address, uint16_t data)
 	address &= ~1;
 
 	// Check if this is the clock line. (/SCL)
-	if (address == d->eprType.type.scl_adr) {
-		d->scl = (data >> (d->eprType.type.scl_bit + 8)) & 1;
-	} else if ((address | 1) == d->eprType.type.scl_adr) {
-		d->scl = (data >> (d->eprType.type.scl_bit)) & 1;
+	if (address == d->eprMapper.scl_adr) {
+		d->scl = (data >> (d->eprMapper.scl_bit + 8)) & 1;
+	} else if ((address | 1) == d->eprMapper.scl_adr) {
+		d->scl = (data >> (d->eprMapper.scl_bit)) & 1;
 	} else {
 		d->scl = d->scl_prev;
 	}
 
 	// Check if this is the data line. (/SDA)
-	if (address == d->eprType.type.sda_in_adr) {
-		d->sda_in = (data >> (d->eprType.type.sda_in_bit + 8)) & 1;
-	} else if ((address | 1) == d->eprType.type.sda_in_adr) {
-		d->sda_in = (data >> (d->eprType.type.sda_in_bit)) & 1;
+	if (address == d->eprMapper.sda_in_adr) {
+		d->sda_in = (data >> (d->eprMapper.sda_in_bit + 8)) & 1;
+	} else if ((address | 1) == d->eprMapper.sda_in_adr) {
+		d->sda_in = (data >> (d->eprMapper.sda_in_bit)) & 1;
 	} else {
 		d->sda_in = d->sda_in_prev;
 	}

src/libgens/Save/EEPRomI2C_p.hpp
@@ -28,6 +28,7 @@
 // C++ includes.
 #include <string>
 
+#include "EEPRomI2C.hpp"
 namespace LibGens {
 
 class EEPRomI2C;
@@ -153,23 +154,13 @@ class EEPRomI2CPrivate
 			{ return (checkSCL_H() && checkSDA_LtoH()); }
 
 	public:
-		/** EEPROM types. **/
-		enum EEPRomType_t {
-			EPR_NONE = 0,
-
-			// Mode 1: 7-bit addressing.
-			EPR_X24C01,
-
-			EPR_MAX
-		};
-
 		/** EEPROM specifications. **/
 		struct EEPRomSpec_t {
 			uint16_t sz_mask;	// Size mask.
 			uint8_t pg_mask;	// Page mask.
 			uint8_t reserved;
 		};
-		static const EEPRomSpec_t eeprom_spec[EPR_MAX];
+		static const EEPRomSpec_t eeprom_spec[EEPRomI2C::EPR_MAX];
 
 		/** EEPROM map. **/
 		struct EEPRomMap_t {
@@ -192,7 +183,7 @@ class EEPRomI2CPrivate
 
 		// Current EEPRom type.
 		EEPRomSpec_t eprSpec;
-		GameEEPRomInfo_t eprType;
+		EEPRomMap_t eprMapper;
 
 	public:
 		/**

src/libgens/CMakeLists.txt
@@ -87,6 +87,7 @@ SET(libgens_SRCS
 	Save/EEPRomI2C.cpp
 	Save/EEPRomI2C_File.cpp
 	Save/EEPRomI2C_DB.cpp
+	Save/EEPRomI2C_Debug.cpp
 	)
 
 SET(libgens_VDP_SRCS

src/libgens/Save/EEPRomI2C.hpp
@@ -135,6 +135,33 @@ class EEPRomI2C
 	protected:
 		/** EEPRom file handling functions. **/
 		int getUsedSize(void);
+
+	public:
+		// Super secret debug stuff!
+		// For use by MDP plugins and test suites.
+		// Return value is an MDP error code.
+
+		/** EEPRom types. **/
+		enum EEPRomType_t {
+			EPR_NONE = 0,
+
+			// Mode 1: 7-bit addressing.
+			EPR_X24C01,
+
+			EPR_MAX
+		};
+
+		int dbg_getEEPRomType(EEPRomType_t *eprType) const;
+		int dbg_setEEPRomType(EEPRomType_t eprType);
+		int dbg_getEEPRomSize(unsigned int *sz) const;
+
+		int dbg_readEEPRom(uint32_t address, uint8_t *data, int length) const;
+		int dbg_writeEEPRom(uint32_t address, const uint8_t *data, int length);
+
+		int dbg_getSCL(uint8_t *scl) const;
+		int dbg_setSCL(uint8_t scl);
+		int dbg_getSDA(uint8_t *sda) const;
+		int dbg_setSDA(uint8_t sda);
 };
 
 }

src/libgens/Save/EEPRomI2C_DB.cpp
@@ -35,7 +35,7 @@
 namespace LibGens {
 
 /** EEPROM specifications. **/
-const EEPRomI2CPrivate::EEPRomSpec_t EEPRomI2CPrivate::eeprom_spec[EPR_MAX] =
+const EEPRomI2CPrivate::EEPRomSpec_t EEPRomI2CPrivate::eeprom_spec[EEPRomI2C::EPR_MAX] =
 {
 	{0, 0, 0},		// EPR_NONE
 	{0x7F, 0x03, 0},	// EPR_X24C01
@@ -47,18 +47,18 @@ const EEPRomI2CPrivate::GameEEPRomInfo_t EEPRomI2CPrivate::rom_db[12] =
 	// TODO: Port other EEPROMs.
 
 	/** SEGA mapper (X24C01) **/
-	{"T-12046"    , 0,      {0x200001, 0x200001, 0x200001, 0, 0, 1, EPR_X24C01}},   /* Megaman - The Wily Wars */
-	{"T-12046"    , 0,      {0x200001, 0x200001, 0x200001, 0, 0, 1, EPR_X24C01}},   /* Megaman - The Wily Wars */
-	{"T-12053"    , 0xEA80, {0x200001, 0x200001, 0x200001, 0, 0, 1, EPR_X24C01}},   /* Rockman Mega World (J) [A] */
-	{"MK-1215"    , 0,      {0x200001, 0x200001, 0x200001, 0, 0, 1, EPR_X24C01}},   /* Evander 'Real Deal' Holyfield's Boxing */
-	{"MK-1228"    , 0,      {0x200001, 0x200001, 0x200001, 0, 0, 1, EPR_X24C01}},   /* Greatest Heavyweights of the Ring (U) */
-	{"G-5538"     , 0,      {0x200001, 0x200001, 0x200001, 0, 0, 1, EPR_X24C01}},   /* Greatest Heavyweights of the Ring (J) */
-	{"PR-1993"    , 0,      {0x200001, 0x200001, 0x200001, 0, 0, 1, EPR_X24C01}},   /* Greatest Heavyweights of the Ring (E) */
-	{"G-4060"     , 0,      {0x200001, 0x200001, 0x200001, 0, 0, 1, EPR_X24C01}},   /* Wonderboy in Monster World */
-	{"00001211-00", 0,      {0x200001, 0x200001, 0x200001, 0, 0, 1, EPR_X24C01}},   /* Sports Talk Baseball */
-	{"00004076-00", 0,      {0x200001, 0x200001, 0x200001, 0, 0, 1, EPR_X24C01}},   /* Honoo no Toukyuuji Dodge Danpei */
-	{"G-4524"     , 0,      {0x200001, 0x200001, 0x200001, 0, 0, 1, EPR_X24C01}},   /* Ninja Burai Densetsu */
-	{"00054503-00", 0,      {0x200001, 0x200001, 0x200001, 0, 0, 1, EPR_X24C01}},   /* Game Toshokan */
+	{"T-12046"    , 0,      {0x200001, 0x200001, 0x200001, 0, 0, 1, EEPRomI2C::EPR_X24C01}},   /* Megaman - The Wily Wars */
+	{"T-12046"    , 0,      {0x200001, 0x200001, 0x200001, 0, 0, 1, EEPRomI2C::EPR_X24C01}},   /* Megaman - The Wily Wars */
+	{"T-12053"    , 0xEA80, {0x200001, 0x200001, 0x200001, 0, 0, 1, EEPRomI2C::EPR_X24C01}},   /* Rockman Mega World (J) [A] */
+	{"MK-1215"    , 0,      {0x200001, 0x200001, 0x200001, 0, 0, 1, EEPRomI2C::EPR_X24C01}},   /* Evander 'Real Deal' Holyfield's Boxing */
+	{"MK-1228"    , 0,      {0x200001, 0x200001, 0x200001, 0, 0, 1, EEPRomI2C::EPR_X24C01}},   /* Greatest Heavyweights of the Ring (U) */
+	{"G-5538"     , 0,      {0x200001, 0x200001, 0x200001, 0, 0, 1, EEPRomI2C::EPR_X24C01}},   /* Greatest Heavyweights of the Ring (J) */
+	{"PR-1993"    , 0,      {0x200001, 0x200001, 0x200001, 0, 0, 1, EEPRomI2C::EPR_X24C01}},   /* Greatest Heavyweights of the Ring (E) */
+	{"G-4060"     , 0,      {0x200001, 0x200001, 0x200001, 0, 0, 1, EEPRomI2C::EPR_X24C01}},   /* Wonderboy in Monster World */
+	{"00001211-00", 0,      {0x200001, 0x200001, 0x200001, 0, 0, 1, EEPRomI2C::EPR_X24C01}},   /* Sports Talk Baseball */
+	{"00004076-00", 0,      {0x200001, 0x200001, 0x200001, 0, 0, 1, EEPRomI2C::EPR_X24C01}},   /* Honoo no Toukyuuji Dodge Danpei */
+	{"G-4524"     , 0,      {0x200001, 0x200001, 0x200001, 0, 0, 1, EEPRomI2C::EPR_X24C01}},   /* Ninja Burai Densetsu */
+	{"00054503-00", 0,      {0x200001, 0x200001, 0x200001, 0, 0, 1, EEPRomI2C::EPR_X24C01}},   /* Game Toshokan */
 
 #if 0
 	/** ACCLAIM mappers **/

src/libgens/Save/EEPRomI2C_Debug.cpp
@@ -1,0 +1,171 @@
+/***************************************************************************
+ * libgens: Gens Emulation Library.                                        *
+ * EEPRomI2C_Debug.cpp: I2C Serial EEPROM handler. (Debugging functions)   *
+ * For use by MDP plugins and test suites.                                 *
+ *                                                                         *
+ * Copyright (c) 2015 by David Korth.                                      *
+ *                                                                         *
+ * This program is free software; you can redistribute it and/or modify it *
+ * under the terms of the GNU General Public License as published by the   *
+ * Free Software Foundation; either version 2 of the License, or (at your  *
+ * option) any later version.                                              *
+ *                                                                         *
+ * This program is distributed in the hope that it will be useful, but     *
+ * WITHOUT ANY WARRANTY; without even the implied warranty of              *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           *
+ * GNU General Public License for more details.                            *
+ *                                                                         *
+ * You should have received a copy of the GNU General Public License along *
+ * with this program; if not, write to the Free Software Foundation, Inc., *
+ * 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.           *
+ ***************************************************************************/
+
+#include "EEPRomI2C.hpp"
+#include "EEPRomI2C_p.hpp"
+
+// C includes. (C++ namespace)
+#include <cstring>
+
+namespace LibGens {
+
+// TODO: Use MDP error codes.
+
+/**
+ * Get the EEPROM chip type.
+ * @param eprType Buffer for EEPROM chip type.
+ * @return MDP error code.
+ */
+int EEPRomI2C::dbg_getEEPRomType(EEPRomType_t *eprType) const
+{
+	*eprType = (EEPRomType_t)d->eprMapper.epr_type;
+	return 0;
+}
+
+/**
+ * Set the EEPROM chip type.
+ * This function will reset the EEPROM status and clear the data.
+ * @param eprType EEPROM chip type.
+ * @return MDP error code.
+ */
+int EEPRomI2C::dbg_setEEPRomType(EEPRomType_t eprType)
+{
+	if (eprType < EPR_NONE || eprType >= EPR_MAX)
+		return -1;
+
+	// Set the EEPROM type.
+	memset(&d->eprMapper, 0, sizeof(d->eprMapper));
+	d->eprMapper.epr_type = eprType;
+
+	// Set the EEPROM chip specification.
+	memcpy(&d->eprSpec, &d->eeprom_spec[eprType], sizeof(d->eprSpec));
+	return 0;
+}
+
+/**
+ * Get the EEPROM size.
+ * This is the total size of the EEPROM based on the chip specification.
+ * @param sz Buffer for the EEPROM size.
+ * @return MDP error code.
+ */
+int EEPRomI2C::dbg_getEEPRomSize(unsigned int *sz) const
+{
+	if (d->eprSpec.sz_mask == 0) {
+		*sz = 0;
+	} else {
+		*sz = d->eprSpec.sz_mask + 1;
+	}
+	return 0;
+}
+
+/**
+ * Read data from the EEPROM.
+ * NOTE: Wraparound is not supported.
+ * @param address Start address.
+ * @param data Buffer to store the data.
+ * @param length Length of data to read.
+ * @return MDP error code.
+ */
+int EEPRomI2C::dbg_readEEPRom(uint32_t address, uint8_t *data, int length) const
+{
+	unsigned int eprSize;
+	dbg_getEEPRomSize(&eprSize);
+	if (eprSize <= 0 || address >= eprSize || address + length >= eprSize) {
+		return -1;
+	}
+
+	// Read the data.
+	memcpy(data, &d->eeprom[address], length);
+	return 0;
+}
+
+/**
+ * Write data to the EEPROM.
+ * NOTE: Wraparound is not supported.
+ * @param address Start address.
+ * @param data Buffer to store the data.
+ * @param length Length of data to read.
+ * @return MDP error code.
+ */
+int EEPRomI2C::dbg_writeEEPRom(uint32_t address, const uint8_t *data, int length)
+{
+	unsigned int eprSize;
+	dbg_getEEPRomSize(&eprSize);
+	if (eprSize <= 0 || address >= eprSize || address + length >= eprSize) {
+		return -1;
+	}
+
+	// Write the data.
+	memcpy(&d->eeprom[address], data, length);
+	d->setDirty();
+	return 0;
+}
+
+/**
+ * Get the state of the /SCL line.
+ * @param scl Buffer to store the /SCL line state. (0 or 1)
+ * @return MDP error code.
+ */
+int EEPRomI2C::dbg_getSCL(uint8_t *scl) const
+{
+	*scl = d->scl;
+	return 0;
+}
+
+/**
+ * Set the state of the /SCL line.
+ * @param scl New /SCL line state. (0 or 1)
+ * @return MDP error code.
+ */
+int EEPRomI2C::dbg_setSCL(uint8_t scl)
+{
+	d->scl = scl & 1;
+	// TODO: Only if /SCL has actually changed?
+	d->processI2Cbit();
+	return 0;
+}
+
+/**
+ * Get the state of the /SDL line.
+ * @param scl Buffer to store the /SDL line state. (0 or 1)
+ * @return MDP error code.
+ */
+int EEPRomI2C::dbg_getSDA(uint8_t *sda) const
+{
+	*sda = d->getSDA();
+	return 0;
+}
+
+/**
+ * Set the state of the /SCL line.
+ * @param scl New /SCL line state. (0 or 1)
+ * @return MDP error code.
+ */
+int EEPRomI2C::dbg_setSDA(uint8_t sda)
+{
+	d->sda_in = sda & 1;
+	// TODO: Only if /SDA has actually changed?
+	d->processI2Cbit();
+	return 0;
+}
+
+}

commit 33b0ad6ec741bf145e36b56f10cf9c5b99b29098

[libgens] EEPRomI2C::setEEPRomType(): Fix eprMapper initialization.
It should be initialized to d->rom_db[type].mapper (formerly .type).

This is a regression from commit 609da84e0d8c2423ba518e6f631a423328d797c4.
([libgens] EEPRomI2C_Debug.cpp: Initial set of debugging functions.)

src/libgens/Save/EEPRomI2C.cpp
@@ -294,7 +294,7 @@ int EEPRomI2C::setEEPRomType(int type)
 	}
 
 	// Set the EEPRom type.
-	memcpy(&d->eprMapper, &d->rom_db[type], sizeof(d->eprMapper));
+	memcpy(&d->eprMapper, &d->rom_db[type].mapper, sizeof(d->eprMapper));
 	memcpy(&d->eprSpec, &d->eeprom_spec[d->eprMapper.epr_type], sizeof(d->eprSpec));
 	return 0;
 }
@@ -305,6 +305,7 @@ int EEPRomI2C::setEEPRomType(int type)
  */
 bool EEPRomI2C::isEEPRomTypeSet(void) const
 {
+	//printf("sz mask: 0x%02X\n", d->eprSpec.sz_mask);
 	return !(d->eprSpec.sz_mask == 0);
 }
 

src/libgens/Save/EEPRomI2C_p.hpp
@@ -177,7 +177,7 @@ class EEPRomI2CPrivate
 		struct GameEEPRomInfo_t {
 			char game_id[16];
 			uint16_t checksum;
-			EEPRomMap_t type;
+			EEPRomMap_t mapper;
 		};
 		static const GameEEPRomInfo_t rom_db[12];
 