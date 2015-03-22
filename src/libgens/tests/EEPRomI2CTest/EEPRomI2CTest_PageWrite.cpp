fbe263c6d1abb667b202a5821c7ebd31f4f88841
David Korth <gerbilsoft@gerbilsoft.com>
2015-03-22 11:35:09 -0400

    [libgens/tests] EEPRomI2CTest_PageWrite: Added the Mode 2 base test and a 24C01 wrapper.
    FIXME: Addresses above 0x7F aren't masked properly for the 24C01,
    resulting in test failures.

--- src/libgens/tests/EEPRomI2CTest/EEPRomI2CTest_PageWrite.cpp
+++ src/libgens/tests/EEPRomI2CTest/EEPRomI2CTest_PageWrite.cpp
@@ -28,12 +28,26 @@
 // Test EEPROM data.
 #include "EEPRomI2CTest_data.h"
 
+// C includes.
+#include <alloca.h>
+
 #include "EEPRomI2CTest.hpp"
 namespace LibGens { namespace Tests {
 
 class EEPRomI2CTest_PageWrite : public EEPRomI2CTest
 {
-	// TODO: Add stuff?
+	protected:
+		/**
+		 * Mode 2 EEPROM: Test full pages writes in an EEPROM.
+		 * Starts at the specified address.
+		 * NOTE: Each byte is written exactly once.
+		 * Wraparound can be tested by starting at an address
+		 * within the page instead of at the start of the page.
+		 * @param addr_start Starting address.
+		 * @param eepromSize EEPROM size.
+		 * @param pgSize Page size.
+		 */
+		void eprMode2_pageWrite(unsigned int addr_start, unsigned int eepromSize, unsigned int pgSize);
 };
 
 /**
@@ -53,10 +67,11 @@ class EEPRomI2CTest_PageWrite : public EEPRomI2CTest
  * within the page instead of at the start of the page.
  * @param addr_start Starting address.
  */
-TEST_P(EEPRomI2CTest_PageWrite, X24C01_seqReadEmpty)
+TEST_P(EEPRomI2CTest_PageWrite, eprX24C01_pageWrite)
 {
 	unsigned int addr_start = GetParam();
 
+	// Set the EEPROM as X24C01.
 	const unsigned int eepromSize = 128;
 	const unsigned int eepromMask = eepromSize - 1;
 	ASSERT_EQ(0, m_eeprom->dbg_setEEPRomMode(EEPRomI2C::EPR_MODE1));
@@ -101,17 +116,112 @@ TEST_P(EEPRomI2CTest_PageWrite, X24C01_seqReadEmpty)
 
 	// Determine the expected EEPROM data.
 	// Should be all empty except for the page being written.
-	uint8_t eeprom_expected[128];
+	uint8_t eeprom_expected[eepromSize];
 	unsigned int pgStart = (addr_start & eepromMask & ~pgMask);
 	memset(eeprom_expected, 0xFF, sizeof(eeprom_expected));
 	memcpy(&eeprom_expected[pgStart], &test_EEPRomI2C_data[pgStart], pgSize);
 
 	// Get the data from the EEPROM.
-	uint8_t eeprom_actual[128];
+	uint8_t eeprom_actual[eepromSize];
 	ASSERT_EQ(0, m_eeprom->dbg_readEEPRom(0x00, eeprom_actual, sizeof(eeprom_actual)));
 
 	// Verify the EEPROM data.
 	CompareByteArrays(eeprom_expected, eeprom_actual, sizeof(eeprom_expected));
+}
+
+/**
+ * Mode 2 EEPROM: Test full pages writes in an EEPROM.
+ * Starts at the specified address.
+ * NOTE: Each byte is written exactly once.
+ * Wraparound can be tested by starting at an address
+ * within the page instead of at the start of the page.
+ * @param addr_start Starting address.
+ * @param eepromSize EEPROM size.
+ * @param pgSize Page size.
+ */
+void EEPRomI2CTest_PageWrite::eprMode2_pageWrite(unsigned int addr_start, unsigned int eepromSize, unsigned int pgSize)
+{
+	const unsigned int eepromMask = eepromSize - 1;
+	const unsigned int pgMask = pgSize - 1;
+
+	// Make sure we're in a STOP condition at first.
+	doStop();
+
+	// EEPROM values.
+	uint8_t cmd, response;
+
+	// START an I2C transfer.
+	m_eeprom->dbg_setSDA(0);	// START
+
+	// Device select.
+	// Assuming device address is 0.
+	cmd = (0xA0 | 0);				// RW=0
+	cmd |= (((addr_start & eepromMask) >> 7) & ~1);	// A10-A8
+	response = sendData(cmd);
+	// Check for ACK.
+	m_eeprom->dbg_getSDA(&response);
+	EXPECT_EQ(0, response) << "EPR_MODE2_DEVICE_ADDRESS, RW=0: NACK received; expected ACK.";
+
+	// Word address, low byte.
+	cmd = (addr_start & 0xFF);			// A7-A0
+	response = sendData(cmd);
+	// Check for ACK.
+	m_eeprom->dbg_getSDA(&response);
+	EXPECT_EQ(0, response) << "EPR_MODE2_WORD_ADDRESS_LOW: NACK received; expected ACK.";
+
+	// Write the page twice.
+	unsigned int src_addr = addr_start & eepromMask;
+	for (unsigned int i = (pgSize * 2); i > 0; i--) {
+		uint8_t data = test_EEPRomI2C_data[src_addr];
+		response = sendData(data);
+
+		// Check for ACK.
+		m_eeprom->dbg_getSDA(&response);
+		EXPECT_EQ(0, response) << "NACK received; expected ACK.";
+
+		// Next byte.
+		src_addr = ((src_addr & ~pgMask) | ((src_addr + 1) & pgMask));
+	}
+
+	// STOP the transfer.
+	doStop();
+
+	// Determine the expected EEPROM data.
+	// Should be all empty except for the page being written.
+	uint8_t *eeprom_expected = (uint8_t*)alloca(eepromSize);
+	unsigned int pgStart = (addr_start & eepromMask & ~pgMask);
+	memset(eeprom_expected, 0xFF, eepromSize);
+	memcpy(&eeprom_expected[pgStart], &test_EEPRomI2C_data[pgStart], pgSize);
+
+	// Get the data from the EEPROM.
+	uint8_t *eeprom_actual = (uint8_t*)alloca(eepromSize);
+	ASSERT_EQ(0, m_eeprom->dbg_readEEPRom(0x00, eeprom_actual, eepromSize));
+
+	// Verify the EEPROM data.
+	CompareByteArrays(eeprom_expected, eeprom_actual, eepromSize);
+}
+
+/**
+ * 24C01: Test sequential writing of full pages.
+ * Starts at the specified address.
+ * NOTE: Each byte is written exactly once.
+ * Wraparound can be tested by starting at an address
+ * within the page instead of at the start of the page.
+ * @param addr_start Starting address.
+ */
+TEST_P(EEPRomI2CTest_PageWrite, epr24C01_pageWrite)
+{
+	unsigned int addr_start = GetParam();
+
+	// Set the EEPROM as 24C01.
+	const unsigned int eepromSize = 128;
+	ASSERT_EQ(0, m_eeprom->dbg_setEEPRomMode(EEPRomI2C::EPR_MODE2));
+	ASSERT_EQ(0, m_eeprom->dbg_setEEPRomSize(eepromSize));
+	const unsigned int pgSize = 8;
+	ASSERT_EQ(0, m_eeprom->dbg_setPageSize(pgSize));
+
+	// Run the Mode 2 test.
+	eprMode2_pageWrite(addr_start, eepromSize, pgSize);
 }
 
 // Sequential Read with various starting addresses.
