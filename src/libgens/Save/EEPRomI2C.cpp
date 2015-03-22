GerbilSoft/gens-gs-ii

commit 03b4190434aa272032c80f7315b39fb8faa0def4

[libgens] EEPRomI2C: Initial "Mode 3" support. (24C32, 24C64)
"Frank Thomas Big Hurt Baseball" appears to save correctly, but it
doesn't read it back, so something's not quite right.

src/libgens/Save/EEPRomI2C.cpp
@@ -151,9 +151,13 @@ void EEPRomI2CPrivate::processI2CShiftIn(void)
 			}
 
 			// Verify the device address.
-			// TODO: This is Mode 2 ONLY.
-			// Needs to be updated for Mode 3.
-			dev_addr = ((data_buf >> 1) & 0x07) & ~(eprChip.sz_mask >> 8);
+			dev_addr = ((data_buf >> 1) & 0x07);
+			if (eprChip.epr_mode == EEPRomI2C::EPR_MODE2) {
+				// Mode 2. Part of the device address may be
+				// high bits of the word address.
+				dev_addr &= ~(eprChip.sz_mask >> 8);
+			}
+
 			// TODO: Mask eprChip.dev_addr?
 			if (dev_addr != eprChip.dev_addr) {
 				// Incorrect device address.
@@ -170,19 +174,20 @@ void EEPRomI2CPrivate::processI2CShiftIn(void)
 				break;
 			}
 
-			dev_addr = (data_buf >> 1) & 0x7;
 			rw = (data_buf & 1);
 			counter = 0;
 
-			// Update the address.
-			// TODO: This is Mode 2 ONLY.
-			// Needs to be updated for Mode 3.
-			address = (dev_addr << 8) | (address & 0xFF);
-			address &= eprChip.sz_mask;
+			// Mode 2. Part of the device address may be
+			// high bits of the word address.
+			if (eprChip.epr_mode == EEPRomI2C::EPR_MODE2) {
+				uint16_t m2_hiaddr = (data_buf << 7) & 0x700;
+				address = m2_hiaddr | (address & 0xFF);
+				address &= eprChip.sz_mask;
+			}
 
 			LOG_MSG(eeprom_i2c, LOG_MSG_LEVEL_DEBUG1,
-				"EPR_MODE2_DEVICE_ADDRESS: dev_type=%1X, dev_addr=%02X, rw=%d",
-				dev_type, dev_addr, rw);
+				"EPR_MODE2_DEVICE_ADDRESS: %02X: type=%1X, dev=%02X, address=%04X, rw=%d",
+				data_buf, dev_type, dev_addr, address, rw);
 
 			if (rw) {
 				// Current address read.
@@ -193,10 +198,11 @@ void EEPRomI2CPrivate::processI2CShiftIn(void)
 				// Write data.
 				// Needs another command word to set
 				// Word Address bits A7-A0.
-				// TODO: Mode 3?
 				data_buf = 0;
 				shift_rw = 0;	// Shifting in.
-				state = EPR_MODE2_WORD_ADDRESS_LOW;
+				state = (eprChip.epr_mode == EEPRomI2C::EPR_MODE2
+					? EPR_MODE2_WORD_ADDRESS_LOW
+					: EPR_MODE3_WORD_ADDRESS_HIGH);
 			}
 			break;
 		}
@@ -204,17 +210,33 @@ void EEPRomI2CPrivate::processI2CShiftIn(void)
 		case EPR_MODE2_WORD_ADDRESS_LOW:
 			// Modes 2, 3: Word address, low byte.
 			// Format: [A7 A6 A5 A4 A3 A2 A1 A0]
-			address = (address & ~0xFF) | data_buf;
+			address = (address & ~0xFF) | (data_buf << 8);
 			address &= eprChip.sz_mask;
 			LOG_MSG(eeprom_i2c, LOG_MSG_LEVEL_DEBUG1,
-				"EPR_MODE2_WORD_ADDRESS_LOW: dev_addr=%02X, data_buf=%02X, address=%02X",
-				dev_addr, data_buf, address);
+				"EPR_MODE3_WORD_ADDRESS_LOW: %02X: dev_addr=%02X, address=%04X",
+				data_buf, dev_addr, address);
 
 			// Write data.
 			counter = 0;
 			data_buf = 0;
 			shift_rw = 0;	// Shifting in.
 			state = EPR_WRITE_DATA;
+			break;
+
+		case EPR_MODE3_WORD_ADDRESS_HIGH:
+			// Modes 2, 3: Word address, high byte.
+			// Format: [A15 A14 A13 A12 A11 A10 A9 A8]
+			address = (address & ~0xFF00) | data_buf;
+			address &= eprChip.sz_mask;
+			LOG_MSG(eeprom_i2c, LOG_MSG_LEVEL_DEBUG1,
+				"EPR_MODE3_WORD_ADDRESS_HIGH: %02X: dev_addr=%02X, address=%04X",
+				data_buf, dev_addr, address);
+
+			// Get the low byte of the word address.
+			counter = 0;
+			data_buf = 0;
+			shift_rw = 0;	// Shifting in.
+			state = EPR_MODE2_WORD_ADDRESS_LOW;
 			break;
 
 		default:

src/libgens/Save/EEPRomI2C_DB.cpp
@@ -35,7 +35,7 @@
 namespace LibGens {
 
 /** ROM database. **/
-const EEPRomI2CPrivate::GameEEPRomInfo_t EEPRomI2CPrivate::rom_db[27] =
+const EEPRomI2CPrivate::GameEEPRomInfo_t EEPRomI2CPrivate::rom_db[30] =
 {
 	// TODO: Port other EEPROMs.
 
@@ -63,9 +63,9 @@ const EEPRomI2CPrivate::GameEEPRomInfo_t EEPRomI2CPrivate::rom_db[27] =
 	// NOTE: Emulated as 24C64; same chip but without "smart" features.
 	// TODO: Needs MODE 3.
 	/* College Slam */
-	//{"T-81576"    , 0,      {0x200001, 0x200001, 0x200000, 0, 0, 0}, {EEPRomI2C::EPR_MODE3, 0, 0x1FFF, 0x07}},
+	{"T-81576"    , 0,      {0x200001, 0x200001, 0x200000, 0, 0, 0}, {EEPRomI2C::EPR_MODE3, 0, 0x1FFF, 0x07}},
 	/* Frank Thomas Big Hurt Baseball */
-	//{"T-81476"    , 0,      {0x200001, 0x200001, 0x200000, 0, 0, 0}, {EEPRomI2C::EPR_MODE3, 0, 0x1FFF, 0x07}},
+	{"T-81476"    , 0,      {0x200001, 0x200001, 0x200000, 0, 0, 0}, {EEPRomI2C::EPR_MODE3, 0, 0x1FFF, 0x07}},
 
 	/** EA mapper (X24C01 only) **/
 	/* Rings of Power */
@@ -92,9 +92,9 @@ const EEPRomI2CPrivate::GameEEPRomInfo_t EEPRomI2CPrivate::rom_db[27] =
 	{"G-5538"     , 0,      {0x200001, 0x200001, 0x200001, 0, 0, 1}, {EEPRomI2C::EPR_MODE1, 0, 0x7F, 0x03}},
 	/* Greatest Heavyweights of the Ring (E) */
 	{"PR-1993"    , 0,      {0x200001, 0x200001, 0x200001, 0, 0, 1}, {EEPRomI2C::EPR_MODE1, 0, 0x7F, 0x03}},
-	/* Wonderboy in Monster World */
+	/* Wonder Boy in Monster World */
 	{"G-4060"     , 0,      {0x200001, 0x200001, 0x200001, 0, 0, 1}, {EEPRomI2C::EPR_MODE1, 0, 0x7F, 0x03}},
-	/* Sports Talk Baseball */
+	/* MLBPA Sports Talk Baseball */
 	{"00001211-00", 0,      {0x200001, 0x200001, 0x200001, 0, 0, 1}, {EEPRomI2C::EPR_MODE1, 0, 0x7F, 0x03}},
 	/* Honoo no Toukyuuji Dodge Danpei */
 	{"00004076-00", 0,      {0x200001, 0x200001, 0x200001, 0, 0, 1}, {EEPRomI2C::EPR_MODE1, 0, 0x7F, 0x03}},
@@ -116,7 +116,7 @@ const EEPRomI2CPrivate::GameEEPRomInfo_t EEPRomI2CPrivate::rom_db[27] =
 	/* 24C16 */
 	/* Micro Machines 2 - Turbo Tournament (E) */
 	{"T-120096"   , 0,      {0x300000, 0x380001, 0x300000, 0, 7, 1}, {EEPRomI2C::EPR_MODE2, 0, 0x7FF, 0x0F}},
-	/* Micro Machines Turbo Tournament 96 */
+	/* Micro Machines - Turbo Tournament '96 */
 	{"00000000-00", 0x165E, {0x300000, 0x380001, 0x300000, 0, 7, 1}, {EEPRomI2C::EPR_MODE2, 0, 0x7FF, 0x0F}},
 	/* Micro Machines Turbo Tournament 96 (Bad?)*/
 	{"00000000-00", 0x2C41, {0x300000, 0x380001, 0x300000, 0, 7, 1}, {EEPRomI2C::EPR_MODE2, 0, 0x7FF, 0x0F}},
@@ -126,7 +126,7 @@ const EEPRomI2CPrivate::GameEEPRomInfo_t EEPRomI2CPrivate::rom_db[27] =
 	// TODO: Needs MODE 3.
 	// TODO: Verify page mask.
 	/* Brian Lara Cricket 96, Shane Warne Cricket */
-	//{"T-120146-50", 0,      {0x300000, 0x380001, 0x300000, 0, 7, 1}, {EEPRomI2C::EPR_MODE3, 0, 0x1FFF, 0x07}},
+	{"T-120146-50", 0,      {0x300000, 0x380001, 0x300000, 0, 7, 1}, {EEPRomI2C::EPR_MODE3, 0, 0x1FFF, 0x07}},
 };
 
 /**

src/libgens/Save/EEPRomI2C_p.hpp
@@ -102,6 +102,10 @@ class EEPRomI2CPrivate
 			// Modes 2, 3: Word address, low byte.
 			// Format: [A7 A6 A5 A4 A3 A2 A1 A0]
 			EPR_MODE2_WORD_ADDRESS_LOW,
+
+			// Mode 3: Word address, high byte.
+			// Format: [A15 A14 A13 A12 A11 A10 A9 A8]
+			EPR_MODE3_WORD_ADDRESS_HIGH,
 		};
 		EEPRomState_t state;
 
@@ -191,7 +195,7 @@ class EEPRomI2CPrivate
 			EEPRomMap_t mapper;
 			EEPRomChip_t epr_chip;		// EEPROM chip specification.
 		};
-		static const GameEEPRomInfo_t rom_db[27];
+		static const GameEEPRomInfo_t rom_db[30];
 
 		// Current EEPRom type.
 		EEPRomChip_t eprChip;

commit ec9a7934a416ad643330f5856362c3f775575d87

[libgens] EEPRomI2C: Fixed the address shift.
The address word should be shifted for "Mode 3" high bytes, not for
"Mode 2" low bytes. This broke seqReadFull and probably all games,
including "Frank Thomas Big Hurt Baseball". Loading works correctly
in that game now.

This is a regression from commit 03b4190434aa272032c80f7315b39fb8faa0def4.
([libgens] EEPRomI2C: Initial "Mode 3" support. (24C32, 24C64))

Also, changed some LOG_MSG() statements to show 4-digit addresses.

src/libgens/Save/EEPRomI2C.cpp
@@ -210,10 +210,10 @@ void EEPRomI2CPrivate::processI2CShiftIn(void)
 		case EPR_MODE2_WORD_ADDRESS_LOW:
 			// Modes 2, 3: Word address, low byte.
 			// Format: [A7 A6 A5 A4 A3 A2 A1 A0]
-			address = (address & ~0xFF) | (data_buf << 8);
+			address = (address & ~0xFF) | data_buf;
 			address &= eprChip.sz_mask;
 			LOG_MSG(eeprom_i2c, LOG_MSG_LEVEL_DEBUG1,
-				"EPR_MODE3_WORD_ADDRESS_LOW: %02X: dev_addr=%02X, address=%04X",
+				"EPR_MODE2_WORD_ADDRESS_LOW: %02X: dev_addr=%02X, address=%04X",
 				data_buf, dev_addr, address);
 
 			// Write data.
@@ -226,7 +226,7 @@ void EEPRomI2CPrivate::processI2CShiftIn(void)
 		case EPR_MODE3_WORD_ADDRESS_HIGH:
 			// Modes 2, 3: Word address, high byte.
 			// Format: [A15 A14 A13 A12 A11 A10 A9 A8]
-			address = (address & ~0xFF00) | data_buf;
+			address = (address & ~0xFF00) | (data_buf << 8);
 			address &= eprChip.sz_mask;
 			LOG_MSG(eeprom_i2c, LOG_MSG_LEVEL_DEBUG1,
 				"EPR_MODE3_WORD_ADDRESS_HIGH: %02X: dev_addr=%02X, address=%04X",
@@ -266,7 +266,7 @@ void EEPRomI2CPrivate::processI2CShiftOut(void)
 			data_buf = eeprom[address];
 			counter = 0;
 			LOG_MSG(eeprom_i2c, LOG_MSG_LEVEL_DEBUG1,
-				"EPR_READ_DATA: ACK received: address=%02X, data_buf=%02X",
+				"EPR_READ_DATA: ACK received: address=%04X, data_buf=%02X",
 				address, data_buf);
 			break;
 
@@ -398,7 +398,7 @@ void EEPRomI2CPrivate::processI2Cbit(void)
 			// TODO: Not necessarily EPR_READ_DATA.
 			if (counter == 8) {
 				LOG_MSG(eeprom_i2c, LOG_MSG_LEVEL_DEBUG1,
-					"EPR_READ_DATA: all 8 bits read: address=%02X",
+					"EPR_READ_DATA: all 8 bits read: address=%04X",
 					address);
 			}
 		}