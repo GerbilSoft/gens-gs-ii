571fc8b3505f09de228d05be8f75a95398032279
David Korth <gerbilsoft@gerbilsoft.com>
2015-04-17 00:03:06 -0400

    [extlib] Added libpng-1.6.17, with APNG patch.
    Ported over from GCN MemCard Recover.
    
    This will be used by libzomg's new PngWriter class, which will move PNG
    format handling for preview images from the frontend to the library.

---
+++ cmake/libs/CheckPNG.cmake
@@ -0,0 +1,31 @@
+# Check for libpng.
+# If libpng isn't found, extlib/libpng/ will be used instead.
+IF(NOT HAVE_PNG)
+
+IF(WIN32)
+	MESSAGE(STATUS "Win32: using internal libpng")
+ELSEIF(USE_INTERNAL_PNG)
+	MESSAGE(STATUS "Using internal libpng")
+ELSE()
+	# TODO: Make libpng support optional?
+	FIND_PACKAGE(ZLIB REQUIRED)
+	FIND_PACKAGE(PNG REQUIRED)
+ENDIF()
+SET(HAVE_ZLIB ${ZLIB_FOUND})
+SET(HAVE_PNG ${PNG_FOUND})
+
+IF(NOT PNG_FOUND)
+	# libpng wasn't found.
+	# NOTE: PNG_LIBRARY will need to be updated if upgrading past libpng-1.6.
+	SET(USE_INTERNAL_PNG 1)
+	SET(PNG_LIBRARY png16_static)
+	SET(PNG_FOUND 1)
+	SET(HAVE_PNG 1)
+	INCLUDE(CheckZLIB)
+	SET(PNG_INCLUDE_DIR
+		"${CMAKE_CURRENT_SOURCE_DIR}/extlib/libpng/"
+		"${CMAKE_CURRENT_BINARY_DIR}/extlib/libpng/"
+		)
+ENDIF(NOT PNG_FOUND)
+
+ENDIF(NOT HAVE_PNG)
