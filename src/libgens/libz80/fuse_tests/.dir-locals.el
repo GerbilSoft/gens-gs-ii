0d396de222c890d8d9f520450deb6bc938ba3a02
David Korth <gerbilsoft@gerbilsoft.com>
2015-04-21 19:36:06 -0400

    [libz80] Initial import of libz80 into libgens.
    libz80 is a GPL-licensed Z80 emulator written in C. It's more accurate and
    supports more undocumented Z80 functionality than the old Gens Z80 code.
    
    This is only an initial import. The code doesn't build yet, since I need
    to write CMake rules to build and run the opcode table generator.

---
+++ src/libgens/libz80/fuse_tests/.dir-locals.el
@@ -0,0 +1,9 @@
+;;; Directory Local Variables
+;;; See Info node `(emacs) Directory Variables' for more information.
+
+((c-mode
+  (indent-tabs-mode . nil)
+  (c-basic-offset . 2)
+  (tab-width . 2)
+  (c-file-style . "bsd")))
+
