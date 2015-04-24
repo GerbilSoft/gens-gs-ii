54dd5f6f8517e1b7a9bfa301716fe876b149792f
David Korth <gerbilsoft@gerbilsoft.com>
2015-04-24 00:22:55 -0400

    Removed libz80.
    CZ80 is much faster and more accurate, plus it doesn't require an
    opcode table generation step.

--- src/libgens/libz80/.dir-locals.el
+++
@@ -1,9 +0,0 @@
-;;; Directory Local Variables
-;;; See Info node `(emacs) Directory Variables' for more information.
-
-((c-mode
-  (indent-tabs-mode . t)
-  (c-basic-offset . 4)
-  (tab-width . 4)
-  (c-file-style . "bsd")))
-
--- src/libgens/libz80/fuse_tests/.dir-locals.el
+++
@@ -1,9 +0,0 @@
-;;; Directory Local Variables
-;;; See Info node `(emacs) Directory Variables' for more information.
-
-((c-mode
-  (indent-tabs-mode . nil)
-  (c-basic-offset . 2)
-  (tab-width . 2)
-  (c-file-style . "bsd")))
-
