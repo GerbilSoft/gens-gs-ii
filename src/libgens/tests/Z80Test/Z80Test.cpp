a2f36a1bb9bd8dccf6133b53b7dc1010a327391a
David Korth <gerbilsoft@gerbilsoft.com>
2015-05-09 10:57:43 -0400

    [libgens/tests] Z80Test: Parse the output for error messages.
    Colorize error messages in red. This is based on Google Test's
    colorization code, and works on both Linux and Windows.
    
    If an error occurs in a test, the test will now raise an assertion,
    causing it to fail in Google Test.

--- src/libgens/tests/Z80Test/Z80Test.cpp
+++ src/libgens/tests/Z80Test/Z80Test.cpp
@@ -26,12 +26,23 @@
 
 #include "cz80/cz80.h"
 
+// C includes.
+#include <stdlib.h>
+
 // C includes. (C++ namespace)
 #include <cstdio>
 #include <cstring>
-using namespace std;
 
-// NOTE: This test suite uses libz80 directly.
+#ifdef _WIN32
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+#include <windows.h>
+#include <io.h>
+#else /* !_WIN32 */
+#include <unistd.h>
+#endif /* _WIN32 */
+
+// NOTE: This test suite uses CZ80 directly.
 // The Z80 class is currently hard-coded for MD only.
 
 namespace LibGens { namespace Tests {
@@ -39,7 +50,15 @@ namespace LibGens { namespace Tests {
 class Z80Test : public ::testing::Test
 {
 	protected:
-		Z80Test() { }
+		Z80Test()
+			: use_color(false)
+			, is_red(false)
+			, has_err_occurred(false)
+#ifdef _WIN32
+			, old_attr(FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE)
+#endif /* _WIN32 */
+		{ }
+
 		virtual ~Z80Test() { }
 
 		virtual void SetUp(void) override;
@@ -73,10 +92,117 @@ class Z80Test : public ::testing::Test
 
 		uint8_t Z80_INPort(uint16_t address);
 		void Z80_OUTPort(uint16_t address, uint8_t data);
+
+		// Colorized output
+		static bool ShouldUseColor(bool stdout_is_tty);
+		void setTextColor(bool do_red);
+		bool use_color;         // True if we should use color.
+		bool is_red;            // True if currently printing red text.
+		bool has_err_occurred;  // True if any red text has been printed.
+#ifdef _WIN32
+		WORD old_attr;          // Previous character attributes. (Win32 only)
+#endif /* _WIN32 */
 };
+
+/**
+ * Should we use color in terminal output?
+ * Based on the internal function from gtest-1.7.0.
+ * @param stdout_is_tty True if stdout is a TTY.
+ * @return True if we should use color; false if not.
+ */
+bool Z80Test::ShouldUseColor(bool stdout_is_tty)
+{
+	const char* const gtest_color = testing::GTEST_FLAG(color).c_str();
+
+	if (!strcasecmp(gtest_color, "auto")) {
+#ifdef _WIN32
+		// On Windows the TERM variable is usually not set, but the
+		// console there does support colors.
+		return stdout_is_tty;
+#else /* !_WIN32 */
+		// On non-Windows platforms, we rely on the TERM variable.
+		const char* const term = getenv("TERM");
+		const bool term_supports_color =
+			!strcmp(term, "xterm") ||
+			!strcmp(term, "xterm-color") ||
+			!strcmp(term, "xterm-256color") ||
+			!strcmp(term, "screen") ||
+			!strcmp(term, "screen-256color") ||
+			!strcmp(term, "linux") ||
+			!strcmp(term, "cygwin");
+		return stdout_is_tty && term_supports_color;
+#endif /* WIN32 */
+	}
+
+	return !strcasecmp(gtest_color, "yes") ||
+		!strcasecmp(gtest_color, "true") ||
+		!strcasecmp(gtest_color, "t") ||
+		!strcmp(gtest_color, "1");
+	// We take "yes", "true", "t", and "1" as meaning "yes".  If the
+	// value is neither one of these nor "auto", we treat it as "no" to
+	// be conservative.
+}
+
+/**
+ * Set the text color.
+ * @param do_red If true, make the text red; otherwise, revert to standard colors.
+ */
+void Z80Test::setTextColor(bool do_red)
+{
+	if (is_red == do_red)
+		return;
+	is_red = do_red;
+	if (!use_color) {
+		// Color is disabled.
+		if (do_red) {
+			// Make sure we still mark that an error occurred.
+			has_err_occurred = true;
+		}
+		return;
+	}
+
+#ifdef _WIN32
+	const HANDLE stdout_handle = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	if (do_red) {
+		// Make the text red.
+		has_err_occurred = true;
+
+		// Gets the current text color.
+		CONSOLE_SCREEN_BUFFER_INFO buffer_info;
+		GetConsoleScreenBufferInfo(stdout_handle, &buffer_info);
+		old_attr = buffer_info.wAttributes;
+
+		// We need to flush the stream buffers into the console before each
+		// SetConsoleTextAttribute call lest it affect the text that is already
+		// printed but has not yet reached the console.
+		fflush(stdout);
+		SetConsoleTextAttribute(stdout_handle, FOREGROUND_RED | FOREGROUND_INTENSITY);
+	} else {
+		// Restore the previous text color.
+		SetConsoleTextAttribute(stdout_handle, old_attr);
+	}
+#else /* !_WIN32 */
+	if (do_red) {
+		has_err_occurred = true;
+		printf("\033[0;31m");
+	} else {
+		printf("\033[m");  // Resets the terminal to default.
+	}
+#endif /* _WIN32 */
+}
 
 void Z80Test::SetUp()
 {
+	// Determine if ZEXALL output should be colorized.
+	use_color = ShouldUseColor(!!isatty(fileno(stdout)));
+	is_red = false;
+	has_err_occurred = false;
+#ifdef _WIN32
+	old_attr = FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE;
+#endif /* _WIN32 */
+
+	// Not halted initially.
 	halt = false;
 
 	// Initialize Z80 memory.
@@ -139,16 +265,18 @@ uint8_t Z80Test::Z80_INPort(uint16_t address)
 void Z80Test::Z80_OUTPort(uint16_t address, uint8_t data)
 {
 	switch (address & 0xFF) {
-		case 0x01:
-			// stdout
-			// TODO: Lines beginning with '*' should be in red and trigger an error.
-			fputc(data, stdout);
-			fflush(stdout);
-			break;
-
-		case 0x02:
-			// stderr
-			// TODO: Make this red?
+		case 0x01:	// stdout
+		case 0x02:	// stderr
+			// NOTE: CP/M doesn't actually have stderr.
+			if (data == 'E') {
+				// Start of "ERROR" text.
+				// Should be printed in red.
+				setTextColor(true);
+			} else if (data == '\r' || data == '\n') {
+				// End of the line.
+				// Revert back to normal text colors.
+				setTextColor(false);
+			}
 			fputc(data, stdout);
 			fflush(stdout);
 			break;
@@ -156,6 +284,8 @@ void Z80Test::Z80_OUTPort(uint16_t address, uint8_t data)
 		case 0xFF:
 			// End of program.
 			halt = true;
+			setTextColor(false);
+			fflush(stdout);
 			break;
 	}
 }
@@ -185,7 +315,8 @@ TEST_F(Z80Test, zexdoc)
 	}
 	printf("\n");
 
-	// TODO: If any errors occurred, fail the test.
+	// If any errors have occurred, fail the test.
+	ASSERT_FALSE(has_err_occurred);
 }
 
 /**
@@ -211,7 +342,8 @@ TEST_F(Z80Test, zexall)
 	}
 	printf("\n");
 
-	// TODO: If any errors occurred, fail the test.
+	// If any errors have occurred, fail the test.
+	ASSERT_FALSE(has_err_occurred);
 }
 
 } }
@@ -219,7 +351,8 @@ TEST_F(Z80Test, zexall)
 
 int main(int argc, char *argv[])
 {
-	fprintf(stderr, "LibGens test suite: Z80 tests.\n\n");
+	fprintf(stderr, "LibGens test suite: Z80 tests.\n");
+	fprintf(stderr, "Using the CZ80 emulator.\n\n");
 
 	::testing::InitGoogleTest(&argc, argv);
 	//LibGens::Init(); /* not needed for Z80 tests */
