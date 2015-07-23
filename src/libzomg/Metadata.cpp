GerbilSoft/gens-gs-ii

commit f8a126969828d629f5949f96275af6c82f8d6826

[libzomg] Metadata: Always save "System" after Emulator/OS/etc.
While "System" is a "required" field for ZOMG.ini, it looks better if
Emulator/OS/etc. is before all of the ROM-specific information.

The ZOMG specification doesn't require any specific field ordering,
so this shouldn't cause any issues. (Gens/GS II doesn't even read
ZOMG.ini on load right now, anyway.)

src/libzomg/Metadata.cpp
@@ -194,7 +194,8 @@ std::string Metadata::toZomgIni(int metaFlags) const
 	d->WriteValue(oss, "FileType", "Zipped Original Memory from Genesis");
 	// TODO: Get the ZomgVersion from somewhere.
 	d->WriteValue(oss, "Version", "0.1-DEV-UNSTABLE");
-	d->WriteValue(oss, "System", d->systemId);
+
+	// System metadata.
 	if (metaFlags & MF_Emulator) {
 		d->WriteValue(oss, "Creator", d->creatorInfo.creator);
 		d->WriteValue(oss, "CreatorVersion", d->creatorInfo.creatorVersion);
@@ -211,6 +212,9 @@ std::string Metadata::toZomgIni(int metaFlags) const
 	if (metaFlags & MF_Author) {
 		d->WriteValue(oss, "Author", d->sysInfo.username);
 	}
+
+	// ROM metadata.
+	d->WriteValue(oss, "System", d->systemId);
 	if (metaFlags & MF_RomInfo) {
 		d->WriteValue(oss, "ROM", d->romFilename);
 		d->WriteValue(oss, "ROM_CRC32", d->romCrc32, 8, true);
@@ -263,14 +267,6 @@ int Metadata::toPngData(png_structp png_ptr, png_infop info_ptr, int metaFlags) 
 
 	/** "Description" field. **/
 
-	// System.
-	// NOTE: This is required for ZOMG.ini, so we'll
-	// always save it in screenshots as well.
-	ostringstream desc;
-	if (!d->systemId.empty()) {
-		desc << "System: " << d->systemId << '\n';
-	}
-
 	// Emulator information.
 	if ((metaFlags & MF_Emulator) && !d->creatorInfo.creator.empty()) {
 		desc << "Emulator: " << d->creatorInfo.creator << '\n';
@@ -291,6 +287,14 @@ int Metadata::toPngData(png_structp png_ptr, png_infop info_ptr, int metaFlags) 
 		if (!d->sysInfo.cpu.empty()) {
 			desc << "CPU: " << d->sysInfo.cpu << '\n';
 		}
+	}
+
+	// System.
+	// NOTE: This is required for ZOMG.ini, so we'll
+	// always save it in screenshots as well.
+	ostringstream desc;
+	if (!d->systemId.empty()) {
+		desc << "System: " << d->systemId << '\n';
 	}
 
 	// ROM information.

commit 1e0befec1a321a79b1537615c0bee86ba0b19048

[libzomg] Metadata::toPngData(): Fixed a compile error.
Ended up moving 'ostringstream desc;' along with the "System" field,
which broke other code that used this variable.

This is a regression from commit f8a126969828d629f5949f96275af6c82f8d6826.
([libzomg] Metadata: Always save "System" after Emulator/OS/etc.)

src/libzomg/Metadata.cpp
@@ -268,6 +268,7 @@ int Metadata::toPngData(png_structp png_ptr, png_infop info_ptr, int metaFlags) 
 	/** "Description" field. **/
 
 	// Emulator information.
+	ostringstream desc;
 	if ((metaFlags & MF_Emulator) && !d->creatorInfo.creator.empty()) {
 		desc << "Emulator: " << d->creatorInfo.creator << '\n';
 		if (!d->creatorInfo.creatorVersion.empty()) {
@@ -292,7 +293,6 @@ int Metadata::toPngData(png_structp png_ptr, png_infop info_ptr, int metaFlags) 
 	// System.
 	// NOTE: This is required for ZOMG.ini, so we'll
 	// always save it in screenshots as well.
-	ostringstream desc;
 	if (!d->systemId.empty()) {
 		desc << "System: " << d->systemId << '\n';
 	}