66006453c48f7aae408e49eb8e3604dcfee0e30e
David Korth <gerbilsoft@gerbilsoft.com>
2015-07-27 22:13:13 -0400

    [libzomg] Zomg.hpp: Marked all virtual functions as final.
    I'm pretty sure I won't be implementing any subclasses of Zomg.
    
    If I were to implement a Genecyst/Gens savestate loader, it'd be a
    direct subclass of ZomgBase, since the file is basically a blob
    and has no structure within the file itself, whereas ZOMG is a
    Zip file and has a central directory.

--- src/libzomg/Zomg.hpp
+++ src/libzomg/Zomg.hpp
@@ -50,7 +50,7 @@ class Zomg : public ZomgBase
 		Zomg &operator=(const Zomg &);
 
 	public:
-		virtual void close(void) override;
+		virtual void close(void) final;
 
 		/**
 		 * Detect if a savestate is supported by this class.
@@ -75,42 +75,42 @@ class Zomg : public ZomgBase
 		 * @param img_data Image data. (Caller must free img_data->data.)
 		 * @return 0 on success; non-zero on error.
 		 */
-		virtual int loadPreview(_Zomg_Img_Data_t *img_data) override;
+		virtual int loadPreview(_Zomg_Img_Data_t *img_data) final;
 
 		// VDP
-		virtual int loadVdpReg(uint8_t *reg, size_t siz) override;
-		virtual int loadVdpCtrl_8(_Zomg_VDP_ctrl_8_t *ctrl) override;
-		virtual int loadVdpCtrl_16(_Zomg_VDP_ctrl_16_t *ctrl) override;
-		virtual int loadVRam(void *vram, size_t siz, ZomgByteorder_t byteorder) override;
-		virtual int loadCRam(_Zomg_CRam_t *cram, ZomgByteorder_t byteorder) override;
+		virtual int loadVdpReg(uint8_t *reg, size_t siz) final;
+		virtual int loadVdpCtrl_8(_Zomg_VDP_ctrl_8_t *ctrl) final;
+		virtual int loadVdpCtrl_16(_Zomg_VDP_ctrl_16_t *ctrl) final;
+		virtual int loadVRam(void *vram, size_t siz, ZomgByteorder_t byteorder) final;
+		virtual int loadCRam(_Zomg_CRam_t *cram, ZomgByteorder_t byteorder) final;
 		/// MD-specific
-		virtual int loadMD_VSRam(uint16_t *vsram, size_t siz, ZomgByteorder_t byteorder) override;
-		virtual int loadMD_VDP_SAT(uint16_t *vdp_sat, size_t siz, ZomgByteorder_t byteorder) override;
+		virtual int loadMD_VSRam(uint16_t *vsram, size_t siz, ZomgByteorder_t byteorder) final;
+		virtual int loadMD_VDP_SAT(uint16_t *vdp_sat, size_t siz, ZomgByteorder_t byteorder) final;
 
 		// Audio
-		virtual int loadPsgReg(_Zomg_PsgSave_t *state) override;
+		virtual int loadPsgReg(_Zomg_PsgSave_t *state) final;
 		/// MD-specific
-		virtual int loadMD_YM2612_reg(_Zomg_Ym2612Save_t *state) override;
+		virtual int loadMD_YM2612_reg(_Zomg_Ym2612Save_t *state) final;
 
 		// Z80
-		virtual int loadZ80Mem(uint8_t *mem, size_t siz) override;
-		virtual int loadZ80Reg(_Zomg_Z80RegSave_t *state) override;
+		virtual int loadZ80Mem(uint8_t *mem, size_t siz) final;
+		virtual int loadZ80Reg(_Zomg_Z80RegSave_t *state) final;
 
 		// M68K (MD-specific)
-		virtual int loadM68KMem(uint16_t *mem, size_t siz, ZomgByteorder_t byteorder) override;
-		virtual int loadM68KReg(_Zomg_M68KRegSave_t *state) override;
+		virtual int loadM68KMem(uint16_t *mem, size_t siz, ZomgByteorder_t byteorder) final;
+		virtual int loadM68KReg(_Zomg_M68KRegSave_t *state) final;
 
 		// MD-specific registers
-		virtual int loadMD_IO(_Zomg_MD_IoSave_t *state) override;
-		virtual int loadMD_Z80Ctrl(_Zomg_MD_Z80CtrlSave_t *state) override;
-		virtual int loadMD_TimeReg(_Zomg_MD_TimeReg_t *state) override;
-		virtual int loadMD_TMSS_reg(_Zomg_MD_TMSS_reg_t *tmss) override;
+		virtual int loadMD_IO(_Zomg_MD_IoSave_t *state) final;
+		virtual int loadMD_Z80Ctrl(_Zomg_MD_Z80CtrlSave_t *state) final;
+		virtual int loadMD_TimeReg(_Zomg_MD_TimeReg_t *state) final;
+		virtual int loadMD_TMSS_reg(_Zomg_MD_TMSS_reg_t *tmss) final;
 
 		// Miscellaneous
-		virtual int loadSRam(uint8_t *sram, size_t siz) override;
-		virtual int loadEEPRomCtrl(_Zomg_EPR_ctrl_t *ctrl) override;
-		virtual int loadEEPRomCache(uint8_t *cache, size_t siz) override;
-		virtual int loadEEPRom(uint8_t *eeprom, size_t siz) override;
+		virtual int loadSRam(uint8_t *sram, size_t siz) final;
+		virtual int loadEEPRomCtrl(_Zomg_EPR_ctrl_t *ctrl) final;
+		virtual int loadEEPRomCache(uint8_t *cache, size_t siz) final;
+		virtual int loadEEPRom(uint8_t *eeprom, size_t siz) final;
 
 		/**
 		 * Save savestate functions.
@@ -155,39 +155,39 @@ class Zomg : public ZomgBase
 					const Metadata *metadata, int metaFlags);
 
 		// VDP
-		virtual int saveVdpReg(const uint8_t *reg, size_t siz) override;
-		virtual int saveVdpCtrl_8(const _Zomg_VDP_ctrl_8_t *ctrl) override;
-		virtual int saveVdpCtrl_16(const _Zomg_VDP_ctrl_16_t *ctrl) override;
-		virtual int saveVRam(const void *vram, size_t siz, ZomgByteorder_t byteorder) override;
-		virtual int saveCRam(const _Zomg_CRam_t *cram, ZomgByteorder_t byteorder) override;
+		virtual int saveVdpReg(const uint8_t *reg, size_t siz) final;
+		virtual int saveVdpCtrl_8(const _Zomg_VDP_ctrl_8_t *ctrl) final;
+		virtual int saveVdpCtrl_16(const _Zomg_VDP_ctrl_16_t *ctrl) final;
+		virtual int saveVRam(const void *vram, size_t siz, ZomgByteorder_t byteorder) final;
+		virtual int saveCRam(const _Zomg_CRam_t *cram, ZomgByteorder_t byteorder) final;
 		/// MD-specific
-		virtual int saveMD_VSRam(const uint16_t *vsram, size_t siz, ZomgByteorder_t byteorder) override;
-		virtual int saveMD_VDP_SAT(const void *vdp_sat, size_t siz, ZomgByteorder_t byteorder) override;
+		virtual int saveMD_VSRam(const uint16_t *vsram, size_t siz, ZomgByteorder_t byteorder) final;
+		virtual int saveMD_VDP_SAT(const void *vdp_sat, size_t siz, ZomgByteorder_t byteorder) final;
 
 		// Audio
-		virtual int savePsgReg(const _Zomg_PsgSave_t *state) override;
+		virtual int savePsgReg(const _Zomg_PsgSave_t *state) final;
 		/// MD-specific
-		virtual int saveMD_YM2612_reg(const _Zomg_Ym2612Save_t *state) override;
+		virtual int saveMD_YM2612_reg(const _Zomg_Ym2612Save_t *state) final;
 
 		// Z80
-		virtual int saveZ80Mem(const uint8_t *mem, size_t siz) override;
-		virtual int saveZ80Reg(const _Zomg_Z80RegSave_t *state) override;
+		virtual int saveZ80Mem(const uint8_t *mem, size_t siz) final;
+		virtual int saveZ80Reg(const _Zomg_Z80RegSave_t *state) final;
 
 		// M68K (MD-specific)
-		virtual int saveM68KMem(const uint16_t *mem, size_t siz, ZomgByteorder_t byteorder) override;
-		virtual int saveM68KReg(const _Zomg_M68KRegSave_t *state) override;
+		virtual int saveM68KMem(const uint16_t *mem, size_t siz, ZomgByteorder_t byteorder) final;
+		virtual int saveM68KReg(const _Zomg_M68KRegSave_t *state) final;
 
 		// MD-specific registers
-		virtual int saveMD_IO(const _Zomg_MD_IoSave_t *state) override;
-		virtual int saveMD_Z80Ctrl(const _Zomg_MD_Z80CtrlSave_t *state) override;
-		virtual int saveMD_TimeReg(const _Zomg_MD_TimeReg_t *state) override;
-		virtual int saveMD_TMSS_reg(const _Zomg_MD_TMSS_reg_t *tmss) override;
+		virtual int saveMD_IO(const _Zomg_MD_IoSave_t *state) final;
+		virtual int saveMD_Z80Ctrl(const _Zomg_MD_Z80CtrlSave_t *state) final;
+		virtual int saveMD_TimeReg(const _Zomg_MD_TimeReg_t *state) final;
+		virtual int saveMD_TMSS_reg(const _Zomg_MD_TMSS_reg_t *tmss) final;
 
 		// Miscellaneous
-		virtual int saveSRam(const uint8_t *sram, size_t siz) override;
-		virtual int saveEEPRomCtrl(const _Zomg_EPR_ctrl_t *ctrl) override;
-		virtual int saveEEPRomCache(const uint8_t *cache, size_t siz) override;
-		virtual int saveEEPRom(const uint8_t *eeprom, size_t siz) override;
+		virtual int saveSRam(const uint8_t *sram, size_t siz) final;
+		virtual int saveEEPRomCtrl(const _Zomg_EPR_ctrl_t *ctrl) final;
+		virtual int saveEEPRomCache(const uint8_t *cache, size_t siz) final;
+		virtual int saveEEPRom(const uint8_t *eeprom, size_t siz) final;
 };
 
 }
