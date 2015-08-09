45a939ee45d12985528012668525f23de0248688
David Korth <gerbilsoft@gerbilsoft.com>
2015-08-08 20:17:28 -0400

    [cmake] options.cmake: Cleaned up this file a bit.
    Removed extraneous tabs and spaces.

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -1,12 +1,12 @@
 # Build options.
 
 # LZMA SDK.
-OPTION(USE_LZMA			"Use the LZMA SDK to decompress 7-Zip archives." 		1)
+OPTION(USE_LZMA "Use the LZMA SDK to decompress 7-Zip archives." 1)
 
 # ZLIB.
 # TODO: Make ZLIB optional? (This would disable savestates...)
 IF(NOT WIN32)
-	OPTION(USE_INTERNAL_ZLIB	"Always use the internal copy of ZLIB." 		0)
+	OPTION(USE_INTERNAL_ZLIB "Always use the internal copy of ZLIB." 0)
 ELSE(NOT WIN32)
 	# TODO: Allow use of external zlib on Win32?
 	SET(USE_INTERNAL_ZLIB 1)
@@ -16,7 +16,7 @@ ENDIF(NOT WIN32)
 # NOTE: Also controls internal zlib usage.
 # TODO: Separate it so int libpng / ext zlib can be used?
 IF(NOT WIN32)
-	OPTION(USE_INTERNAL_PNG "Always use the internal copy of libpng."       0)
+	OPTION(USE_INTERNAL_PNG "Always use the internal copy of libpng." 0)
 ELSE(NOT WIN32)
 	# TODO: Allow use of external libpng on Win32?
 	SET(USE_INTERNAL_PNG 1)
@@ -24,9 +24,9 @@ ENDIF(NOT WIN32)
 
 # OpenGL.
 # TODO: Make OpenGL optional once other backends have been added.
-OPTION(USE_GLEW			"Use GLEW for OpenGL extension support." 			1)
+OPTION(USE_GLEW "Use GLEW for OpenGL extension support." 1)
 IF(NOT WIN32)
-	OPTION(USE_INTERNAL_GLEW	"Always use the internal copy of GLEW." 		0)
+	OPTION(USE_INTERNAL_GLEW "Always use the internal copy of GLEW." 0)
 ELSE(NOT WIN32)
 	# Always use the internal GLEW on Win32 regardless.
 	# TODO: Allow use of external GLEW on Win32?
@@ -34,16 +34,16 @@ ELSE(NOT WIN32)
 ENDIF(NOT WIN32)
 
 # Additional stuff.
-OPTION(BUILD_DOC		"Build documentation." 						1)
+OPTION(BUILD_DOC "Build documentation." 1)
 
 # Link-time optimization.
-OPTION(ENABLE_LTO	"Enable link-time optimization. (Release builds only)"	0)
+OPTION(ENABLE_LTO "Enable link-time optimization. (Release builds only)" 0)
 
 # Split debug information into a separate file.
-OPTION(SPLIT_DEBUG		"Split debug information into a separate file."			1)
+OPTION(SPLIT_DEBUG "Split debug information into a separate file." 1)
 
 # Install the split debug file.
-OPTION(INSTALL_DEBUG		"Install the split debug file."					1)
+OPTION(INSTALL_DEBUG "Install the split debug file." 1)
 
 # Compress the executable with UPX.
-OPTION(COMPRESS_EXE		"Compress the executable with UPX."				0)
+OPTION(COMPRESS_EXE "Compress the executable with UPX." 0)
