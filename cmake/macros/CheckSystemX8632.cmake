b2bd8cf7ad84be0f3dba24c42ad0685f94a4b102
David Korth <gerbilsoft@gerbilsoft.com>
2015-08-23 11:44:57 -0400

    [cmake] CheckSystemX8632.cmake: Save the result to an internal variable.
    This prevents us from having to run the same check multiple times
    if the macro is used in multiple places.

--- cmake/macros/CheckSystemX8632.cmake
+++ cmake/macros/CheckSystemX8632.cmake
@@ -21,26 +21,28 @@ INCLUDE(CheckCSourceCompiles)
 
 MACRO(CHECK_SYSTEM_X86_32 _RESULT)
 	# Flag listing borrowed from GNU autoconf's AC_PROG_CC_C99 macro.
-	UNSET(${_RESULT})
-	
-	MESSAGE(STATUS "Checking if the system is X86_32:")
-	
-	CHECK_C_SOURCE_COMPILES(
-		"#if (defined(__i386) || defined(__i386__) || defined(_M_IX86)) && (!defined(__x86_64) && !defined(__x86_64__) && !defined(__amd64) && !defined(__amd64__) && !defined(_M_X64) && !defined(_M_AMD64))
-		/* System is X86_32 */
-		#else
-		/* System is not X86_32 */
-		#error Not X86_32. Aborting.
-		#endif
-		
-		int main(void) { return 0; }" CHECK_X86_32)
-	
-	IF(CHECK_X86_32)
-		SET(${_RESULT} 1)
-		MESSAGE(STATUS "Checking if the system is X86_32: yes")
-	ELSE(CHECK_X86_32)
+	IF(NOT DEFINED CHECK_X86_32)
 		UNSET(${_RESULT})
-		MESSAGE(STATUS "Checking if the system is X86_32: no")
-	ENDIF(CHECK_X86_32)
-	UNSET(CHECK_X86_32)
+
+		MESSAGE(STATUS "Checking if the system is X86_32:")
+
+		CHECK_C_SOURCE_COMPILES(
+			"#if (defined(__i386) || defined(__i386__) || defined(_M_IX86)) && (!defined(__x86_64) && !defined(__x86_64__) && !defined(__amd64) && !defined(__amd64__) && !defined(_M_X64) && !defined(_M_AMD64))
+			/* System is X86_32 */
+			#else
+			/* System is not X86_32 */
+			#error Not X86_32. Aborting.
+			#endif
+
+			int main(void) { return 0; }" CHECK_X86_32)
+
+		IF(CHECK_X86_32)
+			SET(CHECKED_X86_32 1 CACHE INTERNAL "Is the system X86_32?")
+			MESSAGE(STATUS "Checking if the system is X86_32: yes")
+		ELSE(CHECK_X86_32)
+			SET(CHECKED_X86_32 0 CACHE INTERNAL "Is the system X86_32?")
+			MESSAGE(STATUS "Checking if the system is X86_32: no")
+		ENDIF(CHECK_X86_32)
+	ENDIF(NOT DEFINED CHECK_X86_32)
+	SET(${_RESULT} ${CHECK_X86_32})
 ENDMACRO (CHECK_SYSTEM_X86_32)
