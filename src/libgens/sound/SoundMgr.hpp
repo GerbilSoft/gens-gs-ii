280125385e3b39c4b24921be95f51a8e37197be5
David Korth <gerbilsoft@gerbilsoft.com>
2015-08-23 12:37:14 -0400

    [libgens] SoundMgr: Moved some stuff into a private class.
    Preparations for moving the audio write functions from the frontends
    to libgens, with MMX and SSE2 optimizations.

--- src/libgens/sound/SoundMgr.hpp
+++ src/libgens/sound/SoundMgr.hpp
@@ -5,7 +5,7 @@
  *                                                                         *
  * Copyright (c) 1999-2002 by Stéphane Dallongeville                       *
  * Copyright (c) 2003-2004 by Stéphane Akhoun                              *
- * Copyright (c) 2008-2010 by David Korth                                  *
+ * Copyright (c) 2008-2015 by David Korth                                  *
  *                                                                         *
  * This program is free software; you can redistribute it and/or modify it *
  * under the terms of the GNU General Public License as published by the   *
@@ -28,39 +28,35 @@
 // C includes.
 #include <stdint.h>
 
+// C includes. (C++ namespace)
+#include <cassert>
+
 // Audio ICs.
 #include "../sound/Psg.hpp"
 #include "../sound/Ym2612.hpp"
 
-namespace LibGens
-{
+namespace LibGens {
 
 class SoundMgr
 {
 	public:
 		static void Init(void);
 		static void End(void);
-		
+
 		static void ReInit(int rate, bool isPal, bool preserveState = false);
-		static inline void SetRate(int rate, bool preserveState = true)
-		{
-			ReInit(rate, ms_IsPal, preserveState);
-		}
-		static inline void SetRegion(bool isPal, bool preserveState = true)
-		{
-			ReInit(ms_Rate, isPal, preserveState);
-		}
-		
-		static inline int GetSegLength(void) { return ms_SegLength; }
-		
+		static void SetRate(int rate, bool preserveState = true);
+		static void SetRegion(bool isPal, bool preserveState = true);
+
+		static inline int GetSegLength(void);
+
 		// TODO: Bounds checking.
-		static inline int GetWritePos(int line) { return ms_Extrapol[line][0]; }
-		static inline int GetWriteLen(int line) { return ms_Extrapol[line][1]; }
-		
+		static inline int GetWritePos(int line);
+		static inline int GetWriteLen(int line);
+
 		// Maximum sampling rate and segment size.
 		static const int MAX_SAMPLING_RATE = 48000;
 		static const int MAX_SEGMENT_SIZE = 960;	// ceil(MAX_SAMPLING_RATE / 50)
-		
+
 		// Segment buffer.
 		// Stores up to MAX_SEGMENT_SIZE 16-bit stereo samples.
 		// (Samples are actually 32-bit in order to handle oversaturation properly.)
@@ -68,14 +64,14 @@ class SoundMgr
 		// TODO: Convert to interleaved stereo.
 		static int32_t ms_SegBufL[MAX_SEGMENT_SIZE];
 		static int32_t ms_SegBufR[MAX_SEGMENT_SIZE];
-		
+
 		// Audio ICs.
 		// TODO: Add wrapper functions?
 		static Psg ms_Psg;
 		static Ym2612 ms_Ym2612;
-		
+
 		/**
-		 * ResetPtrsAndLens(): Reset buffer pointers and lengths.
+		 * Reset buffer pointers and lengths.
 		 */
 		static inline void ResetPtrsAndLens(void)
 		{
@@ -84,32 +80,50 @@ class SoundMgr
 			ms_Psg.resetBufferPtrs();
 			ms_Psg.clearWriteLen();
 		}
-		
+
 		/**
-		 * SpecialUpdate(): Run the specialUpdate() functions.
+		 * Run the specialUpdate() functions.
 		 */
 		static inline void SpecialUpdate(void)
 		{
 			ms_Psg.specialUpdate();
 			ms_Ym2612.specialUpdate();
 		}
-	
+
 	protected:
+		// TODO: Move these into the private class.
+
 		// Segment length.
-		static int CalcSegLength(int rate, bool isPal);
 		static int ms_SegLength;
-		
-		static int ms_Rate;
-		static bool ms_IsPal;
-		
+
 		// Line extrapolation values. [312 + extra room to prevent overflows]
 		// Index 0 == start; Index 1 == length
 		static unsigned int ms_Extrapol[312+8][2];
-	
+
 	private:
 		SoundMgr() { }
 		~SoundMgr() { }
 };
+
+/** Inline functions **/
+
+inline int SoundMgr::GetSegLength(void)
+{
+	return ms_SegLength;
+}
+
+// TODO: Bounds checking.
+inline int SoundMgr::GetWritePos(int line)
+{
+	assert(line >= 0 && line <= 311);
+	return ms_Extrapol[line][0];
+}
+
+inline int SoundMgr::GetWriteLen(int line)
+{
+	assert(line >= 0 && line <= 311);
+	return ms_Extrapol[line][1];
+}
 
 }
 
