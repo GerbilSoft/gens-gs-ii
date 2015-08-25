e3a683cc9709c925ad31e80a933bad0127eae333
David Korth <gerbilsoft@gerbilsoft.com>
2015-08-24 23:31:01 -0400

    [libgens/tests] ByteswapTest_benchmark.cpp: Added CPU flag parameters.
    Based on similar code in AudioWriteTest_benchmark.
    
    TODO: Add some flag to disable non-MMX asm optimizations,
    e.g. the 'bswap' optimization.

--- src/libgens/tests/ByteswapTest_benchmark.cpp
+++ src/libgens/tests/ByteswapTest_benchmark.cpp
@@ -24,6 +24,7 @@
 
 #include "lg_main.hpp"
 #include "Util/byteswap.h"
+#include "Util/cpuflags.h"
 
 // C includes. (C++ namespace)
 #include <cstdio>
@@ -34,18 +35,73 @@
 
 namespace LibGens { namespace Tests {
 
-class ByteswapTest_benchmark : public ::testing::Test
+struct ByteswapTest_flags {
+	uint32_t cpuFlags;
+	uint32_t cpuFlags_slow;
+
+	ByteswapTest_flags(uint32_t cpuFlags, uint32_t cpuFlags_slow)
+	{
+		this->cpuFlags = cpuFlags;
+		this->cpuFlags_slow = cpuFlags_slow;
+	}
+};
+
+class ByteswapTest_benchmark : public ::testing::TestWithParam<ByteswapTest_flags>
 {
 	protected:
 		ByteswapTest_benchmark()
-			: ::testing::Test() { }
+			: ::testing::TestWithParam<ByteswapTest_flags>() { }
 		virtual ~ByteswapTest_benchmark() { }
+
+		virtual void SetUp(void) override;
+		virtual void TearDown(void) override;
+
+	protected:
+		// Previous CPU flags.
+		uint32_t cpuFlags_old;
 };
+
+/**
+ * Set up the test.
+ */
+void ByteswapTest_benchmark::SetUp(void)
+{
+	// Verify CPU flags.
+	ByteswapTest_flags flags = GetParam();
+	uint32_t totalFlags = (flags.cpuFlags | flags.cpuFlags_slow);
+	if (flags.cpuFlags != 0) {
+		ASSERT_NE(0U, CPU_Flags & totalFlags) <<
+			"CPU does not support the required flags for this test.";
+	}
+
+	// Check if the CPU flag is slow.
+	if (CPU_Flags & flags.cpuFlags_slow) {
+		if ((CPU_Flags & totalFlags) == flags.cpuFlags_slow) {
+			// CPU is always slow.
+			printf("WARNING: CPU is known to be slow with this flag.\n"
+			       "Don't rely on the benchmark results.\n");
+		} else if ((CPU_Flags & totalFlags) == totalFlags) {
+			// CPU may be slow.
+			printf("WARNING: CPU may be slow with some instructions provided by this flag.\n");
+		}
+	}
+
+	cpuFlags_old = CPU_Flags;
+	CPU_Flags = flags.cpuFlags;
+}
+
+/**
+ * Tear down the test.
+ */
+void ByteswapTest_benchmark::TearDown(void)
+{
+	CPU_Flags = cpuFlags_old;
+}
 
 /**
  * Benchmark 16-bit array byteswapping.
  */
-TEST_F(ByteswapTest_benchmark, checkByteSwap16Array)
+TEST_P(ByteswapTest_benchmark, checkByteSwap16Array)
 {
 	uint8_t data[516];
 
@@ -59,7 +115,7 @@ TEST_F(ByteswapTest_benchmark, checkByteSwap16Array)
 /**
  * Benchmark 32-bit array byteswapping.
  */
-TEST_F(ByteswapTest_benchmark, checkByteSwap32Array)
+TEST_P(ByteswapTest_benchmark, checkByteSwap32Array)
 {
 	uint8_t data[516];
 
@@ -69,5 +125,17 @@ TEST_F(ByteswapTest_benchmark, checkByteSwap32Array)
 		__byte_swap_32_array(data, sizeof(data));
 	}
 }
+
+INSTANTIATE_TEST_CASE_P(ByteswapTest_benchmark_NoFlags, ByteswapTest_benchmark,
+	::testing::Values(ByteswapTest_flags(0, 0)
+));
+
+// NOTE: byteswap.c only implements MMX using GNU assembler.
+// TODO: Add some flag to disable non-MMX asm optimizations, e.g. 'bswap'.
+#if defined(__i386__) || defined(__amd64__)
+INSTANTIATE_TEST_CASE_P(ByteswapTest_benchmark_MMX, ByteswapTest_benchmark,
+	::testing::Values(ByteswapTest_flags(MDP_CPUFLAG_X86_MMX, 0)
+));
+#endif
 
 } }
