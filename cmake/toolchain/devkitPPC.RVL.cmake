4bdaf88049c52d930d27efd4b34d42ba0631c562
David Korth <gerbilsoft@gerbilsoft.com>
2015-08-28 01:21:57 -0400

    [cmake] devkitPPC.RVL.cmake: Updated with some settings from dolphin-emu.
    Notably, CMAKE_SYSTEM_NAME is now "Generic", and
    CMAKE_SYSTEM_PROCESSOR is now "powerpc-eabi".
    
    Reference: https://github.com/dolphin-emu/hwtests/blob/master/toolchain-powerpc.cmake

--- cmake/toolchain/devkitPPC.RVL.cmake
+++ cmake/toolchain/devkitPPC.RVL.cmake
@@ -1,28 +1,32 @@
-IF("$ENV{DEVKITPRO}" STREQUAL "")
+# Based on dolphin's toolchain-powerpc.cmake.
+# https://github.com/dolphin-emu/hwtests/blob/master/toolchain-powerpc.cmake
+SET(DEVKITPRO "$ENV{DEVKITPRO}")
+SET(DEVKITPPC "$ENV{DEVKITPPC}")
+
+IF(NOT DEVKITPRO)
 	MESSAGE(FATAL_ERROR "Please set DEVKITPRO in your environment.\nexport DEVKITPRO=/path/to/devkitpro")
 ENDIF()
+IF(NOT DEVKITPPC)
+	MESSAGE(FATAL_ERROR "Please set DEVKITPPC in your environment.\nexport DEVKITPPC=/path/to/devkitpro/devkitPPC")
+ENDIF()
 
-# TODO: Don't set it to Linux.
-SET(CMAKE_SYSTEM_NAME Linux)
+SET(CMAKE_SYSTEM_NAME "Generic")
+SET(CMAKE_SYSTEM_VERSION 0)
+SET(CMAKE_SYSTEM_PROCESSOR powerpc-eabi)
 
-# Bypass -rdynamic.
-SET(__LINUX_COMPILER_GNU 1)
-macro(__linux_compiler_gnu lang)
-endmacro()
-
-#SET(CMAKE_SYSTEM_NAME RVL)
-SET(CMAKE_SYSTEM_PROCESSOR powerpc)
-
-SET(CMAKE_SYSROOT $ENV{DEVKITPRO}/devkitPPC)
+SET(CMAKE_SYSROOT "${DEVKITPPC}")
 #SET(CMAKE_STAGING_PREFIX)
 
+# TODO: Append .exe on Windows build systems.
 SET(CMAKE_C_COMPILER "${CMAKE_SYSROOT}/bin/powerpc-eabi-gcc")
 SET(CMAKE_CXX_COMPILER "${CMAKE_SYSROOT}/bin/powerpc-eabi-g++")
 SET(CMAKE_AR "${CMAKE_SYSROOT}/bin/powerpc-eabi-ar" CACHE FILEPATH "ar")
 SET(CMAKE_NM "${CMAKE_SYSROOT}/bin/powerpc-eabi-nm" CACHE FILEPATH "nm")
 SET(CMAKE_RANLIB "${CMAKE_SYSROOT}/bin/powerpc-eabi-ranlib" CACHE FILEPATH "ranlib")
 
-SET(CMAKE_FIND_ROOT_PATH "/opt/devkitpro/devkitPPC/powerpc-eabi")
+# NOTE: System libraries are in the CPU-specific subdirectory.
+# ${DEVKITPPC}/lib only has gcc's libiberty and startup code.
+SET(CMAKE_FIND_ROOT_PATH "${DEVKITPPC}/${CMAKE_SYSTEM_PROCESSOR}")
 SET(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 SET(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
