a69be059109598f0aafbfb3b8330f5474a581077
David Korth <gerbilsoft@gerbilsoft.com>
2015-08-27 22:57:48 -0400

    [lzma] Ppmd7.c, Ppmd8.c: #include <string.h>, not <memory.h>, on non-Windows systems.
    On glibc, memory.h is simply a wrapper for string.h.
    On devkitPPC, memory.h doesn't exist; the functions are in string.h.

--- extlib/lzma/lzmabase/Ppmd7.c
+++ extlib/lzma/lzmabase/Ppmd7.c
@@ -2,7 +2,14 @@
 2010-03-12 : Igor Pavlov : Public domain
 This code is based on PPMd var.H (2001): Dmitry Shkarin : Public domain */
 
+/* Gens/GS II: memory.h has mem*() functions on MSVC and MinGW-w64. *
+ * On glibc, it's a wrapper for string.h.                           *
+ * It doesn't exist on some embedded systems, e.g. devkitPPC.       */
+#ifdef _WIN32
 #include <memory.h>
+#else
+#include <string.h>
+#endif
 
 #include "Ppmd7.h"
 
