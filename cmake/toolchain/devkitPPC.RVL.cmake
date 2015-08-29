641dccaea4164bfac9c22813619cdd3b66e0a368
David Korth <gerbilsoft@gerbilsoft.com>
2015-08-29 10:12:32 -0400

    [cmake] devkitPPC.RVL.cmake: Some more improvements.
    - Set ${LIBOGC_INCLUDE_DIR}, ${LIBOGC_PLATFORM},
      ${LIBOGC_LIBRARY_DIR}, and ${LIBOGC_LIBRARY}.
    
    - Show errors if certain directories and files couldn't be found.
    
    - Added definitions for GameCube, though they're disabled right now
      since I'm not targetting GameCube.

--- cmake/toolchain/devkitPPC.RVL.cmake
+++ cmake/toolchain/devkitPPC.RVL.cmake
@@ -6,9 +6,15 @@ SET(DEVKITPPC "$ENV{DEVKITPPC}")
 IF(NOT DEVKITPRO)
 	MESSAGE(FATAL_ERROR "Please set DEVKITPRO in your environment.\nexport DEVKITPRO=/path/to/devkitpro")
 ENDIF()
+IF(NOT EXISTS "${DEVKITPRO}/")
+	MESSAGE(FATAL_ERROR "DEVKITPRO is not set to a valid directory.\nDEVKITPRO == ${DEVKITPRO}")
+ENDIF(NOT EXISTS "${DEVKITPRO}/")
 IF(NOT DEVKITPPC)
 	MESSAGE(FATAL_ERROR "Please set DEVKITPPC in your environment.\nexport DEVKITPPC=/path/to/devkitpro/devkitPPC")
 ENDIF()
+IF(NOT EXISTS "${DEVKITPPC}/")
+	MESSAGE(FATAL_ERROR "DEVKITPPC is not set to a valid directory.\nDEVKITPPC == ${DEVKITPPC}")
+ENDIF(NOT EXISTS "${DEVKITPPC}/")
 
 SET(CMAKE_SYSTEM_NAME "Generic")
 SET(CMAKE_SYSTEM_VERSION 0)
@@ -33,18 +39,43 @@ SET(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
 SET(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
 
 # libOGC directories.
-INCLUDE_DIRECTORIES("${DEVKITPRO}/libogc/include")
-LINK_DIRECTORIES("${DEVKITPRO}/libogc/lib/wii")
+SET(LIBOGC_INCLUDE_DIR "${DEVKITPRO}/libogc/include")
+IF(NOT EXISTS "${LIBOGC_INCLUDE_DIR}/")
+	MESSAGE(FATAL_ERROR "libogc include directory not found.\nLIBOGC_INCLUDE_DIR == ${LIBOGC_INCLUDE_DIR}")
+ENDIF(NOT EXISTS "${LIBOGC_INCLUDE_DIR}/")
+INCLUDE_DIRECTORIES("${LIBOGC_INCLUDE_DIR}")
 
-# Wii-specific flags.
-# Based on devkitPPC's base_rules and wii_rules.
+# GameCube/Wii compiler flags.
+# Based on devkitPPC's base_rules, gamecube_rules, and wii_rules.
 ADD_DEFINITIONS(-DGEKKO)
+SET(DOL_C_FLAGS "-mogc -mcpu=750 -meabi -mhard-float")
 SET(RVL_C_FLAGS "-mrvl -mcpu=750 -meabi -mhard-float")
+
+# Enable Wii bulids.
+SET(HW_RVL 1)
 SET(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${RVL_C_FLAGS}")
 SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${RVL_C_FLAGS}")
+SET(LIBOGC_PLATFORM "wii")
+
+# Equivalent code for enabling GameCube builds,
+# but disabled because we're not targetting GameCube.
+IF(0)
+SET(HW_DOL 1)
+SET(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${DOL_C_FLAGS}")
+SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${DOL_C_FLAGS}")
+SET(LIBOGC_PLATFORM "cube")
+ENDIF(0)
 
 # Always link in libogc.
 # NOTE: LINK_DIRECTORIES() isn't working...
-SET(LIBOGC_LIBRARY "${DEVKITPRO}/libogc/lib/wii/libogc.a" CACHE FILEPATH "libogc")
+SET(LIBOGC 1)
+SET(LIBOGC_LIBRARY_DIR "${DEVKITPRO}/libogc/lib/${LIBOGC_PLATFORM}")
+SET(LIBOGC_LIBRARY "${LIBOGC_LIBRARY_DIR}/libogc.a")
+IF(NOT EXISTS "${LIBOGC_LIBRARY_DIR}")
+	MESSAGE(FATAL_ERROR "libogc.a not found.\nLIBOGC_LIBRARY == ${LIBOGC_LIBRARY}")
+ENDIF(NOT EXISTS "${LIBOGC_LIBRARY_DIR}")
+# CMake implicit libraries are set after the toolchain is processed.
+# Add libogc.a to LDFLAGS as a workaround.
 SET(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ${LIBOGC_LIBRARY}")
 SET(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} ${LIBOGC_LIBRARY}")
+LINK_DIRECTORIES("${LIBOGC_LIBRARY_DIR}")
