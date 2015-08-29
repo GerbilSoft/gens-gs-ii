GerbilSoft/gens-gs-ii

commit 094a3fb256d11f5e3e33ecc22a75574c78357125

[cmake] SplitDebugInformation.cmake: Use CMAKE_OBJCOPY and CMAKE_STRIP.
FIND_PROGRAM(OBJCOPY) finds the system objcopy, which fails when
cross-compiling for e.g. PowerPC. CMAKE_OBJCOPY is set to the
correct cross-compile version, so use that.

Use 'strip' instead of 'objcopy --strip-debug'. objcopy doesn't
remove the .symtab or .strtab sections, which can take up a good
amount of space. They're also copied to the .debug file, so there's
no reason to keep them in the main executable.

cmake/macros/SplitDebugInformation.cmake
@@ -12,28 +12,48 @@
 #   executable. (Wine ignores it and works fine!)
 #
 IF(NOT MSVC)
-	FIND_PROGRAM(OBJCOPY objcopy)
-	IF(NOT OBJCOPY)
-		MESSAGE(WARNING "objcopy was not found; debug information will not be split.")
-	ENDIF(NOT OBJCOPY)
+	# CMake automatically finds objcopy and strip as
+	# part of its toolchain initialization.
+	IF(NOT CMAKE_OBJCOPY)
+		MESSAGE(WARNING "'objcopy' was not found; debug information will not be split.")
+	ELSEIF(NOT CMAKE_STRIP)
+		MESSAGE(WARNING "'strip' was not found; debug information will not be split.")
+	ENDIF()
 ENDIF(NOT MSVC)
 
 MACRO(SPLIT_DEBUG_INFORMATION EXE_TARGET)
-IF(OBJCOPY AND NOT MSVC)
+SET(SPLIT_OK 1)
+IF(MSVC)
+	# MSVC splits debug information by itself.
+ELSEIF(NOT CMAKE_OBJCOPY)
+	# 'objcopy' is missing.
+	SET(SPLIT_OK 0)
+ELSEIF(NOT CMAKE_STRIP)
+	# 'strip' is missing.
+	SET(SPLIT_OK 0)
+ENDIF()
+
+IF(SPLIT_OK)
 	# NOTE: $<TARGET_FILE:gcbanner> is preferred,
 	# but this doesn't seem to work on Ubuntu 10.04.
 	# (cmake_2.8.0-5ubuntu1_i386)
 	GET_PROPERTY(SPLITDEBUG_EXE_LOCATION TARGET ${EXE_TARGET} PROPERTY LOCATION)
 
+	# NOTE: objcopy --strip-debug does NOT fully
+	# strip the binary; two sections are left:
+	# - .symtab: Symbol table.
+	# - .strtab: String table.
+	# These sections are split into the .debug file, so there's
+	# no reason to keep them in the executable.
 	ADD_CUSTOM_COMMAND(TARGET ${EXE_TARGET} POST_BUILD
-		COMMAND ${OBJCOPY} --only-keep-debug
+		COMMAND ${CMAKE_OBJCOPY} --only-keep-debug
 			${SPLITDEBUG_EXE_LOCATION} ${CMAKE_CURRENT_BINARY_DIR}/${EXE_TARGET}.debug
-		COMMAND ${OBJCOPY} --strip-debug
+		COMMAND ${CMAKE_STRIP}
 			${SPLITDEBUG_EXE_LOCATION}
-		COMMAND ${OBJCOPY} --add-gnu-debuglink=${EXE_TARGET}.debug
+		COMMAND ${CMAKE_OBJCOPY} --add-gnu-debuglink=${EXE_TARGET}.debug
 			${SPLITDEBUG_EXE_LOCATION}
 		)
 
 	UNSET(SPLITDEBUG_EXE_LOCATION)
-ENDIF(OBJCOPY AND NOT MSVC)
+ENDIF(SPLIT_OK)
 ENDMACRO(SPLIT_DEBUG_INFORMATION)

commit 7f1123c9ff86fef368247a09166c4b23e720fa05

[cmake] SplitDebugInformation.cmake: Set SPLIT_OK=0 if building with MSVC.
Otherwise, we attempt to use 'objcopy', which fails miserably.
(Actually, it might work if cygwin is installed, but it shouldn't
be used anyway, since MSVC stores debug symbols in .pdb files.)

This is a regression from commit 094a3fb256d11f5e3e33ecc22a75574c78357125.
([cmake] SplitDebugInformation.cmake: Use CMAKE_OBJCOPY and CMAKE_STRIP.)

cmake/macros/SplitDebugInformation.cmake
@@ -25,6 +25,7 @@ MACRO(SPLIT_DEBUG_INFORMATION EXE_TARGET)
 SET(SPLIT_OK 1)
 IF(MSVC)
 	# MSVC splits debug information by itself.
+	SET(SPLIT_OK 0)
 ELSEIF(NOT CMAKE_OBJCOPY)
 	# 'objcopy' is missing.
 	SET(SPLIT_OK 0)