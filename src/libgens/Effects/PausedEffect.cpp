3733ca262a64f6212a562b6e95ca0d84c123a07f
David Korth <gerbilsoft@gerbilsoft.com>
2015-08-30 12:38:07 -0400

    [libgens] PausedEffect: Initial 2-FB 32-bit MMX paused effect.
    Currently only does grayscale, and 1px per iteration, but the results
    are quite good:
    
                     | 32-bit C | 32-bit MMX |
    +----------------+----------+------------+
    | gcc-5.2.0, -Og | 3,032 ms |     509 ms |
    | gcc-5.2.0, -O2 | 1,900 ms |     503 ms |
    +----------------+----------+------------+
    
    Note that some of this could be attributed to the C version using floating-point,
    whereas the MMX version uses integers. Using integers results in slightly less
    precision, but that doesn't really matter for a simple effect.

--- src/libgens/Effects/PausedEffect.cpp
+++ src/libgens/Effects/PausedEffect.cpp
@@ -27,10 +27,13 @@
 #include "../Util/MdFb.hpp"
 
 // C includes.
-#include <math.h>
 #include <stdlib.h>
 #include <stdint.h>
-#include <string.h>
+
+// C includes. (C++ namespace)
+#include <cmath>
+#include <cstring>
+#include <cassert>
 
 // Color component masks and shift values for 32-bit color.
 #define PAUSED_MASK32_R  0x00FF0000
@@ -39,6 +42,13 @@
 #define PAUSED_SHIFT32_R (16+3)
 #define PAUSED_SHIFT32_G (8+3)
 #define PAUSED_SHIFT32_B (0+3)
+
+// CPU flags.
+#include "libcompat/cpuflags.h"
+
+#if defined(__GNUC__) && (defined(__i386__) || defined(__amd64__))
+#define HAVE_MMX 1
+#endif
 
 namespace LibGens {
 
@@ -65,12 +75,17 @@ class PausedEffectPrivate
 			pixel* RESTRICT outScreen,
 			const pixel* RESTRICT mdScreen,
 			unsigned int pxCount);
+
+#ifdef HAVE_MMX
+		static inline void DoPausedEffect_32_MMX(uint32_t *outScreen,
+				const uint32_t *mdScreen, unsigned int pxCount);
+#endif
 };
 
 #define MMASK(bits) ((1 << (bits)) - 1)
 
 /**
- * Tint the screen a purple hue to indicate that emulation is paused.
+ * Tint the screen a purple hue to indicate that emulation is paused. [1-FB]
  * @param pixel Type of pixel.
  * @param RBits Number of bits for Red.
  * @param GBits Number of bits for Green.
@@ -119,7 +134,7 @@ inline void PausedEffectPrivate::T_DoPausedEffect(
 }
 
 /**
- * Tint the screen a purple hue to indicate that emulation is paused.
+ * Tint the screen a purple hue to indicate that emulation is paused. [2-FB]
  * @param pixel Type of pixel.
  * @param RBits Number of bits for Red.
  * @param GBits Number of bits for Green.
@@ -169,6 +184,71 @@ inline void PausedEffectPrivate::T_DoPausedEffect(
 			((nB  >> (8 - BBits)));
 	}
 }
+
+#ifdef HAVE_MMX
+
+/**
+ * Tint the screen a purple hue to indicate that emulation is paused. [2-FB]
+ * (32-bit color, MMX-optimized.)
+ * @param outScreen Pointer to the source/destination screen buffer.
+ * @param mdScreen Pointer to the MD screen buffer.
+ * @param pxCount Pixel count.
+ */
+inline void PausedEffectPrivate::DoPausedEffect_32_MMX(
+	uint32_t *outScreen,
+	const uint32_t *mdScreen,
+	unsigned int pxCount)
+{
+	// Grayscale vector: [0.299 0.587 0.114] (ITU-R BT.601)
+	// Source: http://en.wikipedia.org/wiki/YCbCr
+
+	// Load the 32-bit grayscale vector.
+	// Reference: http://www.asmcommunity.net/forums/topic/?id=19704
+	static const uint64_t GRAY_32_MMX = {0x0000004D0096001D};
+	__asm__ (
+		"movq	%[GRAY_32_MMX], %%mm7\n"
+		"pxor	%%mm0, %%mm0\n"
+		:
+		: [GRAY_32_MMX] "m" (GRAY_32_MMX)
+	);
+
+	// Convert the pixels to grayscale.
+	// TODO: Apply the Blue tint.
+	// TODO: Do more than 1px at a time?
+	assert(pxCount % 2 == 0);
+	for (pxCount /= 1; pxCount > 0; pxCount--) {
+		__asm__ (
+			"movd		(%[mdScreen]), %%mm1\n"		// Get 1 pixel.
+			"punpcklbw	%%mm0, %%mm1\n"			// Unpack %mm1 into words using %mm0 as high bytes.
+			"pmaddwd	%%mm7, %%mm1\n"			// %mm1 == 0 + R * MULT | G * MULT + B * MULT
+			"movd		%%mm1, %%edx\n"			// %edx == G * MULT + B * MULT
+			"psrlq		$32, %%mm1\n"			// %mm1 == 0 + R * MULT
+			"movd		%%mm1, %%eax\n"			// %eax == 0 + R * MULT
+			"add		%%edx, %%eax\n"
+			// TODO: Optimize this.
+			"movb		%%ah, 0(%[outScreen])\n"
+			"movb		%%ah, 1(%[outScreen])\n"
+			"movb		%%ah, 2(%[outScreen])\n"
+			"movb		$0, 3(%[outScreen])\n"
+			:
+			: [mdScreen] "r" (mdScreen)
+			, [outScreen] "r" (outScreen)
+			// FIXME: gcc complains mm? registers are unknown.
+			// May need to compile with -mmmx...
+			//: "mm0", "mm1"
+			: "eax", "edx"
+		);
+
+		// Next group of pixels.
+		outScreen += 1;
+		mdScreen += 1;
+	}
+
+	// Reset the FPU state.
+	__asm__ __volatile__ ("emms");
+}
+
+#endif /* HAVE_MMX */
 
 /**
  * Tint the screen a purple hue to indicate that emulation is paused.
@@ -231,8 +311,16 @@ void PausedEffect::DoPausedEffect(MdFb* RESTRICT outScreen, const MdFb* RESTRICT
 			break;
 		case MdFb::BPP_32:
 		default:
-			PausedEffectPrivate::T_DoPausedEffect<uint32_t, 8, 8, 8>
-				(outScreen->fb32(), mdScreen->fb32(), pxCount);
+#ifdef HAVE_MMX
+			if (CPU_Flags & MDP_CPUFLAG_X86_MMX) {
+				PausedEffectPrivate::DoPausedEffect_32_MMX(
+					outScreen->fb32(), mdScreen->fb32(), pxCount);
+			} else
+#endif /* HAVE_MMX */
+			{
+				PausedEffectPrivate::T_DoPausedEffect<uint32_t, 8, 8, 8>
+					(outScreen->fb32(), mdScreen->fb32(), pxCount);
+			}
 			break;
 	}
 
