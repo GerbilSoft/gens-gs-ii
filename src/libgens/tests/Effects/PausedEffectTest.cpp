953f484c47ee73ec1eac01310086f6122456f96e
David Korth <gerbilsoft@gerbilsoft.com>
2015-08-30 10:53:21 -0400

    [libgens/tests] PausedEffectTest: Added helper functions.
    copyToFb32(): Copy a Zomg_Img_Data_t to a 32-bit MdFb.
    
    compareFb(): Compare two MdFbs with the same dimensions and color depth.
    
    Other changes:
    - SetUp(): Copy the reference images into MdFbs for testing.
    - Removed disabled SoundMgr code. This is a leftover from AudioWriteTest,
      which is what this test was initially based on.

--- src/libgens/tests/Effects/PausedEffectTest.cpp
+++ src/libgens/tests/Effects/PausedEffectTest.cpp
@@ -41,6 +41,9 @@ using LibZomg::PngReader;
 #include <cstdlib>
 #include <cstring>
 
+// C++ includes.
+#include <algorithm>
+
 // TODO: More sample data:
 // 48,000 Hz @ 50 Hz (960 samples)
 // 44,100 Hz @ 60 Hz (735 samples)
@@ -55,17 +58,41 @@ class PausedEffectTest : public ::testing::Test
 	protected:
 		PausedEffectTest()
 			: ::testing::Test()
-			, fb(nullptr) { }
+			, fb_normal(nullptr)
+			, fb_paused(nullptr)
+			, fb_test(nullptr) { }
 		virtual ~PausedEffectTest() { }
 
 		virtual void SetUp(void) override;
 		virtual void TearDown(void) override;
 
+		/**
+		 * Copy a loaded image to an MdFb in 32-bit color.
+		 * Source image must be 32-bit color.
+		 * @param fb	[out] Destination MdFb.
+		 * @param src	[in] Source img_data.
+		 */
+		void copyToFb32(MdFb *fb, const Zomg_Img_Data_t *src);
+
+		/**
+		 * Compare two framebuffers.
+		 * Both framebuffers must have the same color depth.
+		 * @param fb_expected	[in] Expected image.
+		 * @param fb_actual	[in] Actual image.
+		 */
+		void compareFb(const MdFb *fb_expected, const MdFb *fb_actual);
+
 	protected:
 		Zomg_Img_Data_t img_normal;
 		Zomg_Img_Data_t img_paused;
 
-		MdFb *fb;
+		// Reference framebuffers.
+		MdFb *fb_normal;
+		MdFb *fb_paused;
+
+		// Test framebuffer.
+		// Paused Effect is applied here.
+		MdFb *fb_test;
 };
 
 /**
@@ -89,16 +116,26 @@ void PausedEffectTest::SetUp(void)
 			PngReader::RF_INVERTED_ALPHA);
 	ASSERT_EQ(0, ret) << "Error loading \"PausedEffect.SW.32.png\": " << strerror(-ret);
 
-	// Allocate an MdFb.
-	fb = new MdFb();
+	// Allocate the framebuffers.
+	fb_normal = new MdFb();
+	fb_paused = new MdFb();
+	fb_test = new MdFb();
+
+	// Set the framebuffers' color depth.
 	// TODO: Parameter.
-	fb->setBpp(MdFb::BPP_32);
+	fb_normal->setBpp(MdFb::BPP_32);
+	fb_normal->setBpp(MdFb::BPP_32);
+	fb_normal->setBpp(MdFb::BPP_32);
 
 	// Make sure the images have the correct dimensions.
-	EXPECT_EQ(fb->pxPerLine(), (int)img_normal.w);
-	EXPECT_EQ(fb->numLines(),  (int)img_normal.h);
-	EXPECT_EQ(fb->pxPerLine(), (int)img_paused.w);
-	EXPECT_EQ(fb->numLines(),  (int)img_paused.h);
+	EXPECT_EQ(fb_normal->pxPerLine(), (int)img_normal.w);
+	EXPECT_EQ(fb_normal->numLines(),  (int)img_normal.h);
+	EXPECT_EQ(fb_normal->pxPerLine(), (int)img_paused.w);
+	EXPECT_EQ(fb_normal->numLines(),  (int)img_paused.h);
+
+	// Copy the reference images into the framebuffers.
+	copyToFb32(fb_normal, &img_normal);
+	copyToFb32(fb_paused, &img_paused);
 }
 
 /**
@@ -110,8 +147,65 @@ void PausedEffectTest::TearDown(void)
 	free(img_normal.data);
 	free(img_paused.data);
 
-	// Unreference the MdFb.
-	fb->unref();
+	// Unreference the framebuffers.
+	fb_normal->unref();
+	fb_paused->unref();
+	fb_test->unref();
+}
+
+/**
+ * Copy a loaded image to an MdFb in 32-bit color.
+ * Source image must be 32-bit color.
+ * @param fb	[out] Destination MdFb.
+ * @param src	[in] Source img_data.
+ */
+void PausedEffectTest::copyToFb32(MdFb *fb, const Zomg_Img_Data_t *src)
+{
+	ASSERT_EQ(32, src->bpp);
+
+	// Set the MdFb to 32-bit color.
+	fb->setBpp(MdFb::BPP_32);
+
+	// Copy the image to the MdFb.
+	const uint32_t *pData = (const uint32_t*)src->data;
+	for (int line = 0; line < fb->numLines(); line++) {
+		memcpy(fb->lineBuf32(line), pData, src->pitch);
+		pData += (src->pitch / 4);
+	}
+}
+
+/**
+ * Compare two framebuffers.
+ * Both framebuffers must have the same color depth.
+ * @param fb_expected	[in] Expected image.
+ * @param fb_actual	[in] Actual image.
+ */
+void PausedEffectTest::compareFb(const MdFb *fb_expected, const MdFb *fb_actual)
+{
+	// NOTE: The full pitch is checked, not just the
+	// actual image data. (336 of 336)
+	ASSERT_EQ(fb_expected->bpp(),       fb_actual->bpp());
+	ASSERT_EQ(fb_expected->pxPerLine(), fb_actual->pxPerLine());
+	ASSERT_EQ(fb_expected->numLines(),  fb_actual->numLines());
+
+	// Use the minimum pitch between the two framebuffers.
+	int pitch = std::min(fb_expected->pxPitch(), fb_actual->pxPitch());
+
+	if (fb_expected->bpp() == MdFb::BPP_32) {
+		pitch *= 4;
+		for (int line = 0; line < fb_expected->numLines(); line++) {
+			EXPECT_EQ(0, memcmp(fb_expected->lineBuf32(line),
+					    fb_actual->lineBuf32(line), pitch)) <<
+				"Line " << line << " did not match the reference image.";
+		}
+	} else {
+		pitch *= 2;
+		for (int line = 0; line < fb_expected->numLines(); line++) {
+			EXPECT_EQ(0, memcmp(fb_expected->lineBuf16(line),
+					    fb_actual->lineBuf16(line), pitch)) <<
+				"Line " << line << " did not match the reference image.";
+		}
+	}
 }
 
 /**
@@ -119,43 +213,15 @@ void PausedEffectTest::TearDown(void)
  */
 TEST_F(PausedEffectTest, do32bit)
 {
-	// Copy the "normal" image into an MdFb.
-	// TODO: Convert to 15/16-bit color if necessary.
-	const uint32_t *src = (const uint32_t*)img_normal.data;
-	for (int line = 0; line < fb->numLines(); line++) {
-		memcpy(fb->lineBuf32(line), src, img_normal.pitch);
-		src += (img_normal.pitch / 4);
-	}
+	// Initialize the test framebuffer with the "normal" image.
+	copyToFb32(fb_test, &img_normal);
 
 	// Apply the "paused" effect. (1-FB version)
 	// TODO: Test 2-FB version as well.
-	PausedEffect::DoPausedEffect(fb);
+	PausedEffect::DoPausedEffect(fb_test);
 
 	// Compare it to the known good "paused" image.
-	// NOTE: Only the actual image is checked;
-	// the 8px borders are ignored. (320 of 336)
-	// TODO: Do we need to clear the high bytes in img_paused?
-	src = (const uint32_t*)img_paused.data;
-	for (int line = 0; line < fb->numLines(); line++) {
-		EXPECT_EQ(0, memcmp(fb->lineBuf32(line), src, img_paused.pitch)) <<
-			"Line " << line << " did not match the reference image.";
-		src += (img_normal.pitch / 4);
-	}
-	
-#if 0	
-	int ret = SoundMgr::writeStereo(buf, samples);
-	ASSERT_EQ(samples, ret);
-
-	// Verify the data.
-	const int16_t *expected = PausedEffectTest_Output_Stereo;
-	for (int i = 0; i < samples*2; i++) {
-		EXPECT_EQ(expected[i], buf[i]) <<
-			"Output sample " << i << " should be " <<
-			std::hex << std::uppercase <<
-			std::setfill('0') << std::setw(4) <<
-			expected[i] << ", but was " << buf[i];
-	}
-#endif
+	compareFb(fb_paused, fb_test);
 }
 
 } }
