22a096eb4a8f2dd11aa6aec43a0fdf580d9471eb
David Korth <gerbilsoft@gerbilsoft.com>
2016-01-07 21:38:07 -0500

    [libcompat/W32U] minizip_iowin32u.c: Use W32U_IsUnicode().
    GetModuleHandleW() returns a valid handle on Win9x if KernelEx
    is installed.
    
    Removed INVALID_HANDLE_VALUE and INVALID_SET_FILE_POINTER,
    since these are defined in the Windows headers.
    
    Don't bother retrieving the iowin32 ANSI function pointer if
    we already have it. This eliminates the need to fill the
    function pointer table twice every time. (It's now only
    filled twice the first time minizip_iowin32u.c is used.)

--- src/libcompat/W32U/minizip_iowin32u.c
+++ src/libcompat/W32U/minizip_iowin32u.c
@@ -1,22 +1,20 @@
 /**
- * iowin32u.c: UTF-8 wrapper for iowin32.c.
+ * minizip_iowin32u.c: UTF-8 wrapper for iowin32.c.
  */
-
-#include <stdlib.h>
-
-#include "zlib.h"
-#include "ioapi.h"
-#include "iowin32.h"
 #include "minizip_iowin32u.h"
 
-#ifndef INVALID_HANDLE_VALUE
-#define INVALID_HANDLE_VALUE (0xFFFFFFFF)
-#endif
+// C includes.
+#include <stdlib.h>
 
-#ifndef INVALID_SET_FILE_POINTER
-#define INVALID_SET_FILE_POINTER ((DWORD)-1)
-#endif
+#include <zlib.h>
+#include "minizip/ioapi.h"
+#include "minizip/iowin32.h"
 
+// Win32 Unicode Translation Layer.
+#include <windows.h>
+#include "W32U_mini.h"
+
+// open64 function pointers from iowin32.
 static open64_file_func pWin32_open64_file_funcA;
 static open64_file_func pWin32_open64_file_funcW;
 
@@ -74,22 +72,26 @@ voidpf ZCALLBACK win32_open64_file_funcUW(voidpf opaque, const void* filename, i
 
 void fill_win32_filefunc64U(zlib_filefunc64_def* pzlib_filefunc_def)
 {
-	// Get the iowin32.c function pointers
 	// TODO: Verify that the system supports UTF-8.
 	// Win9x: Windows 98 and later supports UTF-8. (NOT WINDOWS 95!)
 	// WinNT: Windows NT 4.0 and later supports UTF-8.
 	// Not sure about NT 3.x.
 
-	// ANSI version.
-	fill_win32_filefunc64A(pzlib_filefunc_def);
-	pWin32_open64_file_funcA = pzlib_filefunc_def->zopen64_file;
+	// NOTE: The ANSI and Unicode iowin32 function pointer tables
+	// are identical except for the open64 function.
 
-	// Unicode version.
+	if (!pWin32_open64_file_funcA) {
+		// Get the iowin32 ANSI function pointer.
+		fill_win32_filefunc64A(pzlib_filefunc_def);
+		pWin32_open64_file_funcA = pzlib_filefunc_def->zopen64_file;
+	}
+
+	// Get the iowin32 Unicode function pointer.
 	fill_win32_filefunc64W(pzlib_filefunc_def);
 	pWin32_open64_file_funcW = pzlib_filefunc_def->zopen64_file;
 
 	// Check if this system supports Unicode.
-	if (GetModuleHandleW(NULL) != NULL) {
+	if (W32U_IsUnicode()) {
 		// System supports Unicode.
 		pzlib_filefunc_def->zopen64_file = win32_open64_file_funcUW;
 	} else {
--- src/libcompat/W32U/minizip_iowin32u.h
+++ src/libcompat/W32U/minizip_iowin32u.h
@@ -1,23 +1,26 @@
 /**
- * iowin32u.h: UTF-8 wrapper for iowin32.h.
+ * minizip_iowin32u.h: UTF-8 wrapper for iowin32.h.
  */
 
-#ifndef __MINIZIP_IOWIN32U_H__
-#define __MINIZIP_IOWIN32U_H__
+#ifndef __LIBCOMPAT_W32U_MINIZIP_IOWIN32U_H__
+#define __LIBCOMPAT_W32U_MINIZIP_IOWIN32U_H__
 
-#include "ioapi.h"
-
-/* _MZ_OF() */
-#include "mz_of.h"
+#include "minizip/ioapi.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-void fill_win32_filefunc64U _MZ_OF((zlib_filefunc64_def* pzlib_filefunc_def));
+/**
+ * Create a MiniZip 64-bit file function definition
+ * using the UTF-8 conversion functions.
+ *
+ * @param pzlib_filefunc_def [out]
+ */
+void fill_win32_filefunc64U(zlib_filefunc64_def* pzlib_filefunc_def);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif /* __MINIZIP_IOWIN32U_H__ */
+#endif /* __LIBCOMPAT_W32U_MINIZIP_IOWIN32U_H__ */
