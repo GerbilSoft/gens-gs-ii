def5738875a709bb77e132555cbf258474ac5da0
David Korth <gerbilsoft@gerbilsoft.com>
2016-01-07 23:04:47 -0500

    [libgens] File/: readFile()'s ret_siz parameter is file_offset_t*, not size_t*. (.cpp files)
    Like commit b18ae389d8f8b73b9cd8fe82cf31fc82dee89a2d, but for .cpp files
    instead of .hpp files.

--- src/libgens/File/MemFake.cpp
+++ src/libgens/File/MemFake.cpp
@@ -129,7 +129,7 @@ int MemFake::getFileInfo(mdp_z_entry_t **z_entry_out)
  * @param read_len	[in]  Number of bytes to read.
  * @param buf		[out] Buffer to read the file into.
  * @param siz		[in]  Size of buf. (Must be >= read_len.)
- * @param ret_siz	[out] Pointer to size_t to store the number of bytes read.
+ * @param ret_siz	[out] Pointer to file_offset_t to store the number of bytes read.
  * @return 0 on success; negative POSIX error code on error.
  */
 int MemFake::readFile(const mdp_z_entry_t *z_entry,
