42e14e93150c90542323f004b2cd528c20086013
David Korth <gerbilsoft@gerbilsoft.com>
2016-01-09 01:00:23 -0500

    [libgensfile] Sz.cpp: Fixed a Find/Replace error in a comment.

--- src/libgensfile/Sz.cpp
+++ src/libgensfile/Sz.cpp
@@ -45,8 +45,9 @@ using std::u16string;
 
 // Character set conversion.
 #include "libgenstext/Encoding.hpp"
+
 #ifdef _WIN32
-// Win32 Unicode Translation Laye#ifdef _WIN32
+// Win32 Unicode Translation Layer.
 // Needed for proper Unicode filename support on Windows.
 // Also required for large file support.
 #include "libcompat/W32U/W32U_mini.h"
