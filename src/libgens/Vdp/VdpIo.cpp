GerbilSoft/gens-gs-ii

commit a214b411413a4bd884fbfd9e0b2b8d4d24565afd

VdpReg.hpp: Initial codeword refactoring.
VDP_CodeWord contains enum values that map to CD5..0.

Vdp::VDP_Ctrl:
- Removed 'access' and 'data':
  - 'access' should be implied by 'code'.
  - 'data' is unnecessary, since all the data is in 'code' and 'address'.
- Added a reset() function for convenience.

Vdp::Read_Data(), Vdp::Write_Data():
- Use the new VDP_CodeWord enum values.
- Increment the addresses even if the destination is invalid.

Vdp::Write_Ctrl():
- Only allow CD5 to be updated if DMA Enabled == 1.

Miscellaneous:
- Clear CD5 after DMA operations are completed.

TODO: Remove CD_Table.

Nemesis' VDP FIFO Test ROM currently shows 23 / 99 / 122.
(23 passed, 99 failed, 122 total.) This is the same as before
this initial refactoring.

src/libgens/Vdp/VdpIo.cpp
@@ -574,8 +574,8 @@ uint16_t Vdp::Read_Data(void)
 	// TODO: Test this function.
 	// Soleil (crusader of Centry) reads from VRam.
 	LOG_MSG(vdp_io, LOG_MSG_LEVEL_DEBUG2,
-		"VDP_Ctrl.code == %02X, VDP_Ctrl.access == %04X",
-		VDP_Ctrl.code, VDP_Ctrl.access);
+		"VDP_Ctrl.code == %02X, VDP_Ctrl.address == %04X",
+		VDP_Ctrl.code, VDP_Ctrl.address);
 
 	// Reading the data port clears the control word latch.
 	VDP_Ctrl.ctrl_latch = 0;
@@ -588,19 +588,20 @@ uint16_t Vdp::Read_Data(void)
 	// TODO: Report this as a bug to the gcc developers.
 	volatile uint16_t data;
 
-	// Check the access mode.
-	switch (VDP_Ctrl.access) {
-		case (VDEST_LOC_VRAM | VDEST_ACC_READ):
+	// Check the destination.
+	// TODO: Internal "vdp memory read" function.
+	switch (VDP_Ctrl.code & VdpTypes::CD_DEST_MODE_MASK) {
+		case VdpTypes::CD_DEST_VRAM_READ:
 			// VRam Read.
 			data = VRam.u16[(VDP_Ctrl.address & 0xFFFF) >> 1];
 			break;
 
-		case (VDEST_LOC_CRAM | VDEST_ACC_READ):
+		case VdpTypes::CD_DEST_CRAM_READ:
 			// CRam Read.
 			data = m_palette.readCRam_16(VDP_Ctrl.address & 0x7E);
 			break;
 
-		case (VDEST_LOC_VSRAM | VDEST_ACC_READ):
+		case VdpTypes::CD_DEST_VSRAM_READ:
 			// VSRam Read.
 			// TODO: Mask off high bits? (Only 10/11 bits are present.)
 			// TODO: If we do that, the remaining bits should be what's in the FIFO.
@@ -609,7 +610,9 @@ uint16_t Vdp::Read_Data(void)
 
 		default:
 			// Invalid read specification.
-			return 0;
+			// NOTE: The address register should be incremented anyway.
+			data = 0;
+			break;
 	}
 
 	VDP_Ctrl.address += VDP_Reg.m5.Auto_Inc;
@@ -745,6 +748,7 @@ void Vdp::DMA_Fill(uint16_t data)
 	// Perhaps this should be combined with DMA_LOOP.
 	set_DMA_Length(0);	// Clear the DMA length.
 	VDP_Ctrl.DMA = 0;	// Clear the DMA mode.
+	VDP_Ctrl.code &= ~VdpTypes::CD_DMA_ENABLE;	// Clear CD5.
 
 	// Set DMA type and length.
 	DMAT_Type = 0x02;	// DMA Fill.
@@ -799,16 +803,19 @@ void Vdp::Write_Data_Word(uint16_t data)
 	// Writing to the data port clears the control word latch.
 	VDP_Ctrl.ctrl_latch = 0;
 
+	// Check for DMA FILL.
 	if (VDP_Ctrl.DMA & 0x04) {
 		// DMA Fill operation is in progress.
+		// FIXME: DMA FILL should NOT block the underlying VDP write operation.
 		DMA_Fill(data);
 		return;
 	}
 
-	// Check the access mode.
+	// Check the destination.
+	// TODO: Internal "vdp memory write" function.
 	uint16_t address = VDP_Ctrl.address;
-	switch (VDP_Ctrl.access) {
-		case (VDEST_LOC_VRAM | VDEST_ACC_WRITE):
+	switch (VDP_Ctrl.code & VdpTypes::CD_DEST_MODE_MASK) {
+		case VdpTypes::CD_DEST_VRAM_WRITE:
 			// VRam Write.
 			MarkVRamDirty();
 			address &= 0xFFFF;	// VRam is 64 KB. (32 Kwords)
@@ -823,12 +830,9 @@ void Vdp::Write_Data_Word(uint16_t data)
 
 			// Write the word to VRam.
 			VRam.u16[address>>1] = data;
-
-			// Increment the address register.
-			VDP_Ctrl.address += VDP_Reg.m5.Auto_Inc;
 			break;
 
-		case (VDEST_LOC_CRAM | VDEST_ACC_WRITE):
+		case VdpTypes::CD_DEST_CRAM_WRITE:
 			// CRam Write.
 			// TODO: According to the Genesis Software Manual, writing at
 			// odd addresses results in "interesting side effects".
@@ -838,12 +842,9 @@ void Vdp::Write_Data_Word(uint16_t data)
 			// Write the word to CRam.
 			// CRam is 128 bytes. (64 words)
 			m_palette.writeCRam_16((address & 0x7E), data);
-
-			// Increment the address register.
-			VDP_Ctrl.address += VDP_Reg.m5.Auto_Inc;
 			break;
 
-		case (VDEST_LOC_VSRAM | VDEST_ACC_WRITE):
+		case VdpTypes::CD_DEST_VSRAM_WRITE:
 			// VSRam Write.
 			// TODO: The Genesis Software Manual doesn't mention what happens
 			// with regards to odd address writes for VSRam.
@@ -853,15 +854,15 @@ void Vdp::Write_Data_Word(uint16_t data)
 			// TODO: VSRam is 80 bytes, but we're allowing a maximum of 128 bytes here...
 			// TODO: Mask off high bits? (Only 10/11 bits are present.)
 			VSRam.u16[(address & 0x7E) >> 1] = data;
-
-			// Increment the address register.
-			VDP_Ctrl.address += VDP_Reg.m5.Auto_Inc;
 			break;
 
 		default:
 			// Invalid write specification.
 			break;
 	}
+
+	// Increment the address register.
+	VDP_Ctrl.address += VDP_Reg.m5.Auto_Inc;
 }
 
 
@@ -1108,7 +1109,6 @@ void Vdp::Write_Ctrl(uint16_t data)
 			 * D = data
 			 */
 			VDP_Ctrl.code = 0;		// Reset the access code register.
-			VDP_Ctrl.access = CD_Table[VDP_Ctrl.code];
 			VDP_Ctrl.address = 0x0000;	// Reset the address counter.
 
 			const int reg = (data >> 8) & 0x1F;
@@ -1130,7 +1130,6 @@ void Vdp::Write_Ctrl(uint16_t data)
 		 * first control word is processed. They are replaced
 		 * when the second control word is processed.
 		 */
-		VDP_Ctrl.data[0] = data;
 		VDP_Ctrl.ctrl_latch = 1;	// Latch the first control word.
 
 		// Update the VDP address counter.
@@ -1140,7 +1139,6 @@ void Vdp::Write_Ctrl(uint16_t data)
 		// Update the VDP access code register.
 		VDP_Ctrl.code &= ~0x03;
 		VDP_Ctrl.code |= ((data >> 14) & 0x03);
-		VDP_Ctrl.access = (CD_Table[VDP_Ctrl.code] & 0xFF);
 		return;
 	}
 
@@ -1157,36 +1155,33 @@ void Vdp::Write_Ctrl(uint16_t data)
 	 * NOTE: CD1-CD0 and A13-A01 are left intact after the
 	 * first control word is processed. They are replaced
 	 * when the second control word is processed.
+	 *
+	 * NOTE 2: CD5 is only updated if DMA Enabled == 1.
+	 * (VDP_Reg.m5.Set2 & 0x04)
 	 */
-	VDP_Ctrl.data[1] = data;
 	VDP_Ctrl.ctrl_latch = 0;	// Clear the control word latch.
 
 	// Update the VDP address counter.
 	VDP_Ctrl.address &= ~0xC000;
 	VDP_Ctrl.address |= ((data & 0x0003) << 14);
 
-	// Update the VDP access code register.
-	VDP_Ctrl.code &= ~0x3C;
-	VDP_Ctrl.code |= ((data >> 2) & 0x3C);
-	uint16_t CD = CD_Table[VDP_Ctrl.code];
-	VDP_Ctrl.access = (CD & 0xFF);
-
-	// High byte of CD is the DMA access mode.
-	if (!(CD & 0xFF00)) {
-		// No DMA is needed.
-		return;
+	// Update the VDP access code register: CD(4..2)
+	VDP_Ctrl.code &= ~0x1C;
+	VDP_Ctrl.code |= ((data >> 2) & 0x1C);
+	if (VDP_Reg.m5.Set2 & 0x04) {
+		// DMA is enabled. Update CD5.
+		VDP_Ctrl.code &= ~VdpTypes::CD_DMA_ENABLE;
+		VDP_Ctrl.code |= ((data >> 2) & VdpTypes::CD_DMA_ENABLE);
 	}
 
-	/** Perform a DMA operation. **/
-
-	// Check if DMA is enabled.
-	if (!(VDP_Reg.m5.Set2 & 0x10)) {
-		// DMA is disabled.
-		VDP_Ctrl.DMA = 0;
+	// If CD5 is not set, DMA is disabled,
+	// so we're done here.
+	if (!(VDP_Ctrl.code & VdpTypes::CD_DMA_ENABLE))
 		return;
-	}
 
 	// Check for DMA FILL.
+	// TODO: Remove CD_Table.
+	uint16_t CD = CD_Table[VDP_Ctrl.code];
 	if ((CD & VDEST_DMA_FILL) && (VDP_Ctrl.DMA_Mode == 0x80)) {
 		// DMA FILL.
 		VDP_Ctrl.DMA = ((CD >> 8) & 0xFF);
@@ -1215,6 +1210,7 @@ void Vdp::Write_Ctrl(uint16_t data)
 		if (dest_address >= 0x80) {
 			// CRam/VSRam overflow! Don't do anything.
 			VDP_Ctrl.DMA = 0;
+			VDP_Ctrl.code &= ~VdpTypes::CD_DMA_ENABLE;
 			return;
 		}
 	}
@@ -1226,6 +1222,7 @@ void Vdp::Write_Ctrl(uint16_t data)
 			// Zero-Length DMA transfers are enabled.
 			// Ignore this request.
 			VDP_Ctrl.DMA = 0;
+			VDP_Ctrl.code &= ~VdpTypes::CD_DMA_ENABLE;
 			return;
 		}
 
@@ -1266,6 +1263,7 @@ void Vdp::Write_Ctrl(uint16_t data)
 	if (VDP_Ctrl.DMA_Mode & 0x80) {
 		// TODO: What does this mean?
 		VDP_Ctrl.DMA = 0;
+		VDP_Ctrl.code &= ~VdpTypes::CD_DMA_ENABLE;
 		return;
 	}
 
@@ -1431,6 +1429,7 @@ void Vdp::Write_Ctrl(uint16_t data)
 		default:
 			// Invalid DMA mode.
 			VDP_Ctrl.DMA = 0;
+			VDP_Ctrl.code &= ~VdpTypes::CD_DMA_ENABLE;
 			break;
 	}
 

src/libgens/Vdp/Vdp.cpp
@@ -140,14 +140,7 @@ void Vdp::reset(void)
 	VDP_Int = 0;
 
 	// VDP control struct.
-	VDP_Ctrl.data[0] = 0;
-	VDP_Ctrl.data[1] = 0;
-	VDP_Ctrl.code = 0;
-	VDP_Ctrl.access = CD_Table[VDP_Ctrl.code];
-	VDP_Ctrl.address = 0;
-	VDP_Ctrl.DMA_Mode = 0;
-	VDP_Ctrl.DMA = 0;
-	VDP_Ctrl.ctrl_latch = false;
+	VDP_Ctrl.reset();
 
 	// Set the VDP update flags.
 	MarkVRamDirty();
@@ -174,8 +167,12 @@ void Vdp::zomgSaveMD(LibZomg::Zomg *zomg) const
 	// Save the internal registers.
 	Zomg_VdpCtrl_16_t ctrl_reg;
 	ctrl_reg.header = ZOMG_VDPCTRL_16_HEADER;
+#if 0
+	// FIXME: Remove these from ZOMG.
+	// NOTE: Address latch MAY be needed for SMS...
 	ctrl_reg.ctrl_word[0] = VDP_Ctrl.data[0];
 	ctrl_reg.ctrl_word[1] = VDP_Ctrl.data[1];
+#endif
 	ctrl_reg.ctrl_latch = !!VDP_Ctrl.ctrl_latch;
 	ctrl_reg.code = VDP_Ctrl.code;
 	ctrl_reg.address = VDP_Ctrl.address;
@@ -231,11 +228,14 @@ void Vdp::zomgRestoreMD(LibZomg::Zomg *zomg)
 	Zomg_VdpCtrl_16_t ctrl_reg;
 	int ret = zomg->loadVdpCtrl_16(&ctrl_reg);
 	if (ret > 0) {
+#if 0
+		// FIXME: Remove these from ZOMG.
+		// NOTE: Address latch MAY be needed for SMS...
 		VDP_Ctrl.data[0] = ctrl_reg.ctrl_word[0];
 		VDP_Ctrl.data[1] = ctrl_reg.ctrl_word[1];
+#endif
 		VDP_Ctrl.ctrl_latch = !!ctrl_reg.ctrl_latch;
 		VDP_Ctrl.code = ctrl_reg.code;
-		VDP_Ctrl.access = CD_Table[VDP_Ctrl.code];
 		VDP_Ctrl.address = ctrl_reg.address;
 		Reg_Status.write_raw(ctrl_reg.status);
 

src/libgens/Vdp/Vdp.hpp
@@ -353,9 +353,6 @@ class Vdp
 		 * VDP control struct.
 		 */
 		struct {
-			// Control word buffer.
-			uint16_t data[2];
-
 			/**
 			 * ctrl_latch: Control word latch.
 			 * 0: Next control word is FIRST word.
@@ -371,12 +368,20 @@ class Vdp
 			// automatically updates 'access'?
 			// NOTE: 'access' does NOT include DMA information.
 			uint8_t code;		// Access code. (CD5-CD0)
-			uint8_t access;		// Maps code to VDEST_t values.
 			uint16_t address;	// Address counter.
 
 			// DMA values.
 			uint8_t DMA_Mode;	// (DMA ADDRESS HIGH & 0xC0) [reg 23]
 			uint8_t DMA;		// HIGH byte from CD_Table[].
+
+			void reset(void)
+			{
+				ctrl_latch = 0;
+				code = 0;
+				address = 0;
+				DMA_Mode = 0;
+				DMA = 0;
+			}
 		} VDP_Ctrl;
 
 		/**

src/libgens/Vdp/VdpReg.hpp
@@ -4,7 +4,7 @@
  *                                                                         *
  * Copyright (c) 1999-2002 by Stéphane Dallongeville.                      *
  * Copyright (c) 2003-2004 by Stéphane Akhoun.                             *
- * Copyright (c) 2008-2014 by David Korth.                                 *
+ * Copyright (c) 2008-2015 by David Korth.                                 *
  *                                                                         *
  * This program is free software; you can redistribute it and/or modify it *
  * under the terms of the GNU General Public License as published by the   *
@@ -241,6 +241,48 @@ namespace VdpTypes
 			uint8_t H_Int;		// H Interrupt. [8-bit]
 		};
 		m4_t m4;
+	};
+
+	/**
+	  * VDP Codeword register.
+	  * MD: [CD5 CD4 CD3 CD2 CD1 CD0]
+	  * - CD5: DMA enable.
+	  * - CD4: Busy.
+	  * - CD3-CD1: Destination.
+	  * - CD0: Read/Write. (0 = read, 1 = write)
+	  */
+	enum VDP_CodeWord {
+		// CD5: DMA enable.
+		CD_DMA_ENABLE		= (1 << 5),
+
+		// CD4: Busy.
+		CD_BUSY			= (1 << 4),
+
+		// CD3-1: Destination.
+		// NOTE: CRAM has different destinations
+		// depending on read/write mode.
+		CD_DEST_VRAM		= 0x00,
+		CD_DEST_REGISTER_INT_W	= 0x02, // CD0=0
+		CD_DEST_CRAM_INT_W	= 0x02,
+		CD_DEST_VSRAM		= 0x04,
+		CD_DEST_CRAM_INT_R	= 0x08,
+		CD_DEST_MASK		= 0x0E,
+
+		// CD0: Read/write.
+		CD_MODE_READ		= 0x00,
+		CD_MODE_WRITE		= 0x01,
+		CD_MODE_MASK		= 0x01,
+
+		// CD3-0 combined.
+		CD_DEST_VRAM_READ	= (CD_DEST_VRAM | CD_MODE_READ),
+		CD_DEST_VRAM_WRITE	= (CD_DEST_VRAM | CD_MODE_WRITE),
+		CD_DEST_CRAM_READ	= (CD_DEST_CRAM_INT_R | CD_MODE_READ),
+		CD_DEST_CRAM_WRITE	= (CD_DEST_CRAM_INT_W | CD_MODE_WRITE),
+		CD_DEST_VSRAM_READ	= (CD_DEST_VSRAM | CD_MODE_READ),
+		CD_DEST_VSRAM_WRITE	= (CD_DEST_VSRAM | CD_MODE_WRITE),
+		// NOTE: REGISTER_WRITE looks like a READ, but it isn't!
+		CD_DEST_REGISTER_WRITE	= (CD_DEST_REGISTER_INT_W | CD_MODE_READ),
+		CD_DEST_MODE_MASK	= 0x0F,
 	};
 }
 

commit c3343c4c009456b22461feab3ab0244b988ce15b

[libgens] Vdp::writeCtrlMD(): The DMA Enabled bit is Set2 & 0x10, not Set2 & 0x04.
This fixes broken menus and a black screen in-game in "Bubba 'N' Stix".

This bug was reported by Alfonso on the Sega Retro Gens/GS II
compatibility list: http://segaretro.org/Gens/GS_II_Compatibility_List

This fixes a regression from commit a214b411413a4bd884fbfd9e0b2b8d4d24565afd.
(VdpReg.hpp: Initial codeword refactoring.)

src/libgens/Vdp/VdpIo.cpp
@@ -525,7 +525,7 @@ void Vdp::writeCtrlMD(uint16_t ctrl)
 	 * when the second control word is processed.
 	 *
 	 * NOTE 2: CD5 is only updated if DMA Enabled == 1.
-	 * (VDP_Reg.m5.Set2 & 0x04)
+	 * (VDP_Reg.m5.Set2 & 0x10)
 	 *
 	 * NOTE 3: A16 is only used if 128 KB mode is enabled.
 	 */
@@ -542,7 +542,7 @@ void Vdp::writeCtrlMD(uint16_t ctrl)
 	// Update the VDP access code register: CD(4..2)
 	d->VDP_Ctrl.code &= ~0x1C;
 	d->VDP_Ctrl.code |= ((ctrl >> 2) & 0x1C);
-	if (d->VDP_Reg.m5.Set2 & 0x04) {
+	if (d->VDP_Reg.m5.Set2 & 0x10) {
 		// DMA is enabled. Update CD5.
 		d->VDP_Ctrl.code &= ~VdpTypes::CD_DMA_ENABLE;
 		d->VDP_Ctrl.code |= ((ctrl >> 2) & VdpTypes::CD_DMA_ENABLE);